51150df0dc45f1f67472c0284c0de73a6bd3f50a
Pete Muir <pmuir@bleepbleep.org.uk>
2008-07-01 17:25:14 +0000

    Allow Initializer on constructor
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@33 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-api/src/main/java/javax/webbeans/Initializer.java
+++ webbeans-api/src/main/java/javax/webbeans/Initializer.java
@@ -1,13 +1,14 @@
 package javax.webbeans;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target(METHOD)
+@Target({METHOD,CONSTRUCTOR})
 @Retention(RUNTIME)
 @Documented
 public @interface Initializer
