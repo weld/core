64a67a2ff5859e706dff42be8cf3e1cca8afd0c5
Pete Muir <pmuir@bleepbleep.org.uk>
2008-08-05 23:04:16 +0000

    application scoped dynamic binding
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@63 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-impl/src/main/java/org/jboss/webbeans/bindings/ApplicationScopedBinding.java
@@ -0,0 +1,9 @@
+package org.jboss.webbeans.bindings;
+
+import javax.webbeans.ApplicationScoped;
+import javax.webbeans.DynamicBinding;
+
+public class ApplicationScopedBinding extends DynamicBinding<ApplicationScoped> implements ApplicationScoped
+{
+
+}
