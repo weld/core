c19518723d1e317cb6a55d426218c482debb6af5
Pete Muir <pmuir@bleepbleep.org.uk>
2008-08-10 15:12:21 +0000

    oops
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@90 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/DeploymentStrategyTest.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/DeploymentStrategyTest.java
@@ -2,8 +2,6 @@ package org.jboss.webbeans.test;
 
 import java.net.URL;
 
-import javax.webbeans.Container;
-
 import org.jboss.webbeans.ContainerImpl;
 import org.jboss.webbeans.deployment.DeploymentStrategy;
 import org.jboss.webbeans.scannotation.ClasspathUrlFinder;
@@ -16,7 +14,7 @@ public class DeploymentStrategyTest
    public void testDeploymentStrategy()
    {
       URL[] urls = {ClasspathUrlFinder.findClassBase(DeploymentStrategyTest.class)};
-      Container container = new ContainerImpl(null);
+      ContainerImpl container = new ContainerImpl(null);
       DeploymentStrategy deploymentStrategy = new DeploymentStrategy(Thread.currentThread().getContextClassLoader(), container);
       deploymentStrategy.scan(urls, "org.jboss.webbeans.test.annotations.broken", "org.jboss.webbeans.test.bindings.broken", "org.jboss.webbeans.test.components.broken");
    }
