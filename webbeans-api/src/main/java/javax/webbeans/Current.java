b5d879b9def1ebe9d4be8dad0cff6a58f5325c8b
Pete Muir <pmuir@bleepbleep.org.uk>
2008-09-08 18:56:00 +0000

    Can be applied to parameters
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@96 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-api/src/main/java/javax/webbeans/Current.java
+++ webbeans-api/src/main/java/javax/webbeans/Current.java
@@ -20,6 +20,7 @@ package javax.webbeans;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -31,7 +32,7 @@ import java.lang.annotation.Target;
  * @author Pete Muir
  */
 
-@Target( { TYPE, METHOD, PARAMETER })
+@Target( { TYPE, METHOD, PARAMETER, FIELD })
 @Retention(RUNTIME)
 @Documented
 @BindingType
