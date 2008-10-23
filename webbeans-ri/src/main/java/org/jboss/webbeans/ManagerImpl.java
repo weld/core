fe49b97f3ccad92873c1b75cfc7ba051330e175f
Gavin King <gavin.king@gmail.com>
2008-10-23 21:05:34 +0000

    unused import
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@134 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/main/java/org/jboss/webbeans/ManagerImpl.java
+++ webbeans-ri/src/main/java/org/jboss/webbeans/ManagerImpl.java
@@ -5,7 +5,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.webbeans.DeploymentException;
