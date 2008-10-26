ffbe40445a104f6f6f96161e1a84a5089c195ccf
Pete Muir <pmuir@bleepbleep.org.uk>
2008-10-26 12:25:44 +0000

    Naive tests for a web bean
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@162 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/CommonWebBeanTest.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/CommonWebBeanTest.java
@@ -1,5 +1,12 @@
 package org.jboss.webbeans.test;
 
+import static org.jboss.webbeans.test.util.Util.createSimpleModel;
+
+import javax.webbeans.Production;
+import javax.webbeans.RequestScoped;
+
+import org.jboss.webbeans.model.bean.BeanModel;
+import org.jboss.webbeans.test.beans.RedSnapper;
 import org.testng.annotations.Test;
 
 /**
@@ -8,30 +15,37 @@ import org.testng.annotations.Test;
  * @author Pete Muir
  *
  */
-public class CommonWebBeanTest {
+public class CommonWebBeanTest extends AbstractTest 
+{
 
+   // TODO This should actually somehow test the reverse - that the container throws a definition exception if any of these occur
+   
 	@Test @SpecAssertion(section="2")
 	public void testApiTypesNonEmpty()
 	{
-		assert false;
+	   BeanModel<?, ?> model = createSimpleModel(RedSnapper.class, manager);
+      assert model.getApiTypes().size() > 0;
 	}
 	
 	@Test @SpecAssertion(section="2")
 	public void testBindingTypesNonEmpty()
 	{
-		assert false;
+	   BeanModel<?, ?> model = createSimpleModel(RedSnapper.class, manager);
+      assert model.getBindingTypes().size() > 0;
 	}
 	
 	@Test @SpecAssertion(section="2")
 	public void testHasScopeType()
 	{
-		assert false;
+	   BeanModel<?, ?> model = createSimpleModel(RedSnapper.class, manager);
+      assert model.getScopeType().equals(RequestScoped.class);
 	}
 	
 	@Test @SpecAssertion(section="2")
 	public void testHasDeploymentType()
 	{
-		assert false;
+		BeanModel<?, ?> model = createSimpleModel(RedSnapper.class, manager);
+		assert model.getDeploymentType().equals(Production.class);
 	}
 	
 }
