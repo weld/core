6c3d2532c4400fd42a001c27fdaeb87f399bcb36
Pete Muir <pmuir@bleepbleep.org.uk>
2008-10-26 13:34:47 +0000

    Stupid case names
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@165 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/TypeTest.java
@@ -0,0 +1,22 @@
+package org.jboss.webbeans.test;
+
+import static org.jboss.webbeans.test.util.Util.getEmptyAnnotatedType;
+
+import org.jboss.webbeans.introspector.SimpleAnnotatedType;
+import org.jboss.webbeans.model.bean.SimpleBeanModel;
+import org.jboss.webbeans.test.beans.Haddock;
+import org.testng.annotations.Test;
+
+public class TypeTest extends AbstractTest
+{
+
+   @Test @SpecAssertion(section="2.6.3")
+   public void testDefaultNamed()
+   {
+      SimpleBeanModel<Haddock> haddock = new SimpleBeanModel<Haddock>(new SimpleAnnotatedType<Haddock>(Haddock.class), getEmptyAnnotatedType(Haddock.class), manager);
+      assert haddock.getName() != null;
+      assert haddock.getName().equals("haddock");
+   }
+   
+}
+
