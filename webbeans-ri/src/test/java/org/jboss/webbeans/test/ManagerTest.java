e82bb043d1259769d83013f71a75612c503ec38c
Nicklas Karlsson <nickarls@gmail.com>
2008-10-28 08:25:21 +0000

    Context tests
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@191 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
@@ -11,7 +11,37 @@ public class ManagerTest
    {
       assert false;
    }
+
+   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   public void testAddContext()
+   {
+      assert false;
+   }
    
+   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   public void testGetContextWithNoActiveContextsFails()
+   {
+      assert false;
+   }
+
+   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   public void testGetContextWithTooManyActiveContextsFails()
+   {
+      assert false;
+   }
+
+   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   public void testGetContextWithNoRegisteredContextsFails()
+   {
+      assert false;
+   }
+
+   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   public void testGetContextReturnsActiveContext()
+   {
+      assert false;
+   }
+
    @Test(groups={"manager", "ejb3"}) @SpecAssertion(section="4.8")
    public void testManagerLookupInJndi()
    {
