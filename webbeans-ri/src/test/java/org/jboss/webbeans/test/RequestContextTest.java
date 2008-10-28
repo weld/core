ea99d023865dbe7aed5cee3f1a66226ea46495c9
Pete Muir <pmuir@bleepbleep.org.uk>
2008-10-28 15:39:14 +0000

    Stubs for request context tests
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@198 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/RequestContextTest.java
@@ -0,0 +1,78 @@
+package org.jboss.webbeans.test;
+
+import org.testng.annotations.Test;
+
+public class RequestContextTest extends AbstractTest
+{
+   
+   @Test(groups={"servlet", "contexts"}) @SpecAssertion(section="8.5.1")
+   public void testScopeActiveDuringServiceMethod()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"webservice", "contexts"}) @SpecAssertion(section="8.5.1")
+   public void testScopeActiveDuringWebServiceInvocation()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeActiveDuringRemoteInvocationOfEjbs()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeActiveDuringEjbTimeoust()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeActiveDuringMessageDelivery()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"servlet", "contexts"}) @SpecAssertion(section="8.5.1")
+   public void testScopeNotSharedAcrossRequests()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"webservice", "contexts"}) @SpecAssertion(section="8.5.1")
+   public void testScopeNotSharedAcrossWebServiceInvocations()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeNotSharedAcrossRemoteInvocationsOfEjbs()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeNotSharedAcrossEjbTimeouts()
+   {
+      assert false;
+   }
+   
+   @Test(groups={"contexts", "ejb3"}) @SpecAssertion(section="8.5.1")
+   public void testScopeNotSharedAcrossMessageDelivery()
+   {
+      assert false;
+   }
+   
+   /*
+
+   @Test @SpecAssertion(section="8.5.3")
+   public void test
+   {
+      assert false;
+   }
+
+    */
+   
+}
