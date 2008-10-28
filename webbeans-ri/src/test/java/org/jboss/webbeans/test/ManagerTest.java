6a8ff3d896b5ae418584b1f9a09d4bb04323fcfb
Nicklas Karlsson <nickarls@gmail.com>
2008-10-28 11:02:00 +0000

    fill in stubs for context tests
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@193 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
@@ -1,9 +1,15 @@
 package org.jboss.webbeans.test;
 
+import javax.webbeans.ContextNotActiveException;
+import javax.webbeans.RequestScoped;
+import javax.webbeans.manager.Context;
+
+import org.jboss.webbeans.contexts.NormalContext;
+import org.jboss.webbeans.contexts.RequestContext;
 import org.testng.annotations.Test;
 
 @SpecVersion("PDR")
-public class ManagerTest
+public class ManagerTest extends AbstractTest
 {
    
    @Test(groups={"manager", "injection"}) @SpecAssertion(section="4.8")
@@ -11,35 +17,42 @@ public class ManagerTest
    {
       assert false;
    }
-
-   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
-   public void testAddContext()
-   {
-      assert false;
-   }
    
-   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   @Test(expectedExceptions={ContextNotActiveException.class}, groups={"manager"}) @SpecAssertion(section="8.6")
    public void testGetContextWithNoActiveContextsFails()
    {
-      assert false;
+      Context requestContext = new RequestContext();
+      ((NormalContext)requestContext).setActive(false);
+      manager.addContext(requestContext);
+      manager.getContext(RequestScoped.class);
    }
 
-   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   @Test(expectedExceptions={IllegalArgumentException.class}, groups={"manager"}) @SpecAssertion(section="8.6")
    public void testGetContextWithTooManyActiveContextsFails()
    {
-      assert false;
+      Context firstContext = new RequestContext();
+      Context secondContext = new RequestContext();
+      manager.addContext(firstContext);
+      manager.addContext(secondContext);
+      manager.getContext(RequestScoped.class);
+      assert true;
    }
 
-   @Test(groups={"manager"}) @SpecAssertion(section="8.6")
+   @Test(expectedExceptions={ContextNotActiveException.class}, groups={"manager"}) @SpecAssertion(section="8.6")
    public void testGetContextWithNoRegisteredContextsFails()
    {
+      manager.getContext(RequestScoped.class);
       assert false;
    }
 
    @Test(groups={"manager"}) @SpecAssertion(section="8.6")
    public void testGetContextReturnsActiveContext()
    {
-      assert false;
+      Context requestContext = new RequestContext();
+      manager.addContext(requestContext);
+      Context testContext = manager.getContext(RequestScoped.class);
+      assert testContext == requestContext;
+      
    }
 
    @Test(groups={"manager", "ejb3"}) @SpecAssertion(section="4.8")
