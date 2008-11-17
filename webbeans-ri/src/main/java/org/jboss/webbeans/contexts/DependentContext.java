bd3112fcf889a75e7820d98d28bd57d1ec7473ca
Nicklas Karlsson <nickarls@gmail.com>
2008-11-17 06:55:01 +0000

    oops, forgot to fix the dependent context
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@313 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/main/java/org/jboss/webbeans/contexts/DependentContext.java
+++ webbeans-ri/src/main/java/org/jboss/webbeans/contexts/DependentContext.java
@@ -1,21 +1,16 @@
 package org.jboss.webbeans.contexts;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import javax.webbeans.ContextNotActiveException;
 import javax.webbeans.Dependent;
 import javax.webbeans.manager.Bean;
 
-public class DependentContext extends AbstractContext
+public class DependentContext extends PrivateContext
 {
-   private BeanMap beans;
-   private AtomicBoolean active;
-   
+
    public DependentContext()
    {
       super(Dependent.class);
-      beans = new BeanMap();
-      active = new AtomicBoolean(false);
+      setActive(false);
    }
 
    @Override
@@ -28,18 +23,6 @@ public class DependentContext extends AbstractContext
 
       return create == false ? null : bean.create();
    }   
-
-   @Override
-   protected AtomicBoolean getActive()
-   {
-      return active;
-   }
-
-   @Override
-   protected BeanMap getBeanMap()
-   {
-      return beans;
-   }
    
    @Override
    public String toString()
