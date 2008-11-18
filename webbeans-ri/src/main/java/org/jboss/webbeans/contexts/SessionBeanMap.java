460386765f077e4820fa623c11db8086b89b05b3
Nicklas Karlsson <nickarls@gmail.com>
2008-11-18 11:03:43 +0000

    more docs
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@324 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/main/java/org/jboss/webbeans/contexts/SessionBeanMap.java
+++ webbeans-ri/src/main/java/org/jboss/webbeans/contexts/SessionBeanMap.java
@@ -1,3 +1,20 @@
+/*
+* JBoss, Home of Professional Open Source
+* Copyright 2008, Red Hat Middleware LLC, and individual contributors
+* by the @authors tag. See the copyright.txt in the distribution for a
+* full listing of individual contributors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+* http://www.apache.org/licenses/LICENSE-2.0
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,  
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 package org.jboss.webbeans.contexts;
 
 import java.util.ArrayList;
@@ -13,7 +30,7 @@ import org.jboss.webbeans.ManagerImpl;
  * A BeanMap that uses a HTTP session as backing map
  * 
  * @author Nicklas Karlsson
- *
+ * @see org.jboss.webbeans.SessionContext
  */
 public class SessionBeanMap implements BeanMap
 {
