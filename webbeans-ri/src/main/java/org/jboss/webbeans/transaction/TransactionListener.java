b33a2f5ae95cc71126fe04c77d1d15f99d9379a7
Nicklas Karlsson <nickarls@gmail.com>
2008-11-21 22:05:48 +0000

    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@351 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-ri/src/main/java/org/jboss/webbeans/transaction/TransactionListener.java
@@ -0,0 +1,49 @@
+package org.jboss.webbeans.transaction;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJBException;
+import javax.ejb.SessionSynchronization;
+import javax.ejb.Stateful;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.transaction.Status;
+import javax.transaction.Synchronization;
+import javax.webbeans.RequestScoped;
+
+@Stateful
+@RequestScoped
+@TransactionAttribute(TransactionAttributeType.SUPPORTS)
+public class TransactionListener implements SessionSynchronization
+{
+
+   private List<Synchronization> synchronizations = new ArrayList<Synchronization>();
+
+   public void afterBegin() throws EJBException, RemoteException
+   {
+   }
+
+   public void afterCompletion(boolean success) throws EJBException, RemoteException
+   {
+      for (Synchronization synchronization : synchronizations)
+      {
+         synchronization.afterCompletion(success ? Status.STATUS_COMMITTED : Status.STATUS_ROLLEDBACK);
+      }
+      synchronizations.clear();
+   }
+
+   public void beforeCompletion() throws EJBException, RemoteException
+   {
+      for (Synchronization synchronization : synchronizations)
+      {
+         synchronization.beforeCompletion();
+      }
+   }
+
+   public void registerSynhronization(Synchronization synchronization)
+   {
+      synchronizations.add(synchronization);
+   }
+}
