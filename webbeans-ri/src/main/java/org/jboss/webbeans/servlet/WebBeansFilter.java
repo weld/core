b4f305315d8fdf39549767e7ba650013357efe1e
Nicklas Karlsson <nickarls@gmail.com>
2008-11-26 12:47:44 +0000

    minor: don't handle contexts directly
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@364 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/main/java/org/jboss/webbeans/servlet/WebBeansFilter.java
+++ webbeans-ri/src/main/java/org/jboss/webbeans/servlet/WebBeansFilter.java
@@ -8,45 +8,34 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.webbeans.manager.Manager;
-
-import org.jboss.webbeans.contexts.AbstractContext;
-import org.jboss.webbeans.contexts.RequestContext;
-
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * 
  * @author Shane Bryzak
- *
+ * 
  */
 public class WebBeansFilter implements Filter
 {
-   private Manager container;
-
-   public void init(FilterConfig filterConfig) throws ServletException 
+   public void init(FilterConfig filterConfig) throws ServletException
    {
-      
+
    }
 
-   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) 
-       throws IOException, ServletException 
+   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException
    {
-      AbstractContext requestContext = new RequestContext();
-
       try
       {
-         container.addContext(requestContext);
-         
+         ServletLifecycle.beginRequest((HttpServletRequest) request);
          chain.doFilter(request, response);
       }
       finally
       {
-         requestContext.destroy(container);
-      }      
+         ServletLifecycle.endRequest((HttpServletRequest) request);
+      }
    }
 
-   public void destroy() 
+   public void destroy()
    {
-      
    }
 }
