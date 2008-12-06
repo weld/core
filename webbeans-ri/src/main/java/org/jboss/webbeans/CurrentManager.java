e86c89ff35bb5a8c9806929ff60db9611859576b
Gavin King <gavin.king@gmail.com>
2008-12-06 22:40:50 +0000

    move the statics somewhere else
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@428 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-ri/src/main/java/org/jboss/webbeans/CurrentManager.java
@@ -0,0 +1,31 @@
+package org.jboss.webbeans;
+
+import org.jboss.webbeans.contexts.ApplicationContext;
+import org.jboss.webbeans.contexts.RequestContext;
+import org.jboss.webbeans.contexts.SessionContext;
+
+public class CurrentManager {
+
+   protected static ManagerImpl rootManager = new ManagerImpl();
+
+   public static ManagerImpl rootManager()
+   {
+      return rootManager;
+   }
+   
+   public static void setRootManager(ManagerImpl rootManager) {
+      CurrentManager.rootManager = rootManager;
+   }
+
+   /**
+    * Set up the root manager. 
+    * TODO: move this to Bootstrap
+    */
+   static {
+      rootManager = new ManagerImpl();
+      rootManager.addContext(RequestContext.INSTANCE);
+      rootManager.addContext(SessionContext.INSTANCE);
+      rootManager.addContext(ApplicationContext.INSTANCE);
+   }
+   
+}
