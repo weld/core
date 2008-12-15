69be361c9f33884ca18794d153704266b31596a3
Pete Muir <pmuir@bleepbleep.org.uk>
2008-12-15 12:13:59 +0000

    Minor
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@527 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/beans/nonBeans/UIComponentBean.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/beans/nonBeans/UIComponentBean.java
@@ -19,276 +19,236 @@ public class UIComponentBean extends UIComponent
    @Override
    protected void addFacesListener(FacesListener arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void broadcast(FacesEvent arg0) throws AbortProcessingException
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void decode(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void encodeBegin(FacesContext arg0) throws IOException
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void encodeChildren(FacesContext arg0) throws IOException
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void encodeEnd(FacesContext arg0) throws IOException
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public UIComponent findComponent(String arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public Map<String, Object> getAttributes()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public int getChildCount()
    {
-      // TODO Auto-generated method stub
       return 0;
    }
    
    @Override
    public List<UIComponent> getChildren()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public String getClientId(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    protected FacesContext getFacesContext()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    protected FacesListener[] getFacesListeners(Class arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public UIComponent getFacet(String arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public Map<String, UIComponent> getFacets()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public Iterator<UIComponent> getFacetsAndChildren()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public String getFamily()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public String getId()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public UIComponent getParent()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    protected Renderer getRenderer(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public String getRendererType()
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public boolean getRendersChildren()
    {
-      // TODO Auto-generated method stub
       return false;
    }
    
    @Override
    public ValueBinding getValueBinding(String arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public boolean isRendered()
    {
-      // TODO Auto-generated method stub
       return false;
    }
    
    @Override
    public void processDecodes(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void processRestoreState(FacesContext arg0, Object arg1)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public Object processSaveState(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    @Override
    public void processUpdates(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void processValidators(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void queueEvent(FacesEvent arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    protected void removeFacesListener(FacesListener arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void setId(String arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void setParent(UIComponent arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void setRendered(boolean arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void setRendererType(String arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
    @Override
    public void setValueBinding(String arg0, ValueBinding arg1)
    {
-      // TODO Auto-generated method stub
       
    }
    
    public boolean isTransient()
    {
-      // TODO Auto-generated method stub
       return false;
    }
    
    public void restoreState(FacesContext arg0, Object arg1)
    {
-      // TODO Auto-generated method stub
       
    }
    
    public Object saveState(FacesContext arg0)
    {
-      // TODO Auto-generated method stub
       return null;
    }
    
    public void setTransient(boolean arg0)
    {
-      // TODO Auto-generated method stub
       
    }
    
