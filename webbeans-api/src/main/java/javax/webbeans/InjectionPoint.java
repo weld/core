ee83ab87c18d93edd9e3840565097562795cde38
Gavin King <gavin.king@gmail.com>
2008-12-22 03:36:48 +0000

    Add InjectionPoint API
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@657 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-api/src/main/java/javax/webbeans/InjectionPoint.java
@@ -0,0 +1,19 @@
+package javax.webbeans;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+import java.util.Set;
+
+import javax.webbeans.manager.Bean;
+
+public interface InjectionPoint { 
+	public Type getType(); 
+	public Set<Annotation> getBindingTypes(); 
+	public Object getInstance(); 
+	public Bean<?> getBean(); 
+	public Member getMember();
+	public <T extends Annotation> T getAnnotation(Class<T> annotationType); 
+	public Annotation[] getAnnotations(); 
+	public boolean isAnnotationPresent(Class<? extends Annotation> annotationType); 
+} 
