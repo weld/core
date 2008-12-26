f8959ef8df44db7b84fb278c289d729cb52c4a6f
Nicklas Karlsson <nickarls@gmail.com>
2008-12-26 22:46:18 +0000

    Move beanmap stuff to own package
    Move to singular naming (contexts -> context etc)
    
    Part I
    
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@736 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/main/java/org/jboss/webbeans/bindings/InitializedBinding.java
+++
@@ -1,9 +0,0 @@
-package org.jboss.webbeans.bindings;
-
-import javax.webbeans.AnnotationLiteral;
-import javax.webbeans.manager.Initialized;
-
-public class InitializedBinding extends AnnotationLiteral<Initialized> implements Initialized
-{
-   
-}
