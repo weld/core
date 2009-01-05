afc1ff9fe208e556db75b50adae354df830fc277
Pete Muir <pmuir@bleepbleep.org.uk>
2009-01-05 18:22:16 +0000

    First stab at resolver interfaces for @EJB and @PersistenceContext
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@774 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ webbeans-ri-spi/src/main/java/org/jboss/webbeans/ejb/spi/EjbResolver.java
@@ -0,0 +1,39 @@
+package org.jboss.webbeans.ejb.spi;
+
+/**
+ * A container should implement this interface to allow the Web Beans RI to
+ * resolve EJBs and JPA persistence units
+ * 
+ * @author Pete Muir
+ * 
+ */
+public interface EjbResolver
+{
+   /**
+    * Resolve the EJB for the given parameters
+    * 
+    * @param name
+    *           The logical name of the ejb reference within the declaring
+    *           component's (java:comp/env) environment.
+    * @param beanName
+    *           The ejb-name of the Enterprise Java Bean to which this reference
+    *           is mapped. Only applicable if the target EJB is defined within
+    *           the same application or stand-alone module as the declaring
+    *           component.
+    * @param beanInterface
+    *           Holds one of the following interface types of the target EJB : [
+    *           Local business interface, Remote business interface, Local Home
+    *           interface, Remote Home interface ]
+    * @param mappedName
+    *           The product specific name of the EJB component to which this ejb
+    *           reference should be mapped. This mapped name is often a global
+    *           JNDI name, but may be a name of any form. Application servers
+    *           are not required to support any particular form or type of
+    *           mapped name, nor the ability to use mapped names. The mapped
+    *           name is product-dependent and often installation-dependent. No
+    *           use of a mapped name is portable.
+    * @return
+    */
+   public Object resolveEjb(String name, String beanName, Class<?> beanInterface, String mappedName);
+   
+}
