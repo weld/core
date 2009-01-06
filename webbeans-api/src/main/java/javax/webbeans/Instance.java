1dbc53d3f93d78efa8badbddedbda463bdf33cd0
Pete Muir <pmuir@bleepbleep.org.uk>
2009-01-06 23:07:39 +0000

    Javadoc
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@795 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-api/src/main/java/javax/webbeans/Instance.java
+++ webbeans-api/src/main/java/javax/webbeans/Instance.java
@@ -1,19 +1,19 @@
 /*
-* JBoss, Home of Professional Open Source
-* Copyright 2008, Red Hat Middleware LLC, and individual contributors
-* by the @authors tag. See the copyright.txt in the distribution for a
-* full listing of individual contributors.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-* http://www.apache.org/licenses/LICENSE-2.0
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,  
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,  
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package javax.webbeans;
 
@@ -25,10 +25,25 @@ import java.lang.annotation.Annotation;
  * @author Gavin King
  * 
  * @param <T>
-*            the type of the event object
+ *           the type of the event object
  */
 
 public interface Instance<T>
 {
-   public T get(Annotation... bindings);  
+   /**
+    * Get an instance of a Web Bean of the specified type.
+    * 
+    * Additional binding annotations may be specified at the injection point.
+    * 
+    * @param bindings
+    *           Additional binding types
+    * @return an instance of a Web Bean of the specified type
+    * @throws DuplicateBindingTypeException
+    *            if two instances of the same binding type are passed
+    * @throws IllegalArgumentException
+    *            if an instance of an annotation that is not a binding type is
+    *            passed
+    * 
+    */
+   public T get(Annotation... bindings);
 }
