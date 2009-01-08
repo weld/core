c3873552298baadd36dee445f9d941984c45aac0
Pete Muir <pmuir@bleepbleep.org.uk>
2009-01-08 16:47:30 +0000

    Javadoc
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@831 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri-spi/src/main/java/org/jboss/webbeans/ejb/spi/EjbResolver.java
+++ webbeans-ri-spi/src/main/java/org/jboss/webbeans/ejb/spi/EjbResolver.java
@@ -2,7 +2,7 @@ package org.jboss.webbeans.ejb.spi;
 
 import java.lang.annotation.Annotation;
 
-import javax.persistence.EntityManager;
+import javax.webbeans.DefinitionException;
 import javax.webbeans.InjectionPoint;
 
 import org.jboss.webbeans.resources.spi.Naming;
@@ -20,23 +20,60 @@ public interface EjbResolver
    public static final String PROPERTY_NAME = EjbResolver.class.getName();
    
    /**
-    * Resolve the value for the given @EJB injection point
+    * Resolve the value for the given
     * 
-    * @param injectionPoint The injection point metadata
+    * @EJB injection point
+    * 
+    * @param injectionPoint
+    *           The injection point metadata
     * @return the JNDI name
+    * @throws IllegalArgumentException
+    *            if the injection point is not annotated with
+    * @EJB
+    * @throws DefinitionException
+    *            if the injection point is not suitable for injection
+    * @throws IllegalStateException
+    *            if no EJBs can be resolved for injection
     */
    public Object resolveEjb(InjectionPoint injectionPoint, Naming naming);
    
    /**
-    * Resolve the value for the given @PersistenceContext injection point
+    * Resolve the value for the given
     * 
-    * @param injectionPoint The injection point metadata
+    * @PersistenceContext injection point
+    * 
+    * @param injectionPoint
+    *           The injection point metadata
     * @return the JNDI name
+    * @throws IllegalArgumentException
+    *            if the injection point is not annotated with
+    * @PersistenceContext
+    * @throws UnsupportedOperationException
+    *            if the injection point is annotated
+    * @PersistenceContext(EXTENTED)
+    * @throws IllegalStateException
+    *            if no suitable persistence units can be resolved for injection
     */
    public Object resolvePersistenceUnit(InjectionPoint injectionPoint, Naming naming);
    
+   /**
+    * Get the annotation which defines an
+    * 
+    * @EJB injection point
+    * 
+    * @return the annotation which defines an
+    * @EJB injection point
+    */
    public Class<? extends Annotation> getEJBAnnotation();
    
+   /**
+    * Get the annoation which defines a
+    * 
+    * @PersistenceContext injection point
+    * 
+    * @return the annoation which defines a
+    * @PersistenceContext injection point
+    */
    public Class<? extends Annotation> getPersistenceContextAnnotation();
    
 }
