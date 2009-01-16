522c27af0300f5eb99fabb98e8c7a42f2e4e3aac
Pete Muir <pmuir@bleepbleep.org.uk>
2009-01-16 17:26:15 +0000

    port manager tests, fix build problem
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1026 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/ManagerTest.java
@@ -4,21 +4,13 @@ import javax.webbeans.ContextNotActiveException;
 import javax.webbeans.RequestScoped;
 import javax.webbeans.manager.Context;
 
-import org.jboss.webbeans.bean.SimpleBean;
 import org.jboss.webbeans.context.RequestContext;
-import org.jboss.webbeans.test.beans.FishFarmOffice;
 import org.testng.annotations.Test;
 
 @SpecVersion("PDR")
 public class ManagerTest extends AbstractTest
 {
-   
-   @Test(groups={"manager", "injection", "deployment"}) @SpecAssertion(section="5.8")
-   public void testInjectingManager()
-   {
-      FishFarmOffice fishFarmOffice = SimpleBean.of(FishFarmOffice.class, manager).create();
-      assert fishFarmOffice.manager != null;
-   }
+  
    
    @Test(expectedExceptions={ContextNotActiveException.class}, groups={"manager"}) @SpecAssertion(section="8.6")
    public void testGetContextWithNoActiveContextsFails()
@@ -50,13 +42,6 @@ public class ManagerTest extends AbstractTest
    {
       manager.getContext(RequestScoped.class);
    }
-
-   @Test(groups={"stub", "manager", "ejb3"}) @SpecAssertion(section="5.8")
-   public void testManagerLookupInJndi()
-   {
-      assert false;
-   }
-
 
    /*
    
