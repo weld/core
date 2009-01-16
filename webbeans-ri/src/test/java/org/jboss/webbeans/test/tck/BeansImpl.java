dd4a5ca7420ba7757456cfe4a2b309a9c2d804dd
Shane Bryzak <sbryzak@redhat.com>
2009-01-16 04:21:14 +0000

    ported tests to tck
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1008 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/tck/BeansImpl.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/tck/BeansImpl.java
@@ -1,5 +1,6 @@
 package org.jboss.webbeans.test.tck;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import javax.webbeans.manager.Bean;
@@ -7,6 +8,7 @@ import javax.webbeans.manager.Bean;
 import org.jboss.webbeans.CurrentManager;
 import org.jboss.webbeans.bean.AbstractClassBean;
 import org.jboss.webbeans.bean.EnterpriseBean;
+import org.jboss.webbeans.bean.ProducerFieldBean;
 import org.jboss.webbeans.bean.ProducerMethodBean;
 import org.jboss.webbeans.bean.SimpleBean;
 import org.jboss.webbeans.tck.api.Beans;
@@ -36,6 +38,18 @@ public class BeansImpl implements Beans
       else
       {
          throw new IllegalStateException("Cannot create a producer method from a bean that wasn't created by the RI " + declaringBean);
+      }
+   }
+   
+   public <T> Bean<T> createProducerFieldBean(Field field, Bean<?> declaringBean)
+   {
+      if (declaringBean instanceof AbstractClassBean)
+      {
+         return ProducerFieldBean.of(field, (AbstractClassBean<?>) declaringBean, CurrentManager.rootManager());
+      }
+      else
+      {
+         throw new IllegalStateException("Cannot create a producer field from a bean that wasn't created by the RI " + declaringBean);
       }
    }
 
