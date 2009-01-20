3099a3dba0d40159b1e84a2446fc84db04ef4474
Pete Muir <pmuir@bleepbleep.org.uk>
2009-01-20 13:26:28 +0000

    wrong import
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1122 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/unit/implementation/RemoveMethodTests.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/unit/implementation/RemoveMethodTests.java
@@ -1,6 +1,6 @@
 package org.jboss.webbeans.test.unit.implementation;
 
-import org.jboss.webbeans.tck.AbstractTest;
+import org.jboss.webbeans.test.unit.AbstractTest;
 import org.testng.annotations.Test;
 
 public class RemoveMethodTests extends AbstractTest
