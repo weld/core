2b0735d41aaf42b94fb57bd824b21ec61440471f
Pete Muir <pmuir@bleepbleep.org.uk>
2009-02-08 17:02:04 +0000

    Much better configuration system for TCK, clean up TCK SPI and API
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1453 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ porting-package/src/main/java/org/jboss/webbeans/porting/ELImpl.java
@@ -0,0 +1,25 @@
+package org.jboss.webbeans.porting;
+
+import javax.el.ELContext;
+
+import org.jboss.webbeans.mock.el.EL;
+
+public class ELImpl implements org.jboss.webbeans.tck.spi.EL
+{
+   
+   
+   @SuppressWarnings("unchecked")
+   public <T> T evaluateValueExpression(String expression, Class<T> expectedType)
+   {
+      ELContext elContext = EL.createELContext();
+      return (T) EL.EXPRESSION_FACTORY.createValueExpression(elContext, expression, expectedType).getValue(elContext);
+   }
+ 
+   @SuppressWarnings("unchecked")
+   public <T> T evaluateMethodExpression(String expression, Class<T> expectedType, Class<?>[] expectedParamTypes, Object[] expectedParams)
+   {
+      ELContext elContext = EL.createELContext();
+      return (T) EL.EXPRESSION_FACTORY.createMethodExpression(elContext, expression, expectedType, expectedParamTypes).invoke(elContext, expectedParams);
+   }
+   
+}
---
+++ porting-package/src/main/java/org/jboss/webbeans/porting/StandaloneContainersImpl.java
@@ -0,0 +1,34 @@
+package org.jboss.webbeans.porting;
+
+import java.lang.annotation.Annotation;
+import java.util.List;
+
+import javax.inject.manager.Manager;
+
+import org.jboss.webbeans.ManagerImpl;
+import org.jboss.webbeans.mock.MockBootstrap;
+import org.jboss.webbeans.mock.MockWebBeanDiscovery;
+import org.jboss.webbeans.tck.spi.StandaloneContainers;
+
+public class StandaloneContainersImpl implements StandaloneContainers
+{
+   
+   public Manager deploy(List<Class<? extends Annotation>> enabledDeploymentTypes, Class<?>... classes)
+   {
+      MockBootstrap bootstrap = new MockBootstrap();
+      ManagerImpl manager = bootstrap.getManager();
+      if (enabledDeploymentTypes != null)
+      {
+         manager.setEnabledDeploymentTypes(enabledDeploymentTypes);
+      }
+      bootstrap.setWebBeanDiscovery(new MockWebBeanDiscovery(classes));
+      bootstrap.boot();
+      return manager;
+   }
+   
+   public Manager deploy(java.lang.Class<?>... classes) 
+   {
+      return deploy(null, classes);
+   }
+   
+}
