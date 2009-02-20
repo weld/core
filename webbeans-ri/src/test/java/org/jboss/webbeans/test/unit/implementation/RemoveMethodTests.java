d73bb8b7162a08be9e67b11ea5f16573733004d8
Pete Muir <pmuir@bleepbleep.org.uk>
2009-02-20 16:09:48 +0000

    Atach lots more assertions
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1615 1c488680-804c-0410-94cd-c6b725194a0e

--- webbeans-ri/src/test/java/org/jboss/webbeans/test/unit/implementation/RemoveMethodTests.java
+++ webbeans-ri/src/test/java/org/jboss/webbeans/test/unit/implementation/RemoveMethodTests.java
@@ -8,17 +8,6 @@ public class RemoveMethodTests extends AbstractTest
 
    /**
     * If the application directly calls an EJB remove method of an instance of a
-    * session bean that is a stateful session bean and declares any scope other
-    * than @Dependent, an UnsupportedOperationException is thrown.
-    */
-   @Test(groups = { "enterprisebean", "removemethod", "lifecycle", "stub" })
-   public void testApplicationCalledRemoveMethodOfStatefulSessionBeanWithNonDependentScopeFails()
-   {
-      assert false;
-   }
-
-   /**
-    * If the application directly calls an EJB remove method of an instance of a
     * session bean that is a stateful session bean and has scope @Dependent then
     * no parameters are passed to the method by the container.
     */
