96548f2c598c8ba4fc70a6d09c66474744b5e1b5
David Allen <david.allen@acm.org>
2009-03-12 22:18:47 +0000

    Added a couple more lifecycle tests for EJBs and added new operation to Beans in TCK API.
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@1949 1c488680-804c-0410-94cd-c6b725194a0e

--- porting-package/src/main/java/org/jboss/webbeans/tck/BeansImpl.java
+++ porting-package/src/main/java/org/jboss/webbeans/tck/BeansImpl.java
@@ -2,6 +2,7 @@ package org.jboss.webbeans.tck;
 
 import org.jboss.jsr299.tck.spi.Beans;
 import org.jboss.webbeans.CurrentManager;
+import org.jboss.webbeans.ejb.spi.BusinessInterfaceDescriptor;
 import org.jboss.webbeans.ejb.spi.EjbDescriptor;
 import org.jboss.webbeans.util.Reflections;
 
@@ -69,6 +70,27 @@ public class BeansImpl implements Beans
    public boolean isProxy(Object instance)
    {
       return Reflections.isProxy(instance);
+   }
+
+   public <T> T getEnterpriseBean(Class<? extends T> beanType, Class<T> localInterface)
+   {
+      T enterpriseBean = null;
+      if (CurrentManager.rootManager().getEjbDescriptorCache().containsKey(beanType))
+      {
+         EjbDescriptor<?> ejbDescriptor = CurrentManager.rootManager().getEjbDescriptorCache().get(beanType).iterator().next();
+         String jndiName = null;
+         for (BusinessInterfaceDescriptor<?> businessInterface : ejbDescriptor.getLocalBusinessInterfaces())
+         {
+            if (businessInterface.getInterface().equals(localInterface))
+            {
+               jndiName = businessInterface.getJndiName();
+            }
+         }
+         if (jndiName == null)
+            throw new NullPointerException("No JNDI name found for interface " + localInterface.getName() + " on bean " + beanType.getName());
+         enterpriseBean = CurrentManager.rootManager().getNaming().lookup(jndiName, localInterface);
+      }      
+      return enterpriseBean;
    }
 
 }
