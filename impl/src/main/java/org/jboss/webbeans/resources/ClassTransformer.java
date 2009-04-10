f0f493d29adbbac9e02bedd2f657f8244713d721
Pete Muir <pmuir@bleepbleep.org.uk>
2009-04-10 12:08:49 +0000

    Cache creation of annotated classes
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@2385 1c488680-804c-0410-94cd-c6b725194a0e

--- impl/src/main/java/org/jboss/webbeans/resources/ClassTransformer.java
+++ impl/src/main/java/org/jboss/webbeans/resources/ClassTransformer.java
@@ -1,24 +1,55 @@
 package org.jboss.webbeans.resources;
 
 import java.lang.annotation.Annotation;
+import java.util.concurrent.Callable;
 
 import org.jboss.webbeans.bootstrap.api.Service;
 import org.jboss.webbeans.introspector.AnnotatedAnnotation;
 import org.jboss.webbeans.introspector.AnnotatedClass;
 import org.jboss.webbeans.introspector.jlr.AnnotatedAnnotationImpl;
 import org.jboss.webbeans.introspector.jlr.AnnotatedClassImpl;
+import org.jboss.webbeans.util.collections.ConcurrentCache;
 
 public class ClassTransformer implements Service 
 {
    
-   public <T> AnnotatedClass<T> classForName(Class<T> clazz)
+   private final ConcurrentCache<Class<?>, AnnotatedClass<?>> classes;
+   private final ConcurrentCache<Class<?>, AnnotatedAnnotation<?>> annotations;
+   private final ClassTransformer transformer = this;
+   
+   /**
+    * 
+    */
+   public ClassTransformer()
    {
-      return AnnotatedClassImpl.of(clazz, this);
+      classes = new ConcurrentCache<Class<?>, AnnotatedClass<?>>();
+      annotations = new ConcurrentCache<Class<?>, AnnotatedAnnotation<?>>();
    }
    
-   public <T extends Annotation> AnnotatedAnnotation<T> classForName(Class<T> clazz)
+   public <T> AnnotatedClass<T> classForName(final Class<T> clazz)
    {
-      return AnnotatedAnnotationImpl.of(clazz, this);
+      return classes.putIfAbsent(clazz, new Callable<AnnotatedClass<T>>()
+      {
+
+         public AnnotatedClass<T> call() throws Exception
+         {
+            return AnnotatedClassImpl.of(clazz, transformer);
+         }
+            
+      });
+   }
+   
+   public <T extends Annotation> AnnotatedAnnotation<T> classForName(final Class<T> clazz)
+   {
+      return annotations.putIfAbsent(clazz, new Callable<AnnotatedAnnotation<T>>()
+      {
+
+         public AnnotatedAnnotation<T> call() throws Exception
+         {
+            return AnnotatedAnnotationImpl.of(clazz, transformer);
+         }
+            
+      });
    }
 
 }
