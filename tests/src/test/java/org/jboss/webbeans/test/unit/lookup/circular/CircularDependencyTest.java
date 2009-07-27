b76d63ee03cfa24eedbe5902558a7d52bdbd4749
Jozef Hartinger <jharting@redhat.com>
2009-07-27 07:33:08 +0000

    Removed tests duplicated in TCK
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3267 1c488680-804c-0410-94cd-c6b725194a0e

--- tests/src/test/java/org/jboss/webbeans/test/unit/lookup/circular/CircularDependencyTest.java
+++ tests/src/test/java/org/jboss/webbeans/test/unit/lookup/circular/CircularDependencyTest.java
@@ -8,33 +8,6 @@ import org.testng.annotations.Test;
 public class CircularDependencyTest extends AbstractWebBeansTest
 {
   
-   
-   @Test
-   public void testCircularInjectionOnTwoNormalBeans() throws Exception
-   {
-      getCurrentManager().getInstanceByType(Pig.class).getName();
-      assert Pig.success;
-      assert Food.success;
-   }
-   
-   @Test
-   public void testCircularInjectionOnOneNormalAndOneDependentBean() throws Exception
-   {
-      
-      getCurrentManager().getInstanceByType(Car.class).getName();
-      assert Petrol.success;
-      assert Car.success;
-   }
-   
-   @Test
-   public void testCircularInjectionOnOneDependentAndOneNormalBean() throws Exception
-   {
-      getCurrentManager().getInstanceByType(Petrol.class).getName();
-      assert Petrol.success;
-      assert Car.success;
-   }
-   
-   
    @Test
    public void testCircularInjectionOnTwoSimpleDependentBeans() throws Exception
    {
@@ -49,30 +22,6 @@ public class CircularDependencyTest extends AbstractWebBeansTest
       getCurrentManager().getInstanceByType(DependentSelfConsumingDependentProducer.class).ping();
    }
    
-   @Test
-   public void testNormalProducerMethodDeclaredOnNormalBeanWhichInjectsProducedBean() throws Exception
-   {
-      getCurrentManager().getInstanceByType(NormalSelfConsumingNormalProducer.class).ping();
-   }
-   
-   @Test
-   public void testNormalProducerMethodDeclaredOnDependentBeanWhichInjectsProducedBean() throws Exception
-   {
-      getCurrentManager().getInstanceByType(DependentSelfConsumingNormalProducer.class).ping();
-   }
-   
-   @Test
-   public void testDependentProducerMethodDeclaredOnNormalBeanWhichInjectsProducedBean() throws Exception
-   {
-      getCurrentManager().getInstanceByType(NormalSelfConsumingDependentProducer.class).ping();
-   }
-   
-   @Test
-   public void testNormalSelfConsumingProducer() throws Exception
-   {
-      createContextualInstance(Violation.class).ping();
-   }
-   
    @Test(groups="broken", timeOut=1000)
    public void testDependentSelfConsumingProducer() throws Exception
    {
@@ -85,28 +34,9 @@ public class CircularDependencyTest extends AbstractWebBeansTest
       getCurrentManager().getInstanceByType(Fish.class);
    }
    
-   @Test
-   public void testNormalCircularConstructors() throws Exception
-   {
-      getCurrentManager().getInstanceByType(Bird.class);
-   }
-   
-   @Test
-   public void testNormalAndDependentCircularConstructors() throws Exception
-   {      
-      getCurrentManager().getInstanceByType(Planet.class);
-   }
-   
    @Test(groups="broken", timeOut=1000)
    public void testSelfConsumingConstructorsOnDependentBean() throws Exception
    {      
       getCurrentManager().getInstanceByType(Farm.class);
    }
-   
-   @Test
-   public void testSelfConsumingConstructorsOnNormalBean() throws Exception
-   {      
-      getCurrentManager().getInstanceByType(House.class);
-   }
-   
 }
