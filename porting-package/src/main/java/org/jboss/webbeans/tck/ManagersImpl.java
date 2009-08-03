edde9f29230d052541274020f5f5117e58b399bf
David Allen <david.allen@acm.org>
2009-08-03 08:43:00 +0000

    Fixed porting package to match latest Managers interface
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3393 1c488680-804c-0410-94cd-c6b725194a0e

--- porting-package/src/main/java/org/jboss/webbeans/tck/ManagersImpl.java
+++ porting-package/src/main/java/org/jboss/webbeans/tck/ManagersImpl.java
@@ -1,9 +1,5 @@
 package org.jboss.webbeans.tck;
 
-import java.lang.annotation.Annotation;
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.enterprise.inject.UnproxyableResolutionException;
 import javax.enterprise.inject.spi.BeanManager;
 
@@ -11,7 +7,6 @@ import org.jboss.jsr299.tck.spi.Managers;
 import org.jboss.webbeans.CurrentManager;
 import org.jboss.webbeans.DefinitionException;
 import org.jboss.webbeans.DeploymentException;
-import org.jboss.webbeans.WebBean;
 
 public class ManagersImpl implements Managers
 {
@@ -19,18 +14,6 @@ public class ManagersImpl implements Managers
    public BeanManager getManager()
    {
       return CurrentManager.rootManager();
-   }
-
-   public void setEnabledDeploymentTypes(List<Class<? extends Annotation>> enabledDeploymentTypes)
-   {
-      CurrentManager.rootManager().setEnabledDeploymentTypes(enabledDeploymentTypes);
-   }
-
-   public List<Class<? extends Annotation>> getEnabledDeploymentTypes()
-   {
-      List<Class<? extends Annotation>> deploymentTypes = new ArrayList<Class<? extends Annotation>>(CurrentManager.rootManager().getEnabledDeploymentTypes());
-      deploymentTypes.remove(WebBean.class);
-      return deploymentTypes;
    }
 
    public boolean isDefinitionError(org.jboss.testharness.api.DeploymentException deploymentException)
