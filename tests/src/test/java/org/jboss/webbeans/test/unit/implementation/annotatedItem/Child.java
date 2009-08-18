50759480e741f8cb16d5adc5ace452f87218b4c2
Pete Muir <pmuir@bleepbleep.org.uk>
2009-08-18 13:18:51 +0000

    Add failing test
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3548 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ tests/src/test/java/org/jboss/webbeans/test/unit/implementation/annotatedItem/Child.java
@@ -0,0 +1,4 @@
+package org.jboss.webbeans.test.unit.implementation.annotatedItem;
+public class Child extends Parent
+   {
+   }
---
+++ tests/src/test/java/org/jboss/webbeans/test/unit/implementation/annotatedItem/Parent.java
@@ -0,0 +1,11 @@
+package org.jboss.webbeans.test.unit.implementation.annotatedItem;
+public class Parent
+{
+   int parent;
+
+   void parentMethod()
+   {
+
+   }
+}
+
