3ea1c2c0a45648dc28a5d3a0b6b14342bddb39b3
Kabir Khan <kkhan@redhat.com>
2009-09-17 09:03:41 +0000

    Add decorator test
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3690 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ tests/src/test/java/org/jboss/webbeans/test/unit/decorator/simple/SimpleDecorator2.java
@@ -0,0 +1,58 @@
+/*
+* JBoss, Home of Professional Open Source.
+* Copyright 2006, Red Hat Middleware LLC, and individual contributors
+* as indicated by the @author tags. See the copyright.txt file in the
+* distribution for a full listing of individual contributors. 
+*
+* This is free software; you can redistribute it and/or modify it
+* under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation; either version 2.1 of
+* the License, or (at your option) any later version.
+*
+* This software is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with this software; if not, write to the Free
+* Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+* 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+*/ 
+package org.jboss.webbeans.test.unit.decorator.simple;
+
+import javax.decorator.Decorates;
+import javax.decorator.Decorator;
+
+/**
+ * 
+ * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
+ * @version $Revision: 1.1 $
+ */
+@Decorator
+public class SimpleDecorator2
+{
+   @Decorates
+   SimpleBean delegate;
+   
+   public static boolean echo2;
+   public static boolean echo3;
+   
+   public static void reset()
+   {
+      echo2 = false;
+      echo3 = false;
+   }
+
+   public int echo2(int i)
+   {
+      echo2 = true;
+      return delegate.echo1(i);
+   }
+
+   public int echo3(int i)
+   {
+      echo3 = true;
+      return delegate.echo1(i);
+   }
+}
