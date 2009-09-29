6ae2fe7dc2a578ce2b8d7f04db4165a3e961ac2b
Pete Muir <pmuir@bleepbleep.org.uk>
2009-09-29 19:35:36 +0000

    WBRI-366, support for clustering, WBRI-398
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3813 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ tests/src/test/java/org/jboss/webbeans/test/unit/implementation/enterprise/lifecycle/BeanImpl.java
@@ -0,0 +1,14 @@
+package org.jboss.webbeans.test.unit.implementation.enterprise.lifecycle;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class BeanImpl implements BeanLocal
+{
+   
+   public void ping()
+   {
+      
+   }
+
+}
---
+++ tests/src/test/java/org/jboss/webbeans/test/unit/implementation/enterprise/lifecycle/BeanLocal.java
@@ -0,0 +1,8 @@
+package org.jboss.webbeans.test.unit.implementation.enterprise.lifecycle;
+
+public interface BeanLocal
+{
+   
+   public void ping();
+
+}
