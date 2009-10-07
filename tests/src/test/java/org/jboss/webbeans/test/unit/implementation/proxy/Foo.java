9752ac886f235a04b27fe4da79bbe26d4127cfbf
Pete Muir <pmuir@bleepbleep.org.uk>
2009-10-07 18:23:35 +0000

    set eol-style to native
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3868 1c488680-804c-0410-94cd-c6b725194a0e

--- tests/src/test/java/org/jboss/webbeans/test/unit/implementation/proxy/Foo.java
+++ tests/src/test/java/org/jboss/webbeans/test/unit/implementation/proxy/Foo.java
@@ -1,1 +1,18 @@
-package org.jboss.webbeans.test.unit.implementation.proxy;import java.io.Serializable;import javax.enterprise.context.RequestScoped;import javax.inject.Named;@Named@RequestScopedclass Foo implements Serializable{      public String getMsg()   {      return "Hi";   }   }
+package org.jboss.webbeans.test.unit.implementation.proxy;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+@Named
+@RequestScoped
+class Foo implements Serializable
+{
+   
+   public String getMsg()
+   {
+      return "Hi";
+   }
+   
+}
--- tests/src/test/java/org/jboss/webbeans/test/unit/lookup/circular/DependentSelfConsumingDependentProducer.java
+++ tests/src/test/java/org/jboss/webbeans/test/unit/lookup/circular/DependentSelfConsumingDependentProducer.java
@@ -1,1 +1,17 @@
-package org.jboss.webbeans.test.unit.lookup.circular;import javax.enterprise.inject.Produces;class DependentSelfConsumingDependentProducer{   @SelfConsumingDependent Violation violation;      @Produces @SelfConsumingDependent   public Violation produceViolation() {      return new Violation();   }      public void ping() {         }} 
+package org.jboss.webbeans.test.unit.lookup.circular;
+
+import javax.enterprise.inject.Produces;
+
+class DependentSelfConsumingDependentProducer
+{
+   @SelfConsumingDependent Violation violation;
+   
+   @Produces @SelfConsumingDependent
+   public Violation produceViolation() {
+      return new Violation();
+   }
+   
+   public void ping() {
+      
+   }
+} 
