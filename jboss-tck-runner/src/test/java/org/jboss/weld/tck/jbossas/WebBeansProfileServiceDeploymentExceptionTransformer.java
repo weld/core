f966c36ffe620d705ea5969a36176a58f8a13722
Shane Bryzak <sbryzak@redhat.com>
2009-10-08 04:21:46 +0000

    refactored jboss-tck-runner packages
    git-svn-id: http://anonsvn.jboss.org/repos/weld/ri/trunk@3884 1c488680-804c-0410-94cd-c6b725194a0e

--- jboss-tck-runner/src/test/java/org/jboss/weld/tck/jbossas/WebBeansProfileServiceDeploymentExceptionTransformer.java
+++ jboss-tck-runner/src/test/java/org/jboss/weld/tck/jbossas/WebBeansProfileServiceDeploymentExceptionTransformer.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.webbeans.tck.jbossas;
+package org.jboss.weld.tck.jbossas;
 
 import java.util.Map.Entry;
 
