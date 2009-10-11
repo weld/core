826159c38af9b441729496d0f81047cc4f775999
Pete Muir <pmuir@bleepbleep.org.uk>
2009-10-11 18:28:14 +0000

    move cdi api

--- cdi-api/src/main/java/javax/decorator/package-info.java
+++
@@ -1,5 +0,0 @@
-package javax.decorator;
-
-/**
- * The package javax.decorator contains annotations relating to decorators.
- */
--- cdi-api/src/main/java/javax/enterprise/event/package-info.java
+++
@@ -1,6 +0,0 @@
-package javax.enterprise.event;
-
-/**
- * The package javax.enterprise.event contains annotations and interfaces
- * relating to events.
- */
