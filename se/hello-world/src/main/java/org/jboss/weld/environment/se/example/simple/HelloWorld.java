c572ab6fac169ee9d4875127ecfdbc0bca4ec75e
Pete Royle <pete@screamingcoder.com>
2009-11-02 13:03:40 +0000

    cleaned imports

--- se/hello-world/src/main/java/org/jboss/weld/environment/se/example/simple/HelloWorld.java
+++ se/hello-world/src/main/java/org/jboss/weld/environment/se/example/simple/HelloWorld.java
@@ -18,7 +18,6 @@ package org.jboss.weld.environment.se.example.simple;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
-import javax.inject.Inject;
 import org.jboss.weld.environment.se.events.ContainerInitialized;
 import javax.inject.Inject;
 
