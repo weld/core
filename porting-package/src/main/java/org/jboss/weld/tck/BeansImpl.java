d66fc551c2095ec2bb9926de9a6a35519ce7fc22
Pete Muir <pmuir@bleepbleep.org.uk>
2009-12-02 21:56:18 +0000

    Trivial to check for a proxy without refering to core (make Porting package less dependent on core code)

--- porting-package/src/main/java/org/jboss/weld/tck/BeansImpl.java
+++ porting-package/src/main/java/org/jboss/weld/tck/BeansImpl.java
@@ -1,7 +1,6 @@
 package org.jboss.weld.tck;
 
 import org.jboss.jsr299.tck.spi.Beans;
-import org.jboss.weld.util.Proxies;
 
 /**
  * Implements the Beans SPI for the TCK specifically for the JBoss RI.
@@ -16,7 +15,7 @@ public class BeansImpl implements Beans
 
    public boolean isProxy(Object instance)
    {
-      return Proxies.isProxy(instance);
+      return instance.getClass().getName().indexOf("_$$_javassist_") > 0;
    }
 
 }
