391840f339f7ff8e83829a9d88cff581c7fa88f6
Pete Muir <pmuir@bleepbleep.org.uk>
2009-12-03 16:14:32 +0000

    Add issue

--- tests/src/test/java/org/jboss/weld/tests/beanManager/annotation/ManagerAnnotationTest.java
+++ tests/src/test/java/org/jboss/weld/tests/beanManager/annotation/ManagerAnnotationTest.java
@@ -10,37 +10,37 @@ import org.testng.annotations.Test;
 public class ManagerAnnotationTest extends AbstractWeldTest
 {
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsQualifier() throws Exception
    {
       assert !getCurrentManager().isQualifier(PersistenceContext.class);
    }
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsInterceptorBinding() throws Exception
    {
       assert !getCurrentManager().isInterceptorBinding(PersistenceContext.class);
    }
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsNormalScope() throws Exception
    {
       assert !getCurrentManager().isNormalScope(PersistenceContext.class);
    }
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsPassivatingScope() throws Exception
    {
       assert !getCurrentManager().isPassivatingScope(PersistenceContext.class);
    }
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsScope() throws Exception
    {
       assert !getCurrentManager().isScope(PersistenceContext.class);
    }
    
-   @Test
+   @Test(description="WELD-299")
    public void testIsStereotype() throws Exception
    {
       assert !getCurrentManager().isStereotype(PersistenceContext.class);
