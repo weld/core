588c4e278bac6fca8b71d8eea4fa31f5a0734053
Pete Muir <pmuir@bleepbleep.org.uk>
2010-01-12 13:25:58 +0000

    update test

--- tests/src/test/java/org/jboss/weld/tests/serialization/SerializationTest.java
+++ tests/src/test/java/org/jboss/weld/tests/serialization/SerializationTest.java
@@ -2,16 +2,10 @@ package org.jboss.weld.tests.serialization;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.lang.reflect.Field;
-
-import javax.enterprise.inject.spi.Bean;
 
 import org.jboss.testharness.impl.packaging.Artifact;
-import org.jboss.weld.conversation.AbstractConversationManager;
-import org.jboss.weld.conversation.ConversationManager;
 import org.jboss.weld.test.AbstractWeldTest;
 import org.testng.annotations.Test;
 
@@ -21,16 +15,12 @@ public class SerializationTest extends AbstractWeldTest
    @SuppressWarnings("unchecked")
    @Test(description="WELD-363", groups = "broken")
    public void testConversationManagerSerializable()
-      throws IOException, ClassNotFoundException, SecurityException, NoSuchFieldException, IllegalArgumentException, IllegalAccessException
+      throws Exception
    {
-      Bean<ConversationManager> cmBean = (Bean<ConversationManager>) getCurrentManager()
-         .getBeans(ConversationManager.class).iterator().next();
+      TestConversationManager cMgr = getCurrentManager().getInstanceByType(TestConversationManager.class);
       
-      ConversationManager cMgr = cmBean.create(getCurrentManager().createCreationalContext(cmBean));
-      
-      Field ccField = AbstractConversationManager.class.getDeclaredField("currentConversation");
-      ccField.setAccessible(true);
-      assert ccField.get(cMgr) != null;
+      assert cMgr.getConversationInstance() != null;
+      assert cMgr.getConversationInstance().get() != null;
       
       ByteArrayOutputStream serialized = new ByteArrayOutputStream();
       ObjectOutputStream out = new ObjectOutputStream(serialized);
@@ -41,7 +31,9 @@ public class SerializationTest extends AbstractWeldTest
       ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(serialized.toByteArray()));
       Object deserialized = in.readObject();
       
-      assert deserialized instanceof ConversationManager;
-      assert ccField.get(deserialized) != null;
+      assert deserialized instanceof TestConversationManager;
+      TestConversationManager deserializedCMgr = (TestConversationManager) deserialized;
+      assert deserializedCMgr.getConversationInstance() != null;
+      assert deserializedCMgr.getConversationInstance().get() != null;
    }
 }
---
+++ tests/src/test/java/org/jboss/weld/tests/serialization/TestConversationManager.java
@@ -0,0 +1,26 @@
+package org.jboss.weld.tests.serialization;
+
+import java.lang.reflect.Field;
+
+import javax.enterprise.inject.Instance;
+
+import org.jboss.weld.conversation.AbstractConversationManager;
+import org.jboss.weld.conversation.ConversationImpl;
+import org.jboss.weld.conversation.ServletConversationManager;
+
+public class TestConversationManager extends ServletConversationManager
+{
+
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 9081670661490776033L;
+   
+   public Instance<ConversationImpl> getConversationInstance() throws Exception
+   {
+      Field ccField = AbstractConversationManager.class.getDeclaredField("currentConversation");
+      ccField.setAccessible(true);
+      return (Instance<ConversationImpl>) ccField.get(this);
+   }
+
+}
