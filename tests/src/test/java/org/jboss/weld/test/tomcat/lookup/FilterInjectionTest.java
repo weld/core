51f06284269484850a99dd843e7f826597bff698
Matija Mazi <matija.mazi@gmail.com>
2010-04-13 15:10:51 +0000

    WELDX-83: Tests for injection into Filters and (some) Listeners on Tomcat standalone
    git-svn-id: http://anonsvn.jboss.org/repos/weld/servlet/trunk@6103 1c488680-804c-0410-94cd-c6b725194a0e

---
+++ tests/src/test/java/org/jboss/weld/test/tomcat/lookup/FilterInjectionTest.java
@@ -0,0 +1,52 @@
+package org.jboss.weld.test.tomcat.lookup;
+
+
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.jboss.testharness.impl.packaging.Artifact;
+import org.jboss.testharness.impl.packaging.Classes;
+import org.jboss.testharness.impl.packaging.IntegrationTest;
+import org.jboss.testharness.impl.packaging.Resource;
+import org.jboss.testharness.impl.packaging.Resources;
+import org.jboss.weld.test.AbstractWeldTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Artifact(addCurrentPackage=false)
+@IntegrationTest(runLocally=true)
+@Resources({
+   @Resource(source="context-servlet-injection.xml", destination="/META-INF/context.xml"),
+   @Resource(source="web-filter-injection.xml", destination="/WEB-INF/web.xml")
+})
+@Classes({
+   CatFilter.class,
+   Sewer.class
+})
+public class FilterInjectionTest extends AbstractWeldTest
+{
+
+   private static final Logger log = LoggerFactory.getLogger(FilterInjectionTest.class);
+
+   @Override
+   @BeforeClass
+   public void beforeClass() throws Throwable
+   {
+      log.info("To run the servlet filter injection test you must add tomcat-support.jar to $CATALINA_BASE/lib");
+      super.beforeClass();
+   }
+
+
+   @Test
+   public void testFilterInjection() throws Exception
+   {
+      HttpClient client = new HttpClient();
+      HttpMethod method = new GetMethod(getContextPath() + "/cat");
+      assert client.executeMethod(method) == HttpServletResponse.SC_OK;
+   }
+}
---
+++ tests/src/test/java/org/jboss/weld/test/tomcat/lookup/ListenerInjectionTest.java
@@ -0,0 +1,57 @@
+package org.jboss.weld.test.tomcat.lookup;
+
+
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.jboss.testharness.impl.packaging.Artifact;
+import org.jboss.testharness.impl.packaging.Classes;
+import org.jboss.testharness.impl.packaging.IntegrationTest;
+import org.jboss.testharness.impl.packaging.Resource;
+import org.jboss.testharness.impl.packaging.Resources;
+import org.jboss.weld.test.AbstractWeldTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+@Artifact(addCurrentPackage=false)
+@IntegrationTest(runLocally=true)
+@Resources({
+   @Resource(source="context-servlet-injection.xml", destination="/META-INF/context.xml"),
+   @Resource(source="web-listener-injection.xml", destination="/WEB-INF/web.xml")
+})
+@Classes({
+   BatListener.class,
+   BatServlet.class,
+   Sewer.class
+})
+public class ListenerInjectionTest extends AbstractWeldTest
+{
+
+   private static final Logger log = LoggerFactory.getLogger(ListenerInjectionTest.class);
+
+   @Override
+   @BeforeClass
+   public void beforeClass() throws Throwable
+   {
+      log.info("To run the servlet listener injection test you must add tomcat-support.jar to $CATALINA_BASE/lib");
+      super.beforeClass();
+   }
+
+
+   // This test currently fails showing that injection into Listeners doesn't
+   // work in Tomcat standalone.
+//   @Test
+   public void testListenerInjection() throws Exception
+   {
+      HttpClient client = new HttpClient();
+      HttpMethod method = new GetMethod(getContextPath() + "/bat");
+      int sc = client.executeMethod(method);
+      System.out.println("sc = " + sc);
+      assert sc == HttpServletResponse.SC_OK;
+   }
+}
