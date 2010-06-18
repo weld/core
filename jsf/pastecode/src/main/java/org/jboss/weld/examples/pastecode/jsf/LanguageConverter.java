7e8c38ef449fc10cebcb1d11e1b14a65ad51c37b
Pete Muir <pmuir@bleepbleep.org.uk>
2010-06-18 13:27:09 +0000

    use jsf 2 :-)

--- jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/jsf/LanguageConverter.java
+++ jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/jsf/LanguageConverter.java
@@ -1,9 +1,11 @@
 package org.jboss.weld.examples.pastecode.jsf;
 
 import javax.faces.convert.EnumConverter;
+import javax.faces.convert.FacesConverter;
 
 import org.jboss.weld.examples.pastecode.model.Language;
 
+@FacesConverter(forClass=Language.class)
 public class LanguageConverter extends EnumConverter
 {
    public LanguageConverter()
--- jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/jsf/TruncatingConverter.java
+++ jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/jsf/TruncatingConverter.java
@@ -3,7 +3,9 @@ package org.jboss.weld.examples.pastecode.jsf;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
+import javax.faces.convert.FacesConverter;
 
+@FacesConverter("truncatingConverter")
 public class TruncatingConverter implements Converter
 {
 
