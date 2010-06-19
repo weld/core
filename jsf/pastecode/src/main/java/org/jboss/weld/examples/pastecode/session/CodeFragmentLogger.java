c4b1f2ca1d98e3b97b5c16038b6325472b1e74d2
Pete Muir <pmuir@bleepbleep.org.uk>
2010-06-19 17:40:49 +0000

    Add declarative concurrency

--- jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/session/CodeFragmentLogger.java
+++ jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/session/CodeFragmentLogger.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import javax.ejb.Lock;
+import javax.ejb.LockType;
+import javax.ejb.Singleton;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 
 import org.jboss.weld.examples.pastecode.model.CodeFragment;
 
 @ApplicationScoped
-//@Singleton
+@Singleton
 public class CodeFragmentLogger
 {
    
@@ -21,19 +24,19 @@ public class CodeFragmentLogger
       this.log = new ArrayList<CodeFragment>();
    }
 
-   //@Lock(LockType.READ)
+   @Lock(LockType.READ)
    public List<CodeFragment> getLog()
    {
       return Collections.unmodifiableList(log);
    }
 
-   //@Lock(LockType.WRITE)
+   @Lock(LockType.WRITE)
    public void clearLog()
    {
       this.log.clear();
    }
 
-   //@Lock(LockType.WRITE)
+   @Lock(LockType.WRITE)
    public void addEntry(@Observes CodeFragment codeFragment)
    {
       this.log.add(codeFragment);
