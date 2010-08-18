69eff52883f846aef0fc929b12536d1d7f0a3747
Pete Muir <pmuir@bleepbleep.org.uk>
2010-08-18 16:38:27 +0100

    WELD-604

---
+++ environments/servlet/tests/base/src/main/java/org/jboss/weld/environment/servlet/test/el/Chicken.java
@@ -0,0 +1,14 @@
+package org.jboss.weld.environment.servlet.test.el;
+
+import javax.inject.Named;
+
+@Named
+public class Chicken
+{
+   
+   public String getName()
+   {
+      return "Charlie";
+   }
+
+}
---
+++ environments/servlet/tests/tomcat/src/test/java/org/jboss/weld/environment/servlet/test/el/JsfTest.java
@@ -0,0 +1,32 @@
+package org.jboss.weld.environment.servlet.test.el;
+
+import static org.jboss.arquillian.api.RunModeType.AS_CLIENT;
+import static org.jboss.weld.environment.servlet.test.util.TomcatDeployments.CONTEXT_XML;
+
+import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.api.Run;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+
+@Run(AS_CLIENT)
+@RunWith(Arquillian.class)
+@Ignore
+// This test runs fine in the IDE, but I can't get UEL 2.2 to run on the classpath
+public class JsfTest extends JsfTestBase
+{
+
+   @Deployment
+   public static WebArchive deployment()
+   {
+      return JsfTestBase.deployment().add(CONTEXT_XML, "META-INF/context.xml");
+   }
+   
+   @Override
+   protected String getPath(String page)
+   {
+      return "http://localhost:8888/test/" + page;
+   }
+   
+}
