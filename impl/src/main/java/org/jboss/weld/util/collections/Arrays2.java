3d2ea71027464de1f86663a353fa770a8e54d54b
Pete Muir <pmuir@bleepbleep.org.uk>
2010-11-21 20:33:52 +0000

    improve mem usage Conflicts:
    	impl/src/main/java/org/jboss/weld/util/collections/Arrays2.java

--- impl/src/main/java/org/jboss/weld/util/collections/Arrays2.java
+++ impl/src/main/java/org/jboss/weld/util/collections/Arrays2.java
@@ -38,7 +38,7 @@ public class Arrays2 {
     }
 
     public static <T> Set<T> asSet(T... array) {
-        Set<T> result = new HashSet<T>();
+        Set<T> result = new HashSet<T>(array.length);
         for (T a : array) {
             result.add(a);
         }
@@ -46,29 +46,28 @@ public class Arrays2 {
     }
 
     // Cloning
-
     /**
-     * Copies the specified array, truncating or padding with nulls (if necessary)
-     * so the copy has the specified length.  For all indices that are
-     * valid in both the original array and the copy, the two arrays will
-     * contain identical values.  For any indices that are valid in the
-     * copy but not the original, the copy will contain <tt>null</tt>.
-     * Such indices will exist if and only if the specified length
-     * is greater than that of the original array.
-     * The resulting array is of exactly the same class as the original array.
-     *
-     * @param original  the array to be copied
-     * @param newLength the length of the copy to be returned
-     * @return a copy of the original array, truncated or padded with nulls
-     *         to obtain the specified length
-     * @throws NegativeArraySizeException if <tt>newLength</tt> is negative
-     * @throws NullPointerException       if <tt>original</tt> is null
-     * @since 1.6
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> T[] copyOf(T[] original, int newLength) {
-        return (T[]) copyOf(original, newLength, original.getClass());
-    }
+    * Copies the specified array, truncating or padding with nulls (if necessary)
+    * so the copy has the specified length.  For all indices that are
+    * valid in both the original array and the copy, the two arrays will
+    * contain identical values.  For any indices that are valid in the
+    * copy but not the original, the copy will contain <tt>null</tt>.
+    * Such indices will exist if and only if the specified length
+    * is greater than that of the original array.
+    * The resulting array is of exactly the same class as the original array.
+    *
+    * @param original the array to be copied
+    * @param newLength the length of the copy to be returned
+    * @return a copy of the original array, truncated or padded with nulls
+    *     to obtain the specified length
+    * @throws NegativeArraySizeException if <tt>newLength</tt> is negative
+    * @throws NullPointerException if <tt>original</tt> is null
+    * @since 1.6
+    */
+   @SuppressWarnings("unchecked")
+   public static <T> T[] copyOf(T[] original, int newLength) {
+       return (T[]) copyOf(original, newLength, original.getClass());
+   }
 
     /**
      * Copies the specified array, truncating or padding with nulls (if necessary)
