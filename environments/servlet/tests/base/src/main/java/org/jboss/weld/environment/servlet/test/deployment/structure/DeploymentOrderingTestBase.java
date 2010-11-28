1114297e685ba485b3a0959a792401b8540fb01b
Pete Muir <pmuir@bleepbleep.org.uk>
2010-11-28 23:22:27 +0000

    Test for WELD-719

--- environments/servlet/tests/base/src/main/java/org/jboss/weld/environment/servlet/test/deployment/structure/DeploymentOrderingTestBase.java
+++ environments/servlet/tests/base/src/main/java/org/jboss/weld/environment/servlet/test/deployment/structure/DeploymentOrderingTestBase.java
@@ -2,9 +2,17 @@ package org.jboss.weld.environment.servlet.test.deployment.structure;
 
 import static org.jboss.weld.environment.servlet.test.util.Deployments.baseDeployment;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.inject.spi.AnnotatedType;
 import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.Extension;
 
+import org.jboss.shrinkwrap.api.asset.Asset;
+import org.jboss.shrinkwrap.api.asset.ByteArrayAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.weld.environment.servlet.test.util.BeansXml;
 import org.junit.Test;
@@ -12,11 +20,12 @@ import org.junit.Test;
 public class DeploymentOrderingTestBase
 {
    
+   public static final Asset EXTENSION = new ByteArrayAsset(ContainerLifecycleObserver.class.getName().getBytes());
+   
    
    public static WebArchive deployment()
    {
-      WebArchive war = baseDeployment().addPackage(DeploymentOrderingTestBase.class.getPackage()).addWebResource(new BeansXml().alternatives(Bar.class), "beans.xml").addWebResource(new BeansXml().alternatives(Garply.class), "classes/META-INF/beans.xml");
-      System.out.println(war.toString(true));
+      WebArchive war = baseDeployment().addPackage(DeploymentOrderingTestBase.class.getPackage()).addWebResource(new BeansXml().alternatives(Bar.class), "beans.xml").addWebResource(new BeansXml().alternatives(Garply.class), "classes/META-INF/beans.xml").addManifestResource(EXTENSION, "services/" + Extension.class.getName());
       return war;
    }
    
@@ -25,6 +34,21 @@ public class DeploymentOrderingTestBase
    {
       assertEquals(Bar.class, beanManager.resolve(beanManager.getBeans(Foo.class)).getBeanClass());
       assertEquals(Garply.class, beanManager.resolve(beanManager.getBeans(Baz.class)).getBeanClass());
+   }
+   
+   @Test
+   public void testProcessAnnotatedTypeCalledOnceOnlyPerType(ContainerLifecycleObserver containerLifecycleObserver)
+   {
+      assertEquals(4, containerLifecycleObserver.getProcessedAnnotatedTypes().size());
+      List<Class<?>> classes = new ArrayList<Class<?>>();
+      for (AnnotatedType<?> annotatedType : containerLifecycleObserver.getProcessedAnnotatedTypes())
+      {
+         classes.add(annotatedType.getJavaClass());
+      }
+      assertTrue(classes.contains(Foo.class));
+      assertTrue(classes.contains(Bar.class));
+      assertTrue(classes.contains(Baz.class));
+      assertTrue(classes.contains(Garply.class));
    }
    
 }
