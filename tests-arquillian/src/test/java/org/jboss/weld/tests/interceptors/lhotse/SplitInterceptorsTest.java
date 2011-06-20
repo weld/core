8c6bc7a806be10d890e805e0e47b32751d98eb29
Ales Justin <ales.justin@gmail.com>
2011-06-20 10:16:40 -0700

    More ARQ fixes.

--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/lhotse/SplitInterceptorsTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/lhotse/SplitInterceptorsTest.java
@@ -23,7 +23,7 @@
 
 package org.jboss.weld.tests.interceptors.lhotse;
 
-import org.jboss.arquillian.api.Deployment;
+import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.BeanArchive;
