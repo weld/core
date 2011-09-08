10dea4e0372ea3e3694fea71f6e2354aed951f58
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-09-08 16:13:48 +0200

    Code style for API bundle

---
+++ environments/osgi/core/api/src/main/java/org/jboss/weld/environment/osgi/api/annotation/OSGiBundle.java
@@ -0,0 +1,74 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.osgi.api.annotation;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+/**
+ * <p>This annotation qualifies an injection point that represents a bundle or a
+ * bundle relative object.</p>
+ * <p>It allows to specify:
+ * <ul> <li> <p>The symbolic name of the bundle, as a required value,</p> </li>
+ * <li> <p>The version of the bundle, as an optional value.</p> </li> </ul>
+ * </p> <p>The symbolic name and version are <b>not</b> actually qualifying the
+ * injection point, thus this annotation is for global bundle injection point
+ * with additional data.
+ * In order to actually discriminate on the symbolic name or version see
+ * {@link BundleName} and {@link BundleVersion} annotations.</p>
+ *
+ * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ * @author Matthieu CLOCHARD - SERLI (matthieu.clochard@serli.com)
+ * @see Qualifier
+ * @see org.osgi.framework.Bundle
+ * @see BundleName
+ * @see BundleVersion
+ */
+@Target(
+{
+   METHOD, PARAMETER, FIELD
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Qualifier
+public @interface OSGiBundle
+{
+   /**
+    * The symbolic name of the bundle. Nondiscriminatory value for the typesafe
+    * resolution algorithm.
+    *
+    * @return the symbolic name of the bundle.
+    */
+   @Nonbinding
+   String value();
+
+   /**
+    * The version of the bundle. Nondiscriminatory value for the typesafe
+    * resolution algorithm.
+    *
+    * @return the version of the bundle.
+    */
+   @Nonbinding
+   String version() default "";
+
+}
---
+++ environments/osgi/core/api/src/main/java/org/jboss/weld/environment/osgi/api/annotation/Property.java
@@ -0,0 +1,67 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.osgi.api.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * <p>This annotation wraps an OSGi service property used for automatic OSGi service
+ * publishing.</p>
+ * <p>It allows to specify:<ul>
+ * <li>
+ * <p>The name of the property, as a required
+ * <code>String</code>,</p>
+ * </li>
+ * <li>
+ * <p>The value of the property, as a required
+ * {@link String}.</p>
+ * </li>
+ * </ul></p>
+ * <p>It may be used within the {@link Publish} annotation to provide the
+ * published service implementation properties.</p>
+ *
+ * @author Mathieu ANCELIN - SERLI (mathieu.ancelin@serli.com)
+ * @author Matthieu CLOCHARD - SERLI (matthieu.clochard@serli.com)
+ * @see Publish
+ * @see org.jboss.weld.environment.osgi.api.Service
+ * @see org.jboss.weld.environment.osgi.api.ServiceRegistry
+ */
+@Target(
+{
+})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Property
+{
+   /**
+    * The property name.
+    *
+    * @return the property name.
+    */
+   String name();
+
+   /**
+    * The property value.
+    *
+    * @return the property value.
+    */
+   String value();
+
+}
