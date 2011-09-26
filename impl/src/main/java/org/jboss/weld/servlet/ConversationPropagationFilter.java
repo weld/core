55c27fbf69a0ae96b47f29af0a7e73e4b1b7cf51
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-09-26 19:59:30 +0200

    Remove useless import

--- impl/src/main/java/org/jboss/weld/servlet/ConversationPropagationFilter.java
+++ impl/src/main/java/org/jboss/weld/servlet/ConversationPropagationFilter.java
@@ -36,7 +36,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletResponseWrapper;
 import java.io.IOException;
-import java.util.Enumeration;
 
 /**
  * <p>
