6e16d713fdb77885d433c085842e1d961375a427
Mathieu ANCELIN <mathieu.ancelin@gmail.com>
2011-10-06 15:42:16 +0200

    Enhance hotel sample.

--- environments/osgi/examples/web/web-england/src/main/java/org/jboss/weld/osgi/examples/web/england/EnglandHotelProvider.java
+++ environments/osgi/examples/web/web-england/src/main/java/org/jboss/weld/osgi/examples/web/england/EnglandHotelProvider.java
@@ -14,11 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.weld.osgi.examples.web.england;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import javax.enterprise.context.ApplicationScoped;
 import org.jboss.weld.environment.osgi.api.annotation.Publish;
 import org.jboss.weld.osgi.examples.web.api.Hotel;
@@ -31,13 +31,20 @@ public class EnglandHotelProvider implements HotelProvider {
     @Override
     public Collection<Hotel> hotels() {
         Collection<Hotel> hotels = new ArrayList<Hotel>();
-        hotels.add(new Hotel("The Montcalm", "London", "England"));
-        hotels.add(new Hotel("The Berkeley", "London", "England"));
+        hotels.add(new Hotel("The Montcalm", "London", "England", "2222", new Double(100)));
+        hotels.add(new Hotel("The Berkeley", "London", "England", "2222", new Double(200)));
         return hotels;
     }
 
     @Override
     public String getCountry() {
         return "England";
+    }
+
+    @Override
+    public boolean book(String id, Date checkin, Date checkout, Integer beds,
+            Boolean smocking, String cardNumber, String cardName,
+            String cardMonth, String cardYear) {
+        return true;
     }
 }
--- environments/osgi/examples/web/web-france/src/main/java/org/jboss/weld/osgi/examples/web/france/FranceHotelProvider.java
+++ environments/osgi/examples/web/web-france/src/main/java/org/jboss/weld/osgi/examples/web/france/FranceHotelProvider.java
@@ -14,11 +14,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.jboss.weld.osgi.examples.web.france;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import javax.enterprise.context.ApplicationScoped;
 import org.jboss.weld.environment.osgi.api.annotation.Publish;
 import org.jboss.weld.osgi.examples.web.api.Hotel;
@@ -31,14 +31,21 @@ public class FranceHotelProvider implements HotelProvider {
     @Override
     public Collection<Hotel> hotels() {
         Collection<Hotel> hotels = new ArrayList<Hotel>();
-        hotels.add(new Hotel("Au bon Hotel", "Paris", "France"));
-        hotels.add(new Hotel("Hotel California", "Paris", "France"));
-        hotels.add(new Hotel("Hotel Claridge", "Paris", "France"));
+        hotels.add(new Hotel("Au bon Hotel", "Paris", "France", "2222", new Double(100)));
+        hotels.add(new Hotel("Hotel California", "Paris", "France", "2222", new Double(200)));
+        hotels.add(new Hotel("Hotel Claridge", "Paris", "France", "2222", new Double(400)));
         return hotels;
     }
 
     @Override
     public String getCountry() {
         return "France";
+    }
+
+    @Override
+    public boolean book(String id, Date checkin, Date checkout, Integer beds,
+            Boolean smocking, String cardNumber, String cardName,
+            String cardMonth, String cardYear) {
+        return true;
     }
 }
