65f4139abbbff503ab6db97ba167f523fddcc571
Matthieu Clochard <clochard.matthieu@gmail.com>
2011-10-11 16:07:37 +0200

    Mall container examples similar

--- environments/osgi/run-equinox.sh
+++ environments/osgi/run-equinox.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd ./examples/container-equinox/target/weld-osgi-container-equinox-1.1.3-SNAPSHOT-all/weld-osgi-container-equinox-1.1.3-SNAPSHOT/
-java -jar equinox.jar -console
+java -jar equinox.jar
