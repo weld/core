e4333d86298f2bc59b593909aeb049f8b62de09f
Ales Justin <ales.justin@gmail.com>
2011-10-24 22:23:13 +0200

    Enable jsf test now.

--- environments/servlet/tests/jetty7/src/test/java/org/jboss/weld/environment/servlet/test/el/JsfTest.java
+++ environments/servlet/tests/jetty7/src/test/java/org/jboss/weld/environment/servlet/test/el/JsfTest.java
@@ -25,7 +25,6 @@ package org.jboss.weld.environment.servlet.test.el;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 import static org.jboss.weld.environment.servlet.test.util.JettyDeployments.JETTY_ENV;
@@ -34,7 +33,6 @@ import static org.jboss.weld.environment.servlet.test.util.JettyDeployments.JETT
  * @author Ales Justin
  */
 @RunWith(Arquillian.class)
-@Ignore("Fix Jetty Plus Configuration in Arquillian")
 public class JsfTest extends JsfTestBase {
     @Deployment(testable = false)
     public static WebArchive deployment() {
