06878b414f8c3c4c9b64b99a1ac486c78a1443a5
Ales Justin <ales.justin@gmail.com>
2011-10-24 17:01:25 +0200

    Add weld:exclude tests for servlet env.

---
+++ environments/servlet/tests/jetty/src/test/java/org/jboss/weld/environment/servlet/test/config/ConfigTest.java
@@ -0,0 +1,44 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.weld.environment.servlet.test.config;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.weld.environment.servlet.test.util.JettyDeployments;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class ConfigTest extends ConfigTestBase {
+
+    @Deployment
+    public static WebArchive getDeployment() {
+        WebArchive war = ConfigTestBase.baseDOSDeployment();
+        war.addAsWebInfResource(JettyDeployments.JETTY_WEB, "jetty-web.xml");
+        war.addAsWebInfResource(JettyDeployments.JETTY_ENV, "jetty-env.xml");
+        return war;
+    }
+}
--- impl/src/main/java/org/jboss/weld/context/ejb/EjbRequestContextImpl.java
+++ impl/src/main/java/org/jboss/weld/context/ejb/EjbRequestContextImpl.java
@@ -4,7 +4,6 @@ import org.jboss.weld.context.AbstractBoundContext;
 import org.jboss.weld.context.beanstore.NamingScheme;
 import org.jboss.weld.context.beanstore.SimpleNamingScheme;
 import org.jboss.weld.context.beanstore.ejb.InvocationContextBeanStore;
-import org.jboss.weld.context.http.HttpSessionContextImpl;
 
 import javax.enterprise.context.RequestScoped;
 import javax.interceptor.InvocationContext;
@@ -12,7 +11,7 @@ import java.lang.annotation.Annotation;
 
 public class EjbRequestContextImpl extends AbstractBoundContext<InvocationContext> implements EjbRequestContext {
 
-    private static final String IDENTIFIER = HttpSessionContextImpl.class.getName();
+    private static final String IDENTIFIER = EjbRequestContextImpl.class.getName();
 
     private final NamingScheme namingScheme;
 
