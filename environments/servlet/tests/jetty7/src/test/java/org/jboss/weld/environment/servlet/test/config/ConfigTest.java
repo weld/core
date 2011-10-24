00f84faa6121596b3dd61042da3638c6bb4db9e3
Ales Justin <ales.justin@gmail.com>
2011-10-24 19:19:04 +0200

    Fix Jetty7.2 tests -- needs Jetty-Plus + fix EnvEntry name.

--- environments/servlet/tests/jetty7/src/test/java/org/jboss/weld/environment/servlet/test/config/ConfigTest.java
+++ environments/servlet/tests/jetty7/src/test/java/org/jboss/weld/environment/servlet/test/config/ConfigTest.java
@@ -26,7 +26,6 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.weld.environment.servlet.test.util.JettyDeployments;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -42,8 +41,4 @@ public class ConfigTest extends ConfigTestBase {
         return war;
     }
 
-    @Test
-    public void testDOS() throws Exception {
-        // TODO -- fix this
-    }
 }
