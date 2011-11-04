dbb8451b8797a84be05b60dd281d3ceaab77e2e9
Ales Justin <ales.justin@gmail.com>
2011-11-04 13:19:00 +0100

    [WELD-1001]; add missing abstract methods to decorator proxy

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/decorators/weld1001/DecoratorTest.java
@@ -0,0 +1,56 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2011, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.jboss.weld.tests.decorators.weld1001;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class DecoratorTest {
+
+    @Deployment(name = "decorator")
+    public static Archive getDeployment() {
+        return ShrinkWrap.create(BeanArchive.class)
+                .decorate(LargeAmountAccount.class)
+                .addPackage(LargeAmountAccount.class.getPackage());
+    }
+
+    @Test
+    @OperateOnDeployment("decorator")
+    public void testInterceptors(BusinessObject bo) throws Exception {
+        System.out.println("state = " + bo.getState());
+        bo.deposit(2011.0);
+        bo.witdraw(1509.0);
+        System.out.println("sum = " + bo.getState());
+    }
+
+}
