6378d98a2501b595ef7850ed3a23520f7cc52619
Ales Justin <ales.justin@gmail.com>
2011-11-10 16:32:17 +0100

    Fix broken tests.

--- tests-arquillian/src/test/java/org/jboss/weld/tests/beanDeployment/session/multiple/Panther.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/beanDeployment/session/multiple/Panther.java
@@ -20,7 +20,7 @@ import javax.ejb.Remove;
 import javax.ejb.Stateful;
 
 @Stateful
-class Panther implements PantherLocal {
+public class Panther implements PantherLocal {
 
     @Remove
     public void remove(String foo) {
