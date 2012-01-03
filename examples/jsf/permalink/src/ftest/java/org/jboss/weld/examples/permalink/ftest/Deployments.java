3c10f49ffd85d120a8d3c18bbee5d2c1af232f82
Marek Schmidt <maschmid@redhat.com>
2012-01-03 11:59:39 +0100

    WELD-1043 Convert example ftests to Ajocado

---
+++ examples/jsf/permalink/src/ftest/java/org/jboss/weld/examples/permalink/ftest/Deployments.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.weld.examples.permalink.ftest;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.importer.ZipImporter;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+/**
+ * Creates a deployment from a build Web Archive using ShrinkWrap ZipImporter
+ * @author maschmid
+ *
+ */
+public class Deployments {
+    private static final String ARCHIVE_NAME = "weld-permalink.war";
+    private static final String BUILD_DIRECTORY = "target";
+
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(ZipImporter.class, ARCHIVE_NAME).importFrom(new File(BUILD_DIRECTORY + '/' + ARCHIVE_NAME))
+        .as(WebArchive.class);
+    }
+}
