fc72fe93ffb70779f621ad3d52398ec61bdf8cd2
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-02-08 16:29:00 +1100

    Move jboss-interceptors classes into weld core

---
+++ impl/src/main/java/org/jboss/interceptor/proxy/InterceptorInvocationContext.java
@@ -0,0 +1,226 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.interceptor.proxy;
+
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.interceptor.InvocationContext;
+
+import org.jboss.interceptor.spi.context.InterceptionChain;
+
+/**
+ * @author <a href="mailto:mariusb@redhat.com">Marius Bogoevici</a>
+ */
+public class InterceptorInvocationContext implements InvocationContext {
+
+    private Map<String, Object> contextData = new HashMap<String, Object>();
+
+    private Method method;
+
+    private Object[] parameters;
+
+    private Object target;
+
+    private InterceptionChain interceptionChain;
+
+    private Object timer;
+    private static Map<Class<?>, Set<Class<?>>> WIDENING_TABLE;
+
+    private static Map<Class<?>, Class<?>> WRAPPER_CLASSES;
+    private static Map<Class<?>, Class<?>> REVERSE_WRAPPER_CLASSES;
+
+    static {
+        WRAPPER_CLASSES = new HashMap<Class<?>, Class<?>>();
+        WRAPPER_CLASSES.put(boolean.class, Boolean.class);
+        WRAPPER_CLASSES.put(byte.class, Byte.class);
+        WRAPPER_CLASSES.put(char.class, Character.class);
+        WRAPPER_CLASSES.put(short.class, Short.class);
+        WRAPPER_CLASSES.put(int.class, Integer.class);
+        WRAPPER_CLASSES.put(long.class, Long.class);
+        WRAPPER_CLASSES.put(float.class, Float.class);
+        WRAPPER_CLASSES.put(double.class, Double.class);
+
+        WRAPPER_CLASSES = Collections.unmodifiableMap(WRAPPER_CLASSES);
+
+        REVERSE_WRAPPER_CLASSES = new HashMap<Class<?>, Class<?>>();
+        for (Map.Entry<Class<?>, Class<?>> classEntry : WRAPPER_CLASSES.entrySet()) {
+            REVERSE_WRAPPER_CLASSES.put(classEntry.getValue(), classEntry.getKey());
+        }
+
+        REVERSE_WRAPPER_CLASSES = Collections.unmodifiableMap(REVERSE_WRAPPER_CLASSES);
+
+        WIDENING_TABLE = new HashMap<Class<?>, Set<Class<?>>>();
+        WIDENING_TABLE.put(byte.class, setOf(short.class, int.class, long.class, float.class, double.class));
+        WIDENING_TABLE.put(short.class, setOf(int.class, long.class, float.class, double.class));
+        WIDENING_TABLE.put(char.class, setOf(int.class, long.class, float.class, double.class));
+        WIDENING_TABLE.put(int.class, setOf(long.class, float.class, double.class));
+        WIDENING_TABLE.put(long.class, setOf(float.class, double.class));
+        WIDENING_TABLE.put(float.class, Collections.<Class<?>>singleton(double.class));
+
+    }
+
+    private static Set<Class<?>> setOf(Class<?>... classes) {
+        return new HashSet<Class<?>>(Arrays.asList(classes));
+    }
+
+    public InterceptorInvocationContext(InterceptionChain interceptionChain, Object target, Method targetMethod, Object[] parameters) {
+        this.interceptionChain = interceptionChain;
+        this.method = targetMethod;
+        this.parameters = parameters;
+        this.target = target;
+        this.timer = null;
+    }
+
+    public InterceptorInvocationContext(InterceptionChain interceptionChain, Object target, Method targetMethod, Object timer) {
+        this.interceptionChain = interceptionChain;
+        this.method = targetMethod;
+        this.timer = timer;
+        this.target = target;
+        this.parameters = null;
+    }
+
+    public Map<String, Object> getContextData() {
+        return contextData;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Object[] getParameters() {
+        if (this.method != null) {
+            return parameters;
+        } else {
+            throw new IllegalStateException("Illegal invocation to getParameters() during lifecycle invocation");
+        }
+    }
+
+    public Object getTarget() {
+        return target;
+    }
+
+    public Object proceed() throws Exception {
+        try {
+            return interceptionChain.invokeNextInterceptor(this);
+        } catch (Exception e) {
+            throw e;
+        } catch (Throwable t) {
+            throw new InterceptorException(t);
+        }
+    }
+
+    /**
+     * Checks that the targetClass is widening the argument class
+     *
+     * @param argumentClass
+     * @param targetClass
+     * @return
+     */
+    private static boolean isWideningPrimitive(Class argumentClass, Class targetClass) {
+        return WIDENING_TABLE.containsKey(argumentClass) && WIDENING_TABLE.get(argumentClass).contains(targetClass);
+    }
+
+    private static Class<?> getWrapperClass(Class<?> primitiveClass) {
+        if (!WRAPPER_CLASSES.containsKey(primitiveClass)) {
+            return primitiveClass;
+        } else {
+            return WRAPPER_CLASSES.get(primitiveClass);
+        }
+    }
+
+    private static Class<?> getPrimitiveClass(Class<?> wrapperClass) {
+        if (!REVERSE_WRAPPER_CLASSES.containsKey(wrapperClass)) {
+            return wrapperClass;
+        } else {
+            return REVERSE_WRAPPER_CLASSES.get(wrapperClass);
+        }
+    }
+
+    public void setParameters(Object[] params) {
+        if (method != null) {
+            // there is no requirement to do anything if params is null
+            // but this is theoretically possible only if the target method has no arguments
+            int newParametersCount = params == null ? 0 : params.length;
+            if (method.getParameterTypes().length != newParametersCount)
+                throw new IllegalArgumentException("Wrong number of parameters: method has " + method.getParameterTypes().length
+                        + ", attempting to set " + newParametersCount + (params != null ? "" : " (argument was null)"));
+            if (params != null) {
+                for (int i = 0; i < params.length; i++) {
+                    Class<?> methodParameterClass = method.getParameterTypes()[i];
+                    if (params[i] != null) {
+                        //identity ok
+                        Class<? extends Object> newArgumentClass = params[i].getClass();
+                        if (newArgumentClass.equals(methodParameterClass)) {
+                            break;
+                        }
+                        if (newArgumentClass.isPrimitive()) {
+                            // argument is primitive - never actually a case for interceptors
+                            if (methodParameterClass.isPrimitive()) {
+                                //widening primitive
+                                if (!isWideningPrimitive(newArgumentClass, methodParameterClass)) {
+                                    throw new IllegalArgumentException("Incompatible parameter type on position: " + i + " :" + newArgumentClass + " (expected type was " + methodParameterClass.getName() + ")");
+                                }
+                            } else {
+                                //boxing+widening reference
+                                Class<?> boxedArgumentClass = getWrapperClass(newArgumentClass);
+                                if (!methodParameterClass.isAssignableFrom(boxedArgumentClass)) {
+                                    throw new IllegalArgumentException("Incompatible parameter type on position: " + i + " :" + newArgumentClass + " (expected type was " + methodParameterClass.getName() + ")");
+                                }
+                            }
+                        } else {
+                            // argument is non-primitive
+                            if (methodParameterClass.isPrimitive()) {
+                                // unboxing+widening primitive
+                                Class<?> unboxedClass = getPrimitiveClass(newArgumentClass);
+
+                                if (!unboxedClass.equals(methodParameterClass) && !isWideningPrimitive(unboxedClass, methodParameterClass)) {
+                                    throw new IllegalArgumentException("Incompatible parameter type on position: " + i + " :" + newArgumentClass + " (expected type was " + methodParameterClass.getName() + ")");
+                                }
+                            } else {
+                                //widening reference
+                                if (!methodParameterClass.isAssignableFrom(newArgumentClass)) {
+                                    throw new IllegalArgumentException("Incompatible parameter type on position: " + i + " :" + newArgumentClass + " (expected type was " + methodParameterClass.getName() + ")");
+                                }
+                            }
+                        }
+                    } else {
+                        // null is never acceptable on a primitive type
+                        if (method.getParameterTypes()[i].isPrimitive()) {
+                            throw new IllegalArgumentException("Trying to set a null value on a " + method.getParameterTypes()[i].getName());
+                        }
+                    }
+                }
+                this.parameters = params;
+            }
+        } else {
+            throw new IllegalStateException("Illegal invocation to setParameters() during lifecycle invocation");
+        }
+    }
+
+    public Object getTimer() {
+        return timer;
+    }
+
+}
---
+++ impl/src/main/java/org/jboss/interceptor/reader/InterceptorMetadataUtils.java
@@ -0,0 +1,153 @@
+package org.jboss.interceptor.reader;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import javax.interceptor.InvocationContext;
+
+import org.jboss.interceptor.builder.MethodReference;
+import org.jboss.interceptor.spi.metadata.ClassMetadata;
+import org.jboss.interceptor.spi.metadata.InterceptorMetadata;
+import org.jboss.interceptor.spi.metadata.InterceptorReference;
+import org.jboss.interceptor.spi.metadata.MethodMetadata;
+import org.jboss.interceptor.spi.model.InterceptionType;
+import org.jboss.interceptor.util.InterceptionTypeRegistry;
+import org.jboss.interceptor.util.InterceptorMetadataException;
+import org.jboss.interceptor.util.ReflectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Marius Bogoevici
+ */
+public class InterceptorMetadataUtils {
+    protected static final String OBJECT_CLASS_NAME = Object.class.getName();
+
+    private static final Logger LOG = LoggerFactory.getLogger(InterceptorMetadataUtils.class);
+
+
+    public static InterceptorMetadata readMetadataForInterceptorClass(InterceptorReference<?> interceptorReference) {
+        return new SimpleInterceptorMetadata(interceptorReference, false, buildMethodMap(interceptorReference.getClassMetadata(), false));
+    }
+
+    public static InterceptorMetadata readMetadataForTargetClass(ClassMetadata<?> classMetadata) {
+        return new SimpleInterceptorMetadata(ClassMetadataInterceptorReference.of(classMetadata), true, buildMethodMap(classMetadata, true));
+    }
+
+    public static boolean isInterceptorMethod(InterceptionType interceptionType, MethodMetadata method, boolean forTargetClass) {
+
+        if (!method.getSupportedInterceptionTypes().contains(interceptionType)) {
+            return false;
+        }
+
+        if (interceptionType.isLifecycleCallback()) {
+            if (!Void.TYPE.equals(method.getReturnType())) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "does not have a void return type");
+                }
+                return false;
+            }
+
+            Class<?>[] parameterTypes = method.getJavaMethod().getParameterTypes();
+
+            if (forTargetClass && parameterTypes.length != 0) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "is defined on the target class and does not have 0 arguments");
+                }
+                return false;
+            }
+
+            if (!forTargetClass && parameterTypes.length != 1) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "does not have exactly one parameter");
+                }
+                return false;
+            }
+
+            if (parameterTypes.length == 1 && !InvocationContext.class.isAssignableFrom(parameterTypes[0])) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "its single argument is not a " + InvocationContext.class.getName());
+                }
+                return false;
+            }
+
+            return true;
+        } else {
+            if (!Object.class.equals(method.getReturnType())) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.warn(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "does not return a " + OBJECT_CLASS_NAME);
+                }
+                return false;
+            }
+
+            Class<?>[] parameterTypes = method.getJavaMethod().getParameterTypes();
+
+            if (parameterTypes.length != 1) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.debug(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "does not have exactly 1 parameter");
+                }
+                return false;
+            }
+
+            if (!InvocationContext.class.isAssignableFrom(parameterTypes[0])) {
+                if (LOG.isWarnEnabled()) {
+                    LOG.debug(getStandardIgnoredMessage(interceptionType, method.getJavaMethod()) + "does not have a " + InvocationContext.class.getName() + " parameter ");
+                }
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+    static String getStandardIgnoredMessage(InterceptionType interceptionType, Method method) {
+        return "Method " + method.getName() + " defined on class " + method.getDeclaringClass().getName()
+                + " will not be used for interception, since it is not defined according to the specification. It is annotated with @"
+                + interceptionType.annotationClassName() + ", but ";
+    }
+
+    static Map<InterceptionType, List<MethodMetadata>> buildMethodMap(ClassMetadata<?> interceptorClass, boolean forTargetClass) {
+        Map<InterceptionType, List<MethodMetadata>> methodMap = new HashMap<InterceptionType, List<MethodMetadata>>();
+        ClassMetadata<?> currentClass = interceptorClass;
+        Set<MethodReference> foundMethods = new HashSet<MethodReference>();
+        do {
+            Set<InterceptionType> detectedInterceptorTypes = new HashSet<InterceptionType>();
+
+            for (MethodMetadata method : currentClass.getDeclaredMethods()) {
+                MethodReference methodReference = MethodReference.of(method, Modifier.isPrivate(method.getJavaMethod().getModifiers()));
+                if (!foundMethods.contains(methodReference)) {
+                    for (InterceptionType interceptionType : InterceptionTypeRegistry.getSupportedInterceptionTypes()) {
+                        if (isInterceptorMethod(interceptionType, method, forTargetClass)) {
+                            if (methodMap.get(interceptionType) == null) {
+                                methodMap.put(interceptionType, new LinkedList<MethodMetadata>());
+                            }
+                            if (detectedInterceptorTypes.contains(interceptionType)) {
+                                throw new InterceptorMetadataException("Same interception type cannot be specified twice on the same class");
+                            } else {
+                                detectedInterceptorTypes.add(interceptionType);
+                            }
+                            // add method in the list - if it is there already, it means that it has been added by a subclass
+                            // final methods are treated separately, as a final method cannot override another method nor be
+                            // overridden
+                            ReflectionUtils.ensureAccessible(method.getJavaMethod());
+                            if (!foundMethods.contains(methodReference)) {
+                                methodMap.get(interceptionType).add(0, method);
+                            }
+                        }
+                    }
+                    // the method reference must be added anyway - overridden methods are not taken into consideration
+                    foundMethods.add(methodReference);
+                }
+            }
+            currentClass = currentClass.getSuperclass();
+        }
+        while (currentClass != null && !OBJECT_CLASS_NAME.equals(currentClass.getJavaClass()));
+        return methodMap;
+    }
+}
