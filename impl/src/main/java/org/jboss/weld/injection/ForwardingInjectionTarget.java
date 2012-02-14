75a9d966dc9dc9020bdd91221297ed3304c123fa
Stuart Douglas <stuart.w.douglas@gmail.com>
2012-02-14 13:42:10 +1100

    Change the way the bean instance is setup for decorators with field delegate injection points

---
+++ impl/src/main/java/org/jboss/weld/injection/ForwardingInjectionTarget.java
@@ -0,0 +1,39 @@
+package org.jboss.weld.injection;
+
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.InjectionTarget;
+
+/**
+ * @author Stuart Douglas
+ */
+public abstract class ForwardingInjectionTarget<T> implements InjectionTarget<T>{
+
+    protected abstract InjectionTarget<T> delegate();
+
+    public void inject(final T instance, final CreationalContext<T> ctx) {
+        delegate().inject(instance, ctx);
+    }
+
+    public void postConstruct(final T instance) {
+        delegate().postConstruct(instance);
+    }
+
+    public void preDestroy(final T instance) {
+        delegate().preDestroy(instance);
+    }
+
+    public T produce(final CreationalContext<T> ctx) {
+        return delegate().produce(ctx);
+    }
+
+    public void dispose(final T instance) {
+        delegate().dispose(instance);
+    }
+
+    public Set<InjectionPoint> getInjectionPoints() {
+        return delegate().getInjectionPoints();
+    }
+}
