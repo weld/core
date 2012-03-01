11da8967ed12d6347db96f3452b93776d4adee8d
Ales Justin <ales.justin@gmail.com>
2012-03-01 12:12:02 +0100

    Test modified -- nullify fields.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/annotatedType/superclass/ModifyExtension.java
@@ -0,0 +1,31 @@
+package org.jboss.weld.tests.annotatedType.superclass;
+
+import org.jboss.weld.introspector.ForwardingAnnotatedType;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.AnnotatedField;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessAnnotatedType;
+import java.util.Collections;
+import java.util.Set;
+
+/**
+ * @author Ales Justin
+ */
+public class ModifyExtension implements Extension {
+    public void processAnnotatedType(@Observes ProcessAnnotatedType<Child> event) {
+        final AnnotatedType<Child> annotatedType = event.getAnnotatedType();
+        event.setAnnotatedType(new ForwardingAnnotatedType<Child>() {
+            @Override
+            public AnnotatedType<Child> delegate() {
+                return annotatedType;
+            }
+
+            @Override
+            public Set<AnnotatedField<? super Child>> getFields() {
+                return Collections.emptySet();
+            }
+        });
+    }
+}
