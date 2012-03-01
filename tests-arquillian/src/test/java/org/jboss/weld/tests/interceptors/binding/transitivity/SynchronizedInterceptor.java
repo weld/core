648b7fc7d905523d8c0e74e0f80190b28a586b50
Marko Luksa <marko.luksa@gmail.com>
2012-03-01 12:02:19 +0100

    WELD-999 Interceptor binding transitivity broken - additional test

--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/binding/transitivity/SynchronizedInterceptor.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/binding/transitivity/SynchronizedInterceptor.java
@@ -24,8 +24,11 @@ import javax.interceptor.InvocationContext;
 @Synchronized
 public class SynchronizedInterceptor {
 
+    public static int invocationCount;
+
     @AroundInvoke
     public Object intercept(InvocationContext ctx) throws Exception {
+        invocationCount++;
         return ctx.proceed();
     }
 }
