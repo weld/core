436018ae2e01cff301fb838e99eb2ebc0fc5edaf
Marko Luksa <marko.luksa@gmail.com>
2012-03-02 08:22:25 +0100

    WELD-1019 test

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/weld1019/HelloBean.java
@@ -0,0 +1,17 @@
+package org.jboss.weld.tests.interceptors.weld1019;
+
+import javax.inject.Named;
+import java.io.Serializable;
+
+/**
+ *
+ */
+@MyScope
+public class HelloBean implements Serializable {
+    private static final long serialVersionUID = -3216074155876250969L;
+
+    @UpperCased
+    public String getMessage() {
+        return "Hello World";
+    }
+}
