629e73dd43ce8424c8de10ef058c1625a0d26be4
Ales Justin <ales.justin@gmail.com>
2012-03-02 17:16:34 +0100

    [WELD-1007]; initial wctor fix; TODO on actual generic params usage.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/smoke/SmokeTest.java
@@ -0,0 +1,34 @@
+package org.jboss.weld.tests.smoke;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.enterprise.inject.Instance;
+
+/**
+ * Smoke tests -- check anything unusual.
+ *
+ * @author Sam Corbet
+ * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
+ */
+@RunWith(Arquillian.class)
+public class SmokeTest {
+    @Deployment
+    public static Archive getDeployment() {
+        return ShrinkWrap.create(BeanArchive.class)
+                .addPackage(Crasher.class.getPackage());
+    }
+
+    @Test
+    public void testInnerClass(Instance<Crasher> instance) {
+        Crasher crasher = instance.get();
+        Assert.assertNotNull(crasher);
+    }
+
+}
