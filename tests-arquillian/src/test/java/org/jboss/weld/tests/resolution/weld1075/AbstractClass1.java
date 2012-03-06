7c45798209277f098bec7bec33969748caea3799
Ales Justin <ales.justin@gmail.com>
2012-03-06 16:52:41 +0100

    [WELD-1075]; port .zip tests.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/resolution/weld1075/AbstractClass1.java
@@ -0,0 +1,5 @@
+package org.jboss.weld.tests.resolution.weld1075;
+
+
+public abstract class AbstractClass1<T1, T2, T3> implements Interface2<T1, T2> {
+}
---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/resolution/weld1075/ConcreteClass1.java
@@ -0,0 +1,8 @@
+package org.jboss.weld.tests.resolution.weld1075;
+
+
+import java.util.UUID;
+
+
+public class ConcreteClass1 extends AbstractClass2<String, Integer, UUID> {
+}
