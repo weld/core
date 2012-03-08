749633ce98ed4f86b0938e95cc6850629009d254
Ales Justin <ales.justin@gmail.com>
2012-03-09 00:52:22 +0100

    One less map lookup.

--- impl/src/main/java/org/jboss/weld/bean/AbstractReceiverBean.java
+++ impl/src/main/java/org/jboss/weld/bean/AbstractReceiverBean.java
@@ -32,6 +32,7 @@ import static org.jboss.weld.logging.messages.BeanMessage.CIRCULAR_CALL;
 
 /**
  * @author pmuir
+ * @author alesj
  */
 public abstract class AbstractReceiverBean<X, T, S extends Member> extends AbstractBean<T, S> {
 
@@ -53,6 +54,7 @@ public abstract class AbstractReceiverBean<X, T, S extends Member> extends Abstr
     /**
      * Gets the receiver of the product
      *
+     * @param creationalContext the creational context
      * @return The receiver
      */
     protected Object getReceiver(CreationalContext<?> creationalContext) {
@@ -64,9 +66,10 @@ public abstract class AbstractReceiverBean<X, T, S extends Member> extends Abstr
         } else {
             if (creationalContext instanceof WeldCreationalContext<?>) {
                 WeldCreationalContext<?> creationalContextImpl = (WeldCreationalContext<?>) creationalContext;
-                if (creationalContextImpl.containsIncompleteInstance(getDeclaringBean())) {
+                final X incompleteInstance = creationalContextImpl.getIncompleteInstance(getDeclaringBean());
+                if (incompleteInstance != null) {
                     log.warn(CIRCULAR_CALL, getWeldAnnotated(), getDeclaringBean());
-                    return creationalContextImpl.getIncompleteInstance(getDeclaringBean());
+                    return incompleteInstance;
                 }
             }
             return beanManager.getReference(getDeclaringBean(), creationalContext, true);
