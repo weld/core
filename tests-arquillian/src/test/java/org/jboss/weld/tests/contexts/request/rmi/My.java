3cdb514809ecc05c533f732ff072f21fefdc5733
Ales Justin <ales.justin@gmail.com>
2012-03-19 14:50:21 +0100

    Refactor rmi test; ignored atm.

--- tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/request/rmi/My.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/request/rmi/My.java
@@ -1,11 +1,11 @@
 package org.jboss.weld.tests.contexts.request.rmi;
 
-import javax.enterprise.inject.Default;
-import javax.inject.Qualifier;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+
+import javax.enterprise.inject.Default;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE})
--- tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/request/rmi/RmiRequestScopeTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/request/rmi/RmiRequestScopeTest.java
@@ -1,49 +1,87 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.tests.contexts.request.rmi;
 
-import org.jboss.arquillian.api.Deployment;
-import org.jboss.arquillian.api.Run;
-import org.jboss.arquillian.api.RunModeType;
+import java.util.Hashtable;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.RunAsClient;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.weld.tests.category.Integration;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
-import javax.naming.InitialContext;
-import javax.naming.NamingException;
-
+/**
+ * @author Matija Mazi
+ * @author Ales Justin
+ */
 @Category(Integration.class)
 @RunWith(Arquillian.class)
-@Run(RunModeType.AS_CLIENT)
+@RunAsClient
 public class RmiRequestScopeTest {
 
     @Deployment
-    public static WebArchive createDeployment()
-    {
+    public static WebArchive createDeployment() {
         return ShrinkWrap.create(WebArchive.class, "test.war")
                 .addClasses(Bridge.class, BridgeBean.class, Config.class, Manager.class, My.class)
-                .addWebResource(EmptyAsset.INSTANCE, "beans.xml");
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
     }
 
-    private static Bridge getBridge()
-    {
-        try
-        {
-            Object serverObject = new InitialContext().lookup("BridgeBean/remote");
-            return (Bridge) serverObject;
-        }
-        catch (NamingException e)
-        {
+    private static Bridge getBridge() {
+        try {
+            final Hashtable jndiProperties = new Hashtable();
+            jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
+            final Context context = new InitialContext(jndiProperties);
+            // The app name is the application name of the deployed EJBs. This is typically the ear name
+            // without the .ear suffix. However, the application name could be overridden in the application.xml of the
+            // EJB deployment on the server.
+            // Since we haven't deployed the application as a .ear, the app name for us will be an empty string
+            final String appName = "";
+            // This is the module name of the deployed EJBs on the server. This is typically the jar name of the
+            // EJB deployment, without the .jar suffix, but can be overridden via the ejb-jar.xml
+            // In this example, we have deployed the EJBs in a jboss-as-ejb-remote-app.jar, so the module name is
+            // jboss-as-ejb-remote-app
+            final String moduleName = "test";
+            // AS7 allows each deployment to have an (optional) distinct name. We haven't specified a distinct name for
+            // our EJB deployment, so this is an empty string
+            final String distinctName = "";
+            // The EJB name which by default is the simple class name of the bean implementation class
+            final String beanName = BridgeBean.class.getSimpleName();
+            // the remote view fully qualified class name
+            final String viewClassName = Bridge.class.getName();
+            // let's do the lookup
+            return (Bridge) context.lookup("ejb:" + appName + "/" + moduleName + "/" + distinctName + "/" + beanName + "!" + viewClassName);
+        } catch (NamingException e) {
             throw new RuntimeException(e);
         }
     }
 
     @Test
-    public void testRmiRequestScopeActive()
-    {
+    @Ignore
+    public void testRmiRequestScopeActive() {
         System.out.println(getBridge().doSomething());
     }
 }
