53d7934af881c5e999e28f0a12ad62fb4efb35b8
Marko Luksa <marko.luksa@gmail.com>
2012-04-03 11:13:47 +0200

    Fix Weld930Test

--- tests-arquillian/src/test/java/org/jboss/weld/tests/alternatives/weld930/AlternativeProducer.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/alternatives/weld930/AlternativeProducer.java
@@ -8,13 +8,11 @@ import javax.inject.Named;
 /**
  * @author Marko Luksa
  */
-@RequestScoped
 @Alternative
 public class AlternativeProducer {
 
     @Produces
     @Named("product")
-    @RequestScoped
     public Product produce() throws Exception {
         return new Product("Alternative");
     }
