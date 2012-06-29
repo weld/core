ce7508267816ea4c7e0f9a9b05dd9150959de315
Jozef Hartinger <jharting@redhat.com>
2012-06-29 13:17:49 +0200

    WELD-1155 Testcase

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/session/weld1155/SessionScopedBean.java
@@ -0,0 +1,42 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.contexts.session.weld1155;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+
+@SessionScoped
+public class SessionScopedBean implements Serializable {
+
+    private static final long serialVersionUID = -3019516240186020733L;
+
+    private static final AtomicInteger ids = new AtomicInteger();
+
+    private volatile int id;
+
+    @PostConstruct
+    public void init() {
+        this.id = ids.incrementAndGet();
+    }
+
+    public int getId() {
+        return id;
+    }
+}
