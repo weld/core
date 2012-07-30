a59b54017590512c9bd4e1485ac1ebac784e1764
Ales Justin <ales.justin@gmail.com>
2012-07-30 23:37:11 +0200

    Small fixes wrt updates
    Conflicts:
    	environments/osgi/core/api/pom.xml
    	environments/osgi/core/integration/pom.xml
    	environments/osgi/core/mandatory/pom.xml
    	environments/osgi/pom.xml
    	environments/osgi/tests/pom.xml
    	environments/osgi/tests/tester/pom.xml
    	impl/src/main/java/org/jboss/weld/bean/DecoratorImpl.java
    	impl/src/main/java/org/jboss/weld/jsf/WeldPhaseListener.java
    	tests-arquillian/src/test/java/org/jboss/weld/tests/specialization/weld802/CustomExtension.java
    	tests/src/test/java/org/jboss/weld/tests/unit/util/AnnotatedTypesTest.java

--- environments/osgi/core/extension/src/main/java/org/jboss/weld/environment/osgi/impl/extension/service/WeldOSGiExtension.java
+++ environments/osgi/core/extension/src/main/java/org/jboss/weld/environment/osgi/impl/extension/service/WeldOSGiExtension.java
@@ -1,6 +1,6 @@
 /*
  * JBoss, Home of Professional Open Source
- * Copyright 2011, Red Hat Middleware LLC, and individual contributors
+ * Copyright 2012, Red Hat, Inc., and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -310,7 +310,7 @@ public class WeldOSGiExtension implements Extension {
             delegate.removeServiceListener(listener);
         }
     }
-    
+
     void afterDeployment(@Observes AfterDeploymentValidation event) {
         if (listener != null) {
             currentContext.remove();
