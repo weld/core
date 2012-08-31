9fb8d2560693191e969c9796ec478c7953d63877
Marko Luksa <marko.luksa@gmail.com>
2012-08-31 14:05:44 +0200

    Added test for WELD-1162

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/bridgemethods/managed/ManagedSpecialServiceImpl.java
@@ -0,0 +1,19 @@
+package org.jboss.weld.tests.interceptors.bridgemethods.managed;
+
+import org.jboss.weld.tests.interceptors.bridgemethods.common.SomeInterceptorBinding;
+import org.jboss.weld.tests.interceptors.bridgemethods.common.SpecialService;
+
+
+/**
+ *
+ */
+@SomeInterceptorBinding
+public class ManagedSpecialServiceImpl implements SpecialService {
+
+    public void doSomething(String param) {
+    }
+
+    public String returnSomething() {
+        return "";
+    }
+}
