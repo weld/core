2fcb719bd6c8a70b022b40af3f2e2e59574113a6
Marko Luksa <marko.luksa@gmail.com>
2012-10-29 11:55:32 +0100

    WELD-1236 Deserialization fails for bean containing intercepted methods with primitive or array-typed parameters
    Conflicts:
    	tests/src/main/java/org/jboss/weld/test/util/Utils.java

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/passivating/serialization/interception/FooDecorator.java
@@ -0,0 +1,49 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.contexts.passivating.serialization.interception;
+
+import java.io.Serializable;
+
+import javax.decorator.Delegate;
+import javax.inject.Inject;
+
+/**
+ *
+ */
+@javax.decorator.Decorator
+public class FooDecorator implements Foo, Serializable {
+
+    @Inject
+    @Delegate
+    private Foo delegate;
+
+    public void methodWithObjectParameter(String str) {
+    }
+
+    public void methodWithPrimitiveParameters(byte b, short s, int i, long l, float f, double d, boolean bool, char ch) {
+    }
+
+    public void methodWithObjectArrayParameter(String[] strArray) {
+    }
+
+    public void methodWithPrimitiveArrayParameter(int[] intArray) {
+    }
+
+    public void methodWithInnerClassParameter(FooImpl.InnerClass innerClass) {
+    }
+}
