c340f7f6beaa72f42345c3f0578665e98cee5807
Jozef Hartinger <jharting@redhat.com>
2012-11-23 13:37:54 +0100

    Fix SplitInterceptorsTest

--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/lhotse/SplitInterceptorsTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/lhotse/SplitInterceptorsTest.java
@@ -26,9 +26,12 @@ package org.jboss.weld.tests.interceptors.lhotse;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.BeanArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.impl.BeansXml;
 import org.jboss.weld.tests.category.Integration;
 import org.jboss.weld.tests.interceptors.lhotse.fst.TDAO;
 import org.jboss.weld.tests.interceptors.lhotse.fst.TxInterceptor;
@@ -53,7 +56,7 @@ public class SplitInterceptorsTest {
         fst.addPackage(TDAO.class.getPackage());
         web.addAsLibrary(fst);
 
-        BeanArchive snd = ShrinkWrap.create(BeanArchive.class).intercept(TxInterceptor.class);
+        JavaArchive snd = ShrinkWrap.create(JavaArchive.class).addAsManifestResource(new BeansXml().interceptors(TxInterceptor.class), ArchivePaths.create("beans.xml"));
         snd.addPackage(CDAO.class.getPackage());
         web.addAsLibrary(snd);
 
