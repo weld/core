423e498a96fc2e9ee8159058a394c116023d1cf3
Marek Schmidt <maschmid@redhat.com>
2012-12-11 16:29:03 +0100

    AnnotatedTypeObserversTest

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/annotatedType/observers/RoomId.java
@@ -0,0 +1,86 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.annotatedType.observers;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.enterprise.util.AnnotationLiteral;
+import javax.inject.Qualifier;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Qualifier
+@Target(
+{TYPE, METHOD, PARAMETER, FIELD})
+@Retention(RUNTIME)
+@Documented
+public @interface RoomId {
+   String value();
+
+   public static class RoomIdLiteral extends AnnotationLiteral<RoomId> implements RoomId
+   {
+      @Override
+      public int hashCode()
+      {
+         final int prime = 31;
+         int result = super.hashCode();
+         result = prime * result + ((value == null) ? 0 : value.hashCode());
+         return result;
+      }
+
+      @Override
+      public boolean equals(Object obj)
+      {
+         if (this == obj)
+            return true;
+         if (!super.equals(obj))
+            return false;
+         if (getClass() != obj.getClass())
+            return false;
+         RoomIdLiteral other = (RoomIdLiteral) obj;
+         if (value == null)
+         {
+            if (other.value != null)
+               return false;
+         }
+         else if (!value.equals(other.value))
+            return false;
+         return true;
+      }
+
+      private static final long serialVersionUID = 3237202887991048175L;
+
+      private String value;
+
+      public RoomIdLiteral(String value)
+      {
+         this.value = value;
+      }
+
+      @Override
+      public String value()
+      {
+         return value;
+      }
+   }
+}
