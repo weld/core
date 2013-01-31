2f7509be33c206cfbd476d2308647b87bc273444
Jozef Hartinger <jharting@redhat.com>
2013-01-31 11:38:00 +0100

    Testcase for AS7-5966

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/integration/multideployment/CrossDeploymentTest.java
@@ -0,0 +1,71 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.integration.multideployment;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.OperateOnDeployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Weld cross deployment injection test case. Verifies that weld can read beans.xml and scan classes outside the
+ * deployment.
+ *
+ * @author Stuart Douglas
+ */
+@RunWith(Arquillian.class)
+@Ignore("Does not work on AS 7.1.2.Final. Works on AS7 master")
+public class CrossDeploymentTest {
+
+
+
+    @Deployment(name = "d1", order = 1, testable = false)
+    public static Archive<?> deploy() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "d1.jar");
+        jar.addClasses(SimpleBean.class);
+        jar.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        return jar;
+    }
+
+    @Deployment(name = "d2", order = 2)
+    public static Archive<?> deploy2() {
+        JavaArchive jar = ShrinkWrap.create(JavaArchive.class, "d2.jar");
+        jar.addClass(CrossDeploymentTest.class);
+        jar.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+        jar.addAsManifestResource(new StringAsset("Dependencies: deployment.d1.jar meta-inf\n"), "MANIFEST.MF");
+        return jar;
+    }
+
+    @Inject
+    private SimpleBean bean;
+
+    @Test
+    @OperateOnDeployment("d2")
+    public void testSimpleBeanInjected() throws Exception {
+        Assert.assertNotNull(bean);
+    }
+}
