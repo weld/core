41c421c4cb89546926dd0fa02fc6dd0f1ec2d74a
Martin Kouba <mkouba@redhat.com>
2013-02-07 13:04:09 +0100

    Test for WELD-1322

---
+++ environments/se/core/src/test/java/org/jboss/weld/environment/se/test/inheritance/PostConstructInheritanceTest.java
@@ -0,0 +1,48 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.se.test.inheritance;
+
+import static org.junit.Assert.assertTrue;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
+
+/**
+ *
+ * @author Martin Kouba
+ * @see WELD-1322
+ */
+public class PostConstructInheritanceTest {
+
+	@Test
+	public void testPostConstructInherited() {
+
+		Weld weld = new Weld();
+		try {
+
+			Plant.postConstructCalled = false;
+			WeldContainer weldContainer = weld.initialize();
+			weldContainer.instance().select(Tree.class).get();
+			assertTrue(Plant.postConstructCalled);
+
+		} finally {
+			weld.shutdown();
+		}
+	}
+
+}
