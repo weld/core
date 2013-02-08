d805d821be33567d2003cb8ccef073ef29ae79d1
Jozef Hartinger <jharting@redhat.com>
2013-02-08 10:26:44 +0100

    Upgrade Guava and slf4j

--- tests-arquillian/src/test/java/org/jboss/weld/tests/smoke/Crasher.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/smoke/Crasher.java
@@ -1,19 +1,17 @@
 package org.jboss.weld.tests.smoke;
 
-import javax.annotation.Nonnull;
-
 /**
  * @author Sam Corbet
  * @author <a href="mailto:ales.justin@jboss.org">Ales Justin</a>
  */
 public class Crasher {
     protected class NonStaticInner {
-        public NonStaticInner(@Nonnull Integer a) {
+        public NonStaticInner(Integer a) {
             System.out.println("Hi" + a);
         }
 
         protected class NonStaticInner2 {
-            public NonStaticInner2(@Nonnull Double a) {
+            public NonStaticInner2(Double a) {
                 System.out.println("Hi" + a);
             }
         }
