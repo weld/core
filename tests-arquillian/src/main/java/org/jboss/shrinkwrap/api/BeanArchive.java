c3220a1d325cd8f3e9c79c6e935efbfa60a511ed
Jozef Hartinger <jharting@redhat.com>
2013-03-04 10:36:43 +0100

    Revert "CDI-18 expand test utils to support new attributes in beans.xml"
    This reverts commit d9490341319d2b0c6b200ef345b0bc1144b7581e.

--- tests-arquillian/src/main/java/org/jboss/shrinkwrap/api/BeanArchive.java
+++ tests-arquillian/src/main/java/org/jboss/shrinkwrap/api/BeanArchive.java
@@ -15,8 +15,6 @@ public interface BeanArchive extends JavaArchive {
      */
     BeanArchive decorate(Class<?>... classes);
 
-    BeanArchive decorate(BeansXmlClass... classes);
-
     /**
      * Adds Interceptors to the beans.xml.
      *
@@ -25,8 +23,6 @@ public interface BeanArchive extends JavaArchive {
      */
     BeanArchive intercept(Class<?>... classes);
 
-    BeanArchive intercept(BeansXmlClass... classes);
-
     /**
      * Adds Alternatives to the beans.xml.
      *
@@ -34,8 +30,6 @@ public interface BeanArchive extends JavaArchive {
      * @return
      */
     BeanArchive alternate(Class<?>... classes);
-
-    BeanArchive alternate(BeansXmlClass... classes);
 
     /**
      * Adds a Stereotype Alternative to beans.xml.
--- tests-arquillian/src/main/java/org/jboss/shrinkwrap/impl/BeanArchiveImpl.java
+++ tests-arquillian/src/main/java/org/jboss/shrinkwrap/impl/BeanArchiveImpl.java
@@ -4,14 +4,13 @@ import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.ArchivePath;
 import org.jboss.shrinkwrap.api.ArchivePaths;
 import org.jboss.shrinkwrap.api.BeanArchive;
-import org.jboss.shrinkwrap.api.BeansXmlClass;
 import org.jboss.shrinkwrap.impl.base.path.BasicPath;
 import org.jboss.shrinkwrap.impl.base.spec.JavaArchiveImpl;
 
 public class BeanArchiveImpl extends JavaArchiveImpl implements BeanArchive {
-    // -------------------------------------------------------------------------------------||
+    //-------------------------------------------------------------------------------------||
     // Class Members ----------------------------------------------------------------------||
-    // -------------------------------------------------------------------------------------||
+    //-------------------------------------------------------------------------------------||
 
     /**
      * Path to the manifests inside of the Archive.
@@ -41,23 +40,18 @@ public class BeanArchiveImpl extends JavaArchiveImpl implements BeanArchive {
         addAsManifestResource(descriptor, ArchivePaths.create("beans.xml"));
     }
 
-    // -------------------------------------------------------------------------------------||
+    //-------------------------------------------------------------------------------------||
     // Required Implementations -----------------------------------------------------------||
-    // -------------------------------------------------------------------------------------||
+    //-------------------------------------------------------------------------------------||
 
-    // -------------------------------------------------------------------------------------||
+
+    //-------------------------------------------------------------------------------------||
     // Required Implementations - BeanArchive ---------------------------------------------||
-    // -------------------------------------------------------------------------------------||
+    //-------------------------------------------------------------------------------------||
 
     public BeanArchive decorate(Class<?>... classes) {
         descriptor.decorators(classes);
         addClasses(classes);
-        return covarientReturn();
-    }
-
-    @Override
-    public BeanArchive decorate(BeansXmlClass... classes) {
-        descriptor.decorators(classes);
         return covarientReturn();
     }
 
@@ -67,26 +61,14 @@ public class BeanArchiveImpl extends JavaArchiveImpl implements BeanArchive {
         return covarientReturn();
     }
 
-    @Override
-    public BeanArchive intercept(BeansXmlClass... classes) {
-        descriptor.interceptors(classes);
-        return covarientReturn();
-    }
-
     public BeanArchive alternate(Class<?>... classes) {
         descriptor.alternatives(classes);
         addClasses(classes);
         return covarientReturn();
     }
 
-    @Override
-    public BeanArchive alternate(BeansXmlClass... classes) {
-        descriptor.alternatives(classes);
-        return covarientReturn();
-    }
-
     public BeanArchive stereotype(Class<?>... classes) {
-        descriptor.stereotypes(classes);
+        descriptor.stereotype(classes);
         addClasses(classes);
         return covarientReturn();
     }
