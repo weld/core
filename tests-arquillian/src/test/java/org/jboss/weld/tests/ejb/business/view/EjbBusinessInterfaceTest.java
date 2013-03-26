b29dc13da6b1a6fe988a9fc203af34c9df3b76e0
Jozef Hartinger <jharting@redhat.com>
2013-03-26 11:09:06 +0100

    Testcase for WELD-921

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/ejb/business/view/EjbBusinessInterfaceTest.java
@@ -0,0 +1,59 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.ejb.business.view;
+
+import static org.junit.Assert.assertEquals;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.tests.category.Integration;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+@Category(Integration.class)
+public class EjbBusinessInterfaceTest {
+
+    @Inject
+    private InjectedBean bean;
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return ShrinkWrap.create(BeanArchive.class).addPackage(EjbBusinessInterfaceTest.class.getPackage());
+    }
+
+    @Test
+    public void testMethodDefinedOnSuperclass() {
+        assertEquals("ping", bean.getFoo().ping("ping"));
+    }
+
+    @Test
+    public void testMethodDefinedOnSuperInterface1() {
+        assertEquals("ping", bean.getBarLocal().ping("ping"));
+    }
+
+    @Test
+    public void testMethodDefinedOnSuperInterface2() {
+        assertEquals("ping", bean.getBarSuperInterface().ping("ping"));
+    }
+}
