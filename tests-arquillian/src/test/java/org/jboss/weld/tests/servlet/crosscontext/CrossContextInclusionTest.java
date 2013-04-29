e794903fc34e15da6aca26b63f0b54bcc02e9e41
Ron Smeral <rsmeral@redhat.com>
2013-04-29 16:24:25 +0200

    Test for WELD-1415 finished, ignored

--- tests-arquillian/src/test/java/org/jboss/weld/tests/servlet/crosscontext/CrossContextInclusionTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/servlet/crosscontext/CrossContextInclusionTest.java
@@ -5,40 +5,42 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import java.io.IOException;
 import java.net.URL;
 import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.container.test.api.Testable;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.Archive;
-import org.jboss.shrinkwrap.api.BeanArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.EnterpriseArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import static org.junit.Assert.assertEquals;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 
 @RunWith(Arquillian.class)
 public class CrossContextInclusionTest {
 
-    @Deployment
+    @Deployment(testable = false)
     public static Archive<?> deployment() {
+
+        WebArchive war1 = ShrinkWrap.create(WebArchive.class, "app1.war")
+                .addClass(MainServlet.class)
+                .addAsWebInfResource(new StringAsset(""), "beans.xml");
+
+        WebArchive war2 = ShrinkWrap.create(WebArchive.class, "app2.war")
+                .addClass(IncludedServlet.class)
+                .addAsWebInfResource(new StringAsset(""), "beans.xml");
+
         return ShrinkWrap.create(EnterpriseArchive.class)
-                .addAsModule(ShrinkWrap.create(BeanArchive.class)
-                    .addClass(CrossContextInclusionTest.class))
-                .addAsModule(ShrinkWrap.create(WebArchive.class, "app1.war")
-                    .addClass(MainServlet.class)
-                    .addAsManifestResource(new StringAsset(""), "beans.xml"))
-                .addAsModule(ShrinkWrap.create(WebArchive.class, "app2.war")
-                    .addClass(IncludedServlet.class)
-                    .addAsManifestResource(new StringAsset(""), "beans.xml"));
+                .addAsModule(Testable.archiveToTest(war1))
+                .addAsModule(war2);
     }
-    
+
     @Test
-//    @Ignore("WELD-1415")
-    public void testCrossContextInclusion(@ArquillianResource(MainServlet.class) URL servlet) throws IOException {
-        WebClient client = new WebClient();
-        Page page = client.getPage(servlet);
+    @Ignore("WELD-1415")
+    public void testCrossContextInclusion(@ArquillianResource(MainServlet.class) URL contextPath) throws IOException {
+        Page page = new WebClient().getPage(contextPath + "/main");
         assertEquals(200, page.getWebResponse().getStatusCode());
         assertEquals("<h1>Hello World</h1>", page.getWebResponse().getContentAsString());
     }
