1506ae0482e57c778a1252cc5c796d94de5eca0f
Martin Kouba <mkouba@redhat.com>
2013-07-09 13:34:41 +0200

    WELD-1380 Implement util methods to obtain member indexes

---
+++ impl/src/main/java/org/jboss/weld/util/reflection/DeclaredMemberIndexer.java
@@ -0,0 +1,195 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.util.reflection;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.ListIterator;
+
+import com.google.common.base.Preconditions;
+
+/**
+ *
+ * @author Martin Kouba
+ */
+public final class DeclaredMemberIndexer {
+
+    private static final ConstructorComparator CONSTRUCTOR_COMPARATOR_INSTANCE = new ConstructorComparator();
+
+    private static final MethodComparator METHOD_COMPARATOR_INSTANCE = new MethodComparator();
+
+    private static final FieldComparator FIELD_COMPARATOR_INSTANCE = new FieldComparator();
+
+    private DeclaredMemberIndexer() {
+    }
+
+    /**
+     * @param field
+     * @return the index for the given field
+     */
+    public static int getIndexForField(Field field) {
+        Preconditions.checkNotNull(field);
+        return getIndexForMember(field, getDeclaredFields(field.getDeclaringClass()));
+    }
+
+    /**
+     * @param index
+     * @param declaringClass
+     * @return the declared field for the given index and declaring class
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public static Field getFieldForIndex(int index, Class<?> declaringClass) {
+        return getDeclaredFields(declaringClass).get(index);
+    }
+
+    /**
+     * @param method
+     * @return the index for the given method
+     */
+    public static int getIndexForMethod(Method method) {
+        Preconditions.checkNotNull(method);
+        return getIndexForMember(method, getDeclaredMethods(method.getDeclaringClass()));
+    }
+
+    /**
+     * @param index
+     * @param declaringClass
+     * @return the declared method for the given index and declaring class
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public static Method getMethodForIndex(int index, Class<?> declaringClass) {
+        return getDeclaredMethods(declaringClass).get(index);
+    }
+
+    /**
+     * @param constructor
+     * @return the index for the given constructor
+     */
+    public static int getIndexForConstructor(Constructor<?> constructor) {
+        Preconditions.checkNotNull(constructor);
+        return getIndexForMember(constructor, getDeclaredConstructors(constructor.getDeclaringClass()));
+    }
+
+    /**
+     * @param index
+     * @param declaringClass
+     * @return the declared constructor for the given index and declaring class
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
+    public static Constructor<?> getConstructorForIndex(int index, Class<?> declaringClass) {
+        return getDeclaredConstructors(declaringClass).get(index);
+    }
+
+    private static <T extends Member> int getIndexForMember(T declaredMember, List<T> declaredMembers) {
+
+        for (ListIterator<T> iterator = declaredMembers.listIterator(); iterator.hasNext();) {
+            T member = iterator.next();
+            if (member.equals(declaredMember)) {
+                return iterator.previousIndex();
+            }
+        }
+        // This should never happen
+        throw new IllegalStateException("No matching declared member found for: " + declaredMember);
+    }
+
+    /**
+     * @param declaringClass
+     * @return the ordered list of declared fields for the given class
+     */
+    public static List<Field> getDeclaredFields(Class<?> declaringClass) {
+        Preconditions.checkNotNull(declaringClass);
+        List<Field> declaredFields = Arrays.asList(declaringClass.getDeclaredFields());
+        Collections.sort(declaredFields, FIELD_COMPARATOR_INSTANCE);
+        return declaredFields;
+    }
+
+    /**
+     * @param declaringClass
+     * @return the ordered list of declared methods for the given class
+     */
+    public static List<Method> getDeclaredMethods(Class<?> declaringClass) {
+        Preconditions.checkNotNull(declaringClass);
+        List<Method> declaredMethods = Arrays.asList(declaringClass.getDeclaredMethods());
+        Collections.sort(declaredMethods, METHOD_COMPARATOR_INSTANCE);
+        return declaredMethods;
+    }
+
+    /**
+     * @param declaringClass
+     * @return the ordered list of declared constructors for the given class
+     */
+    public static List<Constructor<?>> getDeclaredConstructors(Class<?> declaringClass) {
+        Preconditions.checkNotNull(declaringClass);
+        List<Constructor<?>> declaredConstructors = Arrays.asList(declaringClass.getDeclaredConstructors());
+        Collections.sort(declaredConstructors, CONSTRUCTOR_COMPARATOR_INSTANCE);
+        return declaredConstructors;
+    }
+
+    private static int compareParamTypes(Class<?>[] paramTypes1, Class<?>[] paramTypes2) {
+
+        // First compare the number of parameters
+        if (paramTypes1.length != paramTypes2.length) {
+            return paramTypes1.length - paramTypes2.length;
+        }
+
+        // Then compare the FCQNs of the parameter types
+        for (int i = 0; i < paramTypes1.length; i++) {
+            if (!paramTypes1[i].getName().equals(paramTypes2[i].getName())) {
+                return paramTypes1[i].getName().compareTo(paramTypes2[i].getName());
+            }
+        }
+        // This should never happen - constructors cannot have the same param
+        // types and methods are ordered by name first
+        return 0;
+    }
+
+    private static class ConstructorComparator implements Comparator<Constructor<?>> {
+
+        @Override
+        public int compare(Constructor<?> c1, Constructor<?> c2) {
+            return compareParamTypes(c1.getParameterTypes(), c2.getParameterTypes());
+        }
+    }
+
+    private static class MethodComparator implements Comparator<Method> {
+
+        @Override
+        public int compare(Method m1, Method m2) {
+            // First compare the names
+            if (!m1.getName().equals(m2.getName())) {
+                return m1.getName().compareTo(m2.getName());
+            }
+            // Then compare the parameters
+            return compareParamTypes(m1.getParameterTypes(), m2.getParameterTypes());
+        }
+    }
+
+    private static class FieldComparator implements Comparator<Field> {
+
+        @Override
+        public int compare(Field o1, Field o2) {
+            return o1.getName().compareTo(o2.getName());
+        }
+    }
+
+}
---
+++ impl/src/test/java/org/jboss/weld/tests/unit/util/DeclaredMemberIndexerTest.java
@@ -0,0 +1,173 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.unit.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+import org.jboss.weld.util.reflection.DeclaredMemberIndexer;
+import org.junit.Test;
+
+public class DeclaredMemberIndexerTest {
+
+    @Test
+    public void testFields() {
+        verifyFields(DeclaredMemberIndexer.getDeclaredFields(TestContainer.class), "bar", "baz", "foo", "qux");
+        try {
+            DeclaredMemberIndexer.getFieldForIndex(1000, TestContainer.class);
+            fail();
+        } catch (IndexOutOfBoundsException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void testConstructors() {
+        List<Constructor<?>> constructors = DeclaredMemberIndexer.getDeclaredConstructors(TestContainer.class);
+        assertEquals(5, constructors.size());
+        verifyConstructor(constructors, 0);
+        verifyConstructor(constructors, 1, String.class);
+        verifyConstructor(constructors, 2, String.class, Integer.class);
+        verifyConstructor(constructors, 3, String.class, String.class);
+        verifyConstructor(constructors, 4, Integer.class, String.class, BigDecimal.class);
+        try {
+            DeclaredMemberIndexer.getConstructorForIndex(1000, TestContainer.class);
+            fail();
+        } catch (IndexOutOfBoundsException e) {
+            // Expected
+        }
+    }
+
+    @Test
+    public void testMethods() {
+        List<Method> methods = DeclaredMemberIndexer.getDeclaredMethods(TestContainer.class);
+        assertEquals(5, methods.size());
+        verifyMethod(methods, 0, "bar", Integer.class, String.class);
+        verifyMethod(methods, 1, "bar", String.class, Integer.class);
+        verifyMethod(methods, 2, "bar", String.class, Integer.class, Boolean.class);
+        verifyMethod(methods, 3, "foo", Integer.class);
+        verifyMethod(methods, 4, "noParam");
+        try {
+            DeclaredMemberIndexer.getMethodForIndex(1000, TestContainer.class);
+            fail();
+        } catch (IndexOutOfBoundsException e) {
+            // Expected
+        }
+    }
+
+    private void verifyFields(List<Field> fields, String... names) {
+        assertEquals(names.length, fields.size());
+        for (int i = 0; i < names.length; i++) {
+            assertEquals(names[i], fields.get(i).getName());
+            assertEquals(i, DeclaredMemberIndexer.getIndexForField(fields.get(i)));
+            assertEquals(fields.get(i), DeclaredMemberIndexer.getFieldForIndex(i, TestContainer.class));
+        }
+    }
+
+    private void verifyConstructor(List<Constructor<?>> constructors, int index, Class<?>... expectedParamTypes) {
+        Constructor<?> constructor = constructors.get(index);
+        assertEquals(constructor.getParameterTypes().length, expectedParamTypes.length);
+        for (int i = 0; i < expectedParamTypes.length; i++) {
+            if (!constructor.getParameterTypes()[i].getName().equals(expectedParamTypes[i].getName())) {
+                fail(constructor.getParameterTypes()[i].getName() + " ne " + expectedParamTypes[i].getName());
+            }
+        }
+        assertEquals(index, DeclaredMemberIndexer.getIndexForConstructor(constructor));
+        assertEquals(constructor, DeclaredMemberIndexer.getConstructorForIndex(index, TestContainer.class));
+    }
+
+    private void verifyMethod(List<Method> methods, int index, String expectedName, Class<?>... expectedParamTypes) {
+        Method method = methods.get(index);
+        assertEquals(expectedName, method.getName());
+        assertEquals(method.getParameterTypes().length, expectedParamTypes.length);
+        for (int i = 0; i < expectedParamTypes.length; i++) {
+            if (!method.getParameterTypes()[i].getName().equals(expectedParamTypes[i].getName())) {
+                fail(method.getParameterTypes()[i].getName() + " ne " + expectedParamTypes[i].getName());
+            }
+        }
+        assertEquals(index, DeclaredMemberIndexer.getIndexForMethod(method));
+        assertEquals(method, DeclaredMemberIndexer.getMethodForIndex(index, TestContainer.class));
+    }
+
+    @SuppressWarnings("unused")
+    private static class TestContainer extends Container {
+
+        String foo;
+
+        private Integer bar;
+
+        public Date baz;
+
+        protected Boolean qux;
+
+        private TestContainer() {
+        }
+
+        protected TestContainer(String foo) {
+        }
+
+        TestContainer(String foo, Integer bar) {
+        }
+
+        public TestContainer(String bar, String foo) {
+        }
+
+        private TestContainer(Integer bar, String foo, BigDecimal qux) {
+        }
+
+        public void noParam() {
+        }
+
+        protected void foo(Integer p1) {
+        }
+
+        public void bar(Integer p1, String p2) {
+        }
+
+        private void bar(String p1, Integer p2) {
+        }
+
+        public void bar(String p1, Integer p2, Boolean p3) {
+        }
+
+    }
+
+    @SuppressWarnings("unused")
+    private static class Container {
+
+        protected String inherited;
+
+        protected Container() {
+        }
+
+        public Container(String[] foos) {
+        }
+
+        public String alpha() {
+            return null;
+        }
+
+    }
+
+}
