c0c22430919dd349d3a3e4739e18ac1164160501
Marko Luksa <marko.luksa@gmail.com>
2013-08-02 07:49:22 +0200

    WELD-1474 Direct Injection without a Proxy Breaking in a clustered Environment

--- tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/lifecycle/processBean/passivationCapable/SomeExtension.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/lifecycle/processBean/passivationCapable/SomeExtension.java
@@ -35,4 +35,8 @@ public class SomeExtension implements Extension {
             // nothing - test will fail
         }
     }
+
+    public boolean isIAEthrown() {
+        return IAEthrown;
+    }
 }
