4d9cbe88e96116f3eefc1ed78122bcc2a6b6c1e3
Marko Luksa <marko.luksa@gmail.com>
2013-09-02 09:28:39 +0200

    WELD-1488 Throw DefinitionException if a portable extension tries to add a bean with an invalid bean type

--- impl/src/main/java/org/jboss/weld/bean/attributes/ExternalBeanAttributesFactory.java
+++ impl/src/main/java/org/jboss/weld/bean/attributes/ExternalBeanAttributesFactory.java
@@ -19,13 +19,20 @@ package org.jboss.weld.bean.attributes;
 import static org.jboss.weld.logging.messages.MetadataMessage.NOT_A_QUALIFIER;
 import static org.jboss.weld.logging.messages.MetadataMessage.NOT_A_SCOPE;
 import static org.jboss.weld.logging.messages.MetadataMessage.NOT_A_STEREOTYPE;
+import static org.jboss.weld.logging.messages.MetadataMessage.PARAMETERIZED_TYPE_CONTAINING_WILDCARD_PARAMETER_IS_NOT_A_VALID_BEAN_TYPE;
 import static org.jboss.weld.logging.messages.MetadataMessage.QUALIFIERS_NULL;
 import static org.jboss.weld.logging.messages.MetadataMessage.SCOPE_NULL;
 import static org.jboss.weld.logging.messages.MetadataMessage.STEREOTYPES_NULL;
 import static org.jboss.weld.logging.messages.MetadataMessage.TYPES_EMPTY;
 import static org.jboss.weld.logging.messages.MetadataMessage.TYPES_NULL;
+import static org.jboss.weld.logging.messages.MetadataMessage.TYPE_VARIABLE_IS_NOT_A_VALID_BEAN_TYPE;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.GenericArrayType;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.lang.reflect.WildcardType;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -39,6 +46,7 @@ import org.jboss.weld.exceptions.DefinitionException;
  * well as basic validation of provided {@link BeanAttributes}.
  *
  * @author Jozef Hartinger
+ * @author Marko Luksa
  *
  */
 public class ExternalBeanAttributesFactory {
@@ -94,6 +102,37 @@ public class ExternalBeanAttributesFactory {
         checkNull(attributes.getTypes(), TYPES_NULL, attributes);
         if (attributes.getTypes().isEmpty()) {
             throw new DefinitionException(TYPES_EMPTY, attributes);
+        }
+        for (Type type : attributes.getTypes()) {
+            validateBeanType(type, attributes);
+        }
+    }
+
+    private static void validateBeanType(Type type, BeanAttributes<?> attributes) {
+        checkBeanTypeNotATypeVariable(type, type, attributes);
+        checkBeanTypeDoesNotContainWildcards(type, type, attributes);
+    }
+
+    private static void checkBeanTypeNotATypeVariable(Type beanType, Type type, BeanAttributes<?> attributes) {
+        if (type instanceof TypeVariable<?>) {
+            throw new DefinitionException(TYPE_VARIABLE_IS_NOT_A_VALID_BEAN_TYPE, beanType, attributes);
+        } else if (type instanceof GenericArrayType) {
+            GenericArrayType arrayType = (GenericArrayType) type;
+            checkBeanTypeNotATypeVariable(beanType, arrayType.getGenericComponentType(), attributes);
+        }
+    }
+
+    private static void checkBeanTypeDoesNotContainWildcards(Type beanType, Type type, BeanAttributes<?> attributes) {
+        if (type instanceof WildcardType) {
+            throw new DefinitionException(PARAMETERIZED_TYPE_CONTAINING_WILDCARD_PARAMETER_IS_NOT_A_VALID_BEAN_TYPE, beanType, attributes);
+        } else if (type instanceof ParameterizedType) {
+            ParameterizedType parameterizedType = (ParameterizedType) type;
+            for (Type typeArgument : parameterizedType.getActualTypeArguments()) {
+                checkBeanTypeDoesNotContainWildcards(beanType, typeArgument, attributes);
+            }
+        } else if (type instanceof GenericArrayType) {
+            GenericArrayType arrayType = (GenericArrayType) type;
+            checkBeanTypeDoesNotContainWildcards(beanType, arrayType.getGenericComponentType(), attributes);
         }
     }
 
