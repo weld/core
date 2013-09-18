586dc52f6fcfd481e72d162bdd15fe307962ff8d
Ron Smeral <rsmeral@redhat.com>
2013-09-18 18:45:05 +0200

    WELD-1415 Added test for cross-context forwarding

--- tests-arquillian/src/test/java/org/jboss/weld/tests/servlet/crosscontext/IncludingServlet.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/servlet/crosscontext/IncludingServlet.java
@@ -7,8 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/main")
-public class MainServlet extends HttpServlet {
+@WebServlet("/including")
+public class IncludingServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
