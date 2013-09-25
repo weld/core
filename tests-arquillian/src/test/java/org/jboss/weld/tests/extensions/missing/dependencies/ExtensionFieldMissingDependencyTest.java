5db00ae47704e36ceb66cfcf16c5cf92492f3f64
Martin Kouba <mkouba@redhat.com>
2013-09-25 13:44:22 +0200

    WELD-1515 Extension field missing dependency does not result in deployment problem anymore

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/missing/dependencies/ExtensionFieldMissingDependencyTest.java
@@ -0,0 +1,49 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2013, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.extensions.missing.dependencies;
+
+import static org.junit.Assert.assertFalse;
+
+import javax.enterprise.inject.spi.Extension;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.weld.tests.category.Integration;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+@Category(Integration.class)
+@RunWith(Arquillian.class)
+public class ExtensionFieldMissingDependencyTest {
+
+    @Deployment
+    public static WebArchive createTestArchive() {
+        return ShrinkWrap.create(WebArchive.class).addClasses(InvalidExtension.class, Flag.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addAsServiceProvider(Extension.class, InvalidExtension.class);
+    }
+
+    @Test
+    public void testExtensionDisregarded() {
+        assertFalse(Flag.isExtensionInvoked);
+    }
+
+}
