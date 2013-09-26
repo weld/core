15e0991b377e3510195fc5dcedd94eb7166c9ef0
Jozef Hartinger <jharting@redhat.com>
2013-09-26 12:36:58 +0200

    Do not use @Any in tests as it may clash with built-in beans (Batch)

--- tests-arquillian/src/test/java/org/jboss/weld/tests/producer/field/ListInstance.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/producer/field/ListInstance.java
@@ -16,14 +16,13 @@
  */
 package org.jboss.weld.tests.producer.field;
 
-import javax.enterprise.inject.Any;
+import java.util.List;
+
 import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
-import java.util.List;
 
 public class ListInstance {
     @Inject
-    @Any
     Instance<List<?>> instance;
 
     public Instance<List<?>> get() {
