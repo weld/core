e7a4d0dd7972fa1c9979d1a576a78a49ed84a0a7
Matej Briskar <mbriskar@redhat.com>
2013-11-13 17:38:30 +0100

    Added tests for enablement of interceptors using Priority annotation in WELD-SE

---
+++ environments/se/core/src/test/java/org/jboss/weld/environment/se/test/interceptors/priority/SimpleInterceptorBinding.java
@@ -0,0 +1,16 @@
+package org.jboss.weld.environment.se.test.interceptors.priority;
+
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.interceptor.InterceptorBinding;
+
+@InterceptorBinding
+@Target({ TYPE, METHOD })
+@Retention(RUNTIME)
+public @interface SimpleInterceptorBinding {
+}
