0bcc9d17e33fd9e79815ad67d96c153cc5e4a54e
Matej Briskar <mbriskar@redhat.com>
2013-11-14 12:24:51 +0100

    WELD-1550

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/visibility/PanelInterceptionBinding.java
@@ -0,0 +1,14 @@
+package org.jboss.weld.tests.interceptors.visibility;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.interceptor.InterceptorBinding;
+
+@InterceptorBinding
+@Target({ ElementType.METHOD, ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PanelInterceptionBinding {
+}
