52b6e13b43400c790e582b1d66432604dd9e3f77
Matus Abaffy <maabaffy@redhat.com>
2013-11-29 20:48:19 +0100

    WELD-1562 - Multiple specialization doesn't work

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/specialization/IndirectSpecializationNameInheritedTest.java
@@ -0,0 +1,47 @@
+package org.jboss.weld.tests.specialization;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Matus Abaffy
+ */
+@RunWith(Arquillian.class)
+public class IndirectSpecializationNameInheritedTest {
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap
+                .create(WebArchive.class)
+                .addClasses(BeanA.class, BeanB.class, BeanC.class, IndirectSpecializationNameInheritedTest.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Inject
+    private BeanManager beanManager;
+
+    @Inject
+    BeanA beanA;
+
+    /**
+     * WELD-1562
+     */
+    @Test
+    public void testSpecialization() {
+        Assert.assertEquals(BeanC.class, beanManager.resolve(beanManager.getBeans("beanA")).getBeanClass());
+
+        BeanC beanC = (BeanC) beanA;
+        Assert.assertEquals("PropA from BeanC here!", beanC.getPropA());
+        Assert.assertEquals("PropB from BeanC here!", beanC.getPropB());
+        Assert.assertEquals("PropC from BeanC here!", beanC.getPropC());
+    }
+}
