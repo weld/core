81b13d8475d60c43b63806b684a0dacddb986cbe
Martin Kouba <mkouba@redhat.com>
2013-12-13 10:59:38 +0100

    WELD-1564 Implement equals/hashCode for WeldInterceptorClassMetadata

--- impl/src/main/java/org/jboss/weld/bean/interceptor/WeldInterceptorClassMetadata.java
+++ impl/src/main/java/org/jboss/weld/bean/interceptor/WeldInterceptorClassMetadata.java
@@ -116,6 +116,37 @@ public class WeldInterceptorClassMetadata<T> implements ClassMetadata<T>, Serial
     }
 
     @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((type == null) ? 0 : type.hashCode());
+        return result;
+    }
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        WeldInterceptorClassMetadata other = (WeldInterceptorClassMetadata) obj;
+        if (type == null) {
+            if (other.type != null) {
+                return false;
+            }
+        } else if (!type.equals(other.type)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
         return "WeldInterceptorClassMetadata [type=" + type + "]";
     }
