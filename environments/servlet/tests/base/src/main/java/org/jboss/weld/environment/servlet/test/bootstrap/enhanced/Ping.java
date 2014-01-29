62a892fc236f2fa2cdaf07d7e31985baa48cbced
Martin Kouba <mkouba@redhat.com>
2014-01-29 13:26:41 +0100

    WELD-1581 Add EnhancedListenerTest - workaround Tomcat embedded and Maven Surefire classloading issue - upgrade Tomcat 7.x version to 7.0.50

---
+++ environments/servlet/tests/base/src/main/java/org/jboss/weld/environment/servlet/test/bootstrap/enhanced/Ping.java
@@ -0,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.servlet.test.bootstrap.enhanced;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.event.Observes;
+import javax.servlet.ServletContext;
+
+@ApplicationScoped
+public class Ping {
+
+    private AtomicInteger observations = new AtomicInteger(0);
+
+    public void observesApplicationContext(@Observes @Initialized(ApplicationScoped.class) ServletContext servletContext) {
+        observations.incrementAndGet();
+    }
+
+    public Integer getObservations() {
+        return observations.get();
+    }
+
+}
---
+++ environments/servlet/tests/tomcat/src/test/java/org/jboss/weld/environment/servlet/test/bootstrap/enhanced/EnhancedListenerTest.java
@@ -0,0 +1,44 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.servlet.test.bootstrap.enhanced;
+
+import static org.jboss.weld.environment.servlet.test.util.TomcatDeployments.CONTEXT_XML;
+
+import javax.servlet.ServletContainerInitializer;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.StringAsset;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.weld.environment.servlet.EnhancedListener;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class EnhancedListenerTest extends EnhancedListenerTestBase {
+
+    @Deployment
+    public static WebArchive deployment() {
+        WebArchive archive = EnhancedListenerTestBase.deployment().add(CONTEXT_XML, "META-INF/context.xml");
+        // WORKAROUND Tomcat embedded and Maven Surefire classloading issue
+        archive.addAsLibrary(ShrinkWrap.create(JavaArchive.class).addClass(EnhancedListener.class)
+                .addAsManifestResource(new StringAsset(EnhancedListener.class.getName()), "services/" + ServletContainerInitializer.class.getName()));
+        return archive;
+    }
+
+}
