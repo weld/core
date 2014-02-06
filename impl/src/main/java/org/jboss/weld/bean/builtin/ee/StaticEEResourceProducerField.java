7d98bcf5a476287aae000ebb4ba89f9e54ec3c72
Jozef Hartinger <jharting@redhat.com>
2014-02-06 15:49:05 +0100

    WELD-1505 Support static resource injections with InjectionServices

--- impl/src/main/java/org/jboss/weld/bean/builtin/ee/StaticEEResourceProducerField.java
+++ impl/src/main/java/org/jboss/weld/bean/builtin/ee/StaticEEResourceProducerField.java
@@ -18,6 +18,9 @@ package org.jboss.weld.bean.builtin.ee;
 
 import static org.jboss.weld.util.reflection.Reflections.cast;
 
+import java.lang.reflect.Field;
+import java.security.AccessController;
+
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.BeanAttributes;
 
@@ -25,11 +28,15 @@ import org.jboss.weld.annotated.enhanced.EnhancedAnnotatedField;
 import org.jboss.weld.bean.AbstractClassBean;
 import org.jboss.weld.bean.DisposalMethod;
 import org.jboss.weld.bootstrap.api.ServiceRegistry;
+import org.jboss.weld.exceptions.WeldException;
 import org.jboss.weld.injection.FieldInjectionPoint;
+import org.jboss.weld.injection.InjectionContextImpl;
 import org.jboss.weld.injection.InjectionPointFactory;
 import org.jboss.weld.injection.ResourceInjection;
 import org.jboss.weld.injection.ResourceInjectionFactory;
 import org.jboss.weld.manager.BeanManagerImpl;
+import org.jboss.weld.security.GetAccessibleCopyOfMember;
+import org.jboss.weld.util.reflection.Reflections;
 
 /**
  * A resource producer field that is static (not injected).
@@ -48,12 +55,20 @@ public class StaticEEResourceProducerField<X, T> extends EEResourceProducerField
     }
 
     private final ResourceInjection<T> resourceInjection;
+    private final Field accessibleField;
+    private final InjectionContextImpl<X> injectionContext; // injection context is shared since it does not use instance state
 
     protected StaticEEResourceProducerField(BeanAttributes<T> attributes, EnhancedAnnotatedField<T, ? super X> field,
             AbstractClassBean<X> declaringBean, DisposalMethod<X, ?> disposalMethod, BeanManagerImpl manager,
             ServiceRegistry services) {
         super(attributes, field, declaringBean, disposalMethod, manager, services);
         this.resourceInjection = getResourceInjection(field, declaringBean, manager);
+        this.accessibleField = AccessController.doPrivileged(new GetAccessibleCopyOfMember<Field>(field.getJavaMember()));
+        this.injectionContext = new InjectionContextImpl<X>(manager, declaringBean.getInjectionTarget(), declaringBean.getAnnotated(), null) {
+            @Override
+            public void proceed() {
+            }
+        };
     }
 
     protected ResourceInjection<T> getResourceInjection(EnhancedAnnotatedField<T, ? super X> field,
@@ -64,9 +79,15 @@ public class StaticEEResourceProducerField<X, T> extends EEResourceProducerField
 
     @Override
     public T create(CreationalContext<T> creationalContext) {
-        if (resourceInjection == null) {
-            return null; // may happen if a resource IP is defined in an environment that does not support resource injection
+        if (resourceInjection != null) {
+            return resourceInjection.getResourceReference(creationalContext);
+        } else {
+            injectionContext.run();
+            try {
+                return Reflections.cast(accessibleField.get(null));
+            } catch (IllegalAccessException e) {
+                throw new WeldException(e);
+            }
         }
-        return resourceInjection.getResourceReference(creationalContext);
     }
 }
