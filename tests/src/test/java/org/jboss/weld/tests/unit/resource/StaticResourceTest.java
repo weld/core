8b896b9e09b8c2647507ebffaeab13e9ac0c6d31
Jozef Hartinger <jharting@redhat.com>
2014-03-05 10:33:07 +0100

    WELD-1505 Tests

---
+++ tests/src/test/java/org/jboss/weld/tests/unit/resource/StaticResourceTest.java
@@ -0,0 +1,76 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.unit.resource;
+
+import org.jboss.arquillian.container.weld.ee.embedded_1_1.mock.AbstractDeployment;
+import org.jboss.arquillian.container.weld.ee.embedded_1_1.mock.BeanDeploymentArchiveImpl;
+import org.jboss.weld.bean.builtin.ee.StaticEEResourceProducerField;
+import org.jboss.weld.bootstrap.WeldBootstrap;
+import org.jboss.weld.bootstrap.api.Environments;
+import org.jboss.weld.bootstrap.spi.BeanDeploymentArchive;
+import org.jboss.weld.bootstrap.spi.Deployment;
+import org.jboss.weld.injection.spi.InjectionServices;
+import org.jboss.weld.manager.BeanManagerImpl;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Verifies, that {@link StaticEEResourceProducerField} invokes
+ * {@link InjectionServices#aroundInject(org.jboss.weld.injection.spi.InjectionContext)}
+ * to let it inject a static resource.
+ *
+ * @author Jozef Hartinger
+ *
+ * @see WELD-1505
+ *
+ */
+public class StaticResourceTest {
+
+    @Test
+    public void testStaticResourceInjectionWithInjectionServices() {
+
+        // Create the BDA in which we will deploy FooExtension. This BDA does not have access to bda2
+        final BeanDeploymentArchive bda = new BeanDeploymentArchiveImpl("1", InjectedClass.class) {
+            @Override
+            protected void configureServices() {
+                getServices().add(InjectionServices.class, new TestInjectionServices());
+            }
+        };
+
+        // Create a deployment, that we can use to mirror the structure of one Extension inside a BDA, and one outside
+        Deployment deployment = new AbstractDeployment(new BeanDeploymentArchive[] { bda }) {
+
+            @Override
+            public BeanDeploymentArchive loadBeanDeploymentArchive(Class<?> beanClass) {
+                return bda;
+            }
+        };
+
+        final WeldBootstrap bootstrap = new WeldBootstrap();
+        bootstrap.startContainer(Environments.EE, deployment).startInitialization().deployBeans().validateBeans().endInitialization();
+
+        try {
+            final BeanManagerImpl manager = bootstrap.getManager(bda);
+            final SpecialResource resource = manager.instance().select(SpecialResource.class).get();
+            Assert.assertNotNull(resource);
+            Assert.assertEquals(resource.getName(), TestInjectionServices.RESOURCE_NAME);
+        } finally {
+            bootstrap.shutdown();
+        }
+
+    }
+}
---
+++ tests/src/test/java/org/jboss/weld/tests/unit/resource/TestInjectionServices.java
@@ -0,0 +1,68 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.unit.resource;
+
+import java.lang.reflect.Field;
+
+import javax.enterprise.inject.spi.AnnotatedField;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.InjectionTarget;
+
+import org.jboss.weld.injection.spi.InjectionContext;
+import org.jboss.weld.injection.spi.InjectionServices;
+
+class TestInjectionServices implements InjectionServices {
+
+    public static final String RESOURCE_NAME = "injected resource";
+
+    private Field field;
+
+    @Override
+    public <T> void registerInjectionTarget(InjectionTarget<T> injectionTarget, AnnotatedType<T> annotatedType) {
+        if (annotatedType.getJavaClass().equals(InjectedClass.class)) {
+            for (AnnotatedField<?> field : annotatedType.getFields()) {
+                if (field.getJavaMember().getName().equals("resource")) {
+                    this.field = field.getJavaMember();
+                    this.field.setAccessible(true);
+                }
+            }
+        }
+    }
+
+    private void setField(Object value) {
+        if (field == null) {
+            throw new IllegalStateException("field is null");
+        }
+        try {
+            this.field.set(null, value);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public <T> void aroundInject(InjectionContext<T> injectionContext) {
+        if (injectionContext.getTarget() == null && injectionContext.getAnnotatedType().getJavaClass().equals(InjectedClass.class)) {
+            setField(new SpecialResource(RESOURCE_NAME));
+        }
+    }
+
+    @Override
+    public void cleanup() {
+        setField(null);
+    }
+}
