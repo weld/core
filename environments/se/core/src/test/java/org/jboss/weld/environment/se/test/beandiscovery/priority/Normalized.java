6d869482e2e3c517f8dc3185b020ab2dcabe03e8
Ron Smeral <rsmeral@redhat.com>
2014-03-25 15:32:33 +0100

    Test for Priority-enabled alternative/decorator/interceptor with enabled archive isolation

---
+++ environments/se/core/src/test/java/org/jboss/weld/environment/se/test/beandiscovery/priority/Normalized.java
@@ -0,0 +1,16 @@
+package org.jboss.weld.environment.se.test.beandiscovery.priority;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.interceptor.InterceptorBinding;
+
+@Inherited
+@InterceptorBinding
+@Retention(RUNTIME)
+@Target({METHOD, TYPE})
+public @interface Normalized {
+}
