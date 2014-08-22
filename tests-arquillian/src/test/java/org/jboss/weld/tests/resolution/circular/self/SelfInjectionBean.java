634dc294c146fbf98cceb60d3b0d3d6dcae91f8d
Kirill Gaevskii <kgaevski@redhat.com>
2014-08-22 10:58:09 +0200

    Testcase for WELD-1722

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/resolution/circular/self/SelfInjectionBean.java
@@ -0,0 +1,44 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.resolution.circular.self;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+
+/**
+ * @author Kirill Gaevskii
+ *
+ */
+@RequestScoped
+public class SelfInjectionBean {
+    
+    @Inject
+    private SelfInjectionBean self;
+
+    public Integer methodA(Integer value) {
+        return self.methodC(value);
+    }
+    
+    public Integer methodB(Integer value) {
+        return this.methodC(value);
+    }
+    
+    @SelfIntercept
+    public Integer methodC(Integer value) {
+        return value;
+    }
+}
