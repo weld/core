58cfc2007910fe48d35f84e1ef59fec0e9569fe2
Tomas Remes <tremes@redhat.com>
2014-09-30 14:06:38 +0200

    WELD-1752 servlet api abstraction for javax.servlet.ServletRequest#isAsyncStarted

--- impl/src/main/java/org/jboss/weld/servlet/ServletApiAbstraction.java
+++ impl/src/main/java/org/jboss/weld/servlet/ServletApiAbstraction.java
@@ -16,7 +16,11 @@
  */
 package org.jboss.weld.servlet;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import javax.servlet.ServletRequest;
 import org.jboss.weld.bootstrap.api.Service;
+import org.jboss.weld.logging.ServletLogger;
 import org.jboss.weld.resources.spi.ResourceLoader;
 import org.jboss.weld.util.ApiAbstraction;
 import org.jboss.weld.util.reflection.Reflections;
@@ -25,20 +29,59 @@ public class ServletApiAbstraction extends ApiAbstraction implements Service {
 
     public static final String SERVLET_CONTEXT_CLASS_NAME = "javax.servlet.ServletContext";
     private static final String ASYNC_LISTENER_CONTEXT_CLASS_NAME = "javax.servlet.AsyncListener";
+    private static final String SERVLET_REQUEST_CLASS_NAME = "javax.servlet.ServletRequest";
+    private static final String IS_ASYNC_STARTED_METHOD_NAME = "isAsyncStarted";
 
     private final boolean asyncSupported;
+
+    private final Method isAsyncStartedMethod;
 
     public ServletApiAbstraction(ResourceLoader resourceLoader) {
         super(resourceLoader);
         this.asyncSupported = Reflections.isClassLoadable(ASYNC_LISTENER_CONTEXT_CLASS_NAME, resourceLoader);
+        Method isAsyncStartedMethodLocal = null;
+
+        Class<ServletRequest> servletRequestClass = Reflections.loadClass(SERVLET_REQUEST_CLASS_NAME, resourceLoader);
+        if (servletRequestClass != null) {
+            try {
+                isAsyncStartedMethodLocal = servletRequestClass.getMethod(IS_ASYNC_STARTED_METHOD_NAME);
+            } catch (NoSuchMethodException e) {
+                ServletLogger.LOG.servlet2Environment();
+            }
+        }
+        isAsyncStartedMethod = isAsyncStartedMethodLocal;
     }
 
     /**
      * Indicates, whether the version of Servlet API is available that has support for async processing (Servlet 3.0 and better)
+     *
      * @return
      */
     public boolean isAsyncSupported() {
         return asyncSupported;
+    }
+
+    /**
+     * Evaluates javax.servlet.ServletRequest#isAsyncStarted() method on given ServletRequest instance via reflection api.
+     *
+     * @param request
+     * @return true if servletRequest started asynchronously, otherwise returns false.
+     */
+    public boolean isAsyncStarted(ServletRequest request) {
+
+        if (isAsyncStartedMethod != null) {
+            try {
+                return (Boolean) isAsyncStartedMethod.invoke(request);
+            } catch (IllegalAccessException e) {
+                ServletLogger.LOG.error(e);
+                return false;
+            } catch (InvocationTargetException e) {
+                ServletLogger.LOG.error(e);
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
 
     @Override
