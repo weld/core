1e5a1d37d7462897e61d93ce4a9df7b2a3731fe6
Tomas Remes <tremes@redhat.com>
2014-12-03 08:45:13 +0100

    Add few basic tests for collections.

--- impl/src/test/java/org/jboss/weld/tests/unit/util/collections/WeldCollectionsTest.java
+++ impl/src/test/java/org/jboss/weld/tests/unit/util/collections/WeldCollectionsTest.java
@@ -18,13 +18,15 @@ package org.jboss.weld.tests.unit.util.collections;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 
 import org.jboss.weld.util.collections.WeldCollections;
 import org.junit.Test;
 
 /**
- *
  * @author Martin Kouba
  */
 public class WeldCollectionsTest {
@@ -32,6 +34,24 @@ public class WeldCollectionsTest {
     @Test
     public void testToMultiRowString() {
         assertEquals("\n  - Foo,\n  - Bar,\n  - Baz", WeldCollections.toMultiRowString(Arrays.asList("Foo", "Bar", "Baz")));
+    }
+
+    @Test
+    public void testSorting() {
+        List<String> list = new ArrayList<>();
+        list.add("ab");
+        list.add("a");
+        list.add("abc");
+        List<String> sortedList = WeldCollections.sort(list, new Comparator<String>() {
+            @Override public int compare(String o1, String o2) {
+                return o1.length() != o2.length() ? (o1.length() < o2.length() ? -1 : 1) : 0;
+            }
+        });
+        assertEquals(list.size(), sortedList.size());
+        assertEquals(sortedList.get(0), "a");
+        assertEquals(sortedList.get(1), "ab");
+        assertEquals(sortedList.get(2), "abc");
+
     }
 
 }
