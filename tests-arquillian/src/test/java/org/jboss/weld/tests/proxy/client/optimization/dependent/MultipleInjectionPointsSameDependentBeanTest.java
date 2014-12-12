52c8c607353c521c7fc2a14a23662d95ee9b913c
Martin Kouba <mkouba@redhat.com>
2014-12-12 13:57:09 +0100

    WELD-1810 Add test for managed beans

--- tests-arquillian/src/test/java/org/jboss/weld/tests/proxy/client/optimization/dependent/MultipleInjectionPointsSameDependentBeanTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/proxy/client/optimization/dependent/MultipleInjectionPointsSameDependentBeanTest.java
@@ -14,45 +14,44 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.weld.tests.ejb.dependent.stateful;
+package org.jboss.weld.tests.proxy.client.optimization.dependent;
 
-import javax.inject.Inject;
-
-import junit.framework.Assert;
+import static org.junit.Assert.assertFalse;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.weld.config.ConfigurationKey;
-import org.jboss.weld.tests.category.Integration;
 import org.jboss.weld.tests.util.PropertiesBuilder;
 import org.junit.Test;
-import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
 /**
- * See http://lists.jboss.org/pipermail/cdi-dev/2014-December/005800.html for details
- * @author Steve Millidge
+ * This test should verify that enabled injectable reference lookup optimization does not break multiple injection points of the
+ * same dependent managed bean.
  *
+ * @author Martin Kouba
+ * @see WELD-1810
  */
 @RunWith(Arquillian.class)
-@Category(Integration.class)
-public class DependentStatefulSessionBeanNotSharedTest {
-
-    @Inject
-    private Injection injection;
+public class MultipleInjectionPointsSameDependentBeanTest {
 
     @Deployment
-    public static Archive<?> getDeployment() {
-        return ShrinkWrap.create(WebArchive.class).addPackage(DependentStatefulSessionBeanNotSharedTest.class.getPackage()).addAsResource(
-                PropertiesBuilder.newBuilder().set(ConfigurationKey.INJECTABLE_REFERENCE_OPTIMIZATION.get(), "false")
-                .build(), "weld.properties");
+    public static Archive<?> createTestArchive() {
+        return ShrinkWrap
+                .create(BeanArchive.class)
+                .addClasses(FooRequest.class, FooApplication.class, Bar.class)
+                .addAsResource(
+                        PropertiesBuilder.newBuilder().set(ConfigurationKey.INJECTABLE_REFERENCE_OPTIMIZATION.get(), "true")
+                                .build(), "weld.properties");
     }
 
     @Test
-    public void testDependentStatefulSessionBeanNotSharedBetweenInjectionPoints() {
-        Assert.assertEquals("init", injection.invokeStatefulService());
+    public void testOptimizationDoesNotAffectDependentBeans(FooRequest fooRequest, FooApplication fooApplication) {
+        assertFalse(fooRequest.getBar1().getId().equals(fooRequest.getBar2().getId()));
+        assertFalse(fooApplication.getBar1().getId().equals(fooApplication.getBar2().getId()));
     }
+
 }
