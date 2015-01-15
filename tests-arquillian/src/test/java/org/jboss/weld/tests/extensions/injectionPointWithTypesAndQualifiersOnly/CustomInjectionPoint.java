f822a76b489c9507ada0f13f11bd9289c2d8ea03
Kirill Gaevskii <kgaevski@redhat.com>
2015-01-15 13:46:29 +0100

    WELD-1836 TestCase for calling getInjectableReference() with InjectionPoint wich can be used as a source of types and qualifiers only

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/injectionPointWithTypesAndQualifiersOnly/CustomInjectionPoint.java
@@ -0,0 +1,72 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc. and/or its affiliates, and individual
+ * contributors by the @authors tag. See the copyright.txt in the
+ * distribution for a full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.extensions.injectionPointWithTypesAndQualifiersOnly;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.enterprise.inject.Vetoed;
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.jboss.weld.literal.DefaultLiteral;
+
+/**
+ * @author Kirill Gaevskii
+ */
+@Vetoed
+public class CustomInjectionPoint implements InjectionPoint {
+
+    @Override
+    public Type getType() {
+        return DummyBean.class;
+    }
+
+    @Override
+    public Set<Annotation> getQualifiers() {
+        return Collections.<Annotation> singleton(DefaultLiteral.INSTANCE);
+    }
+
+    @Override
+    public Bean<?> getBean() {
+        return null;
+    }
+
+    @Override
+    public Member getMember() {
+        return null;
+    }
+
+    @Override
+    public Annotated getAnnotated() {
+        return null;
+    }
+
+    @Override
+    public boolean isDelegate() {
+        return false;
+    }
+
+    @Override
+    public boolean isTransient() {
+        return false;
+    }
+}
