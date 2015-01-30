4992f73d937a542e8353be531908651fdf56120b
Kirill Gaevskii <kgaevski@redhat.com>
2015-01-30 13:15:36 +0100

    Getting rid auto-generated TODO comments

--- tests-arquillian/src/test/java/org/jboss/weld/tests/enterprise/lifecycle/MockCreationalContext.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/enterprise/lifecycle/MockCreationalContext.java
@@ -21,12 +21,9 @@ import javax.enterprise.context.spi.CreationalContext;
 public class MockCreationalContext<T> implements CreationalContext<T> {
 
     public void push(T incompleteInstance) {
-
     }
 
     public void release() {
-        // TODO Auto-generated method stub
-
     }
 
 }
--- tests-arquillian/src/test/java/org/jboss/weld/tests/resolution/Wibble.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/resolution/Wibble.java
@@ -1,38 +1,37 @@
 package org.jboss.weld.tests.resolution;
 
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.inject.Inject;
-import javax.inject.Named;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.Set;
 
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.inject.Named;
+
 @RequestScoped
 @Named
 public class Wibble implements Map<String, ResourceBundle>, Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Inject
     BeanManager beanManager;
 
     public int size() {
-        // TODO Auto-generated method stub
         return 0;
     }
 
     public boolean isEmpty() {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean containsKey(Object key) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     public boolean containsValue(Object value) {
-        // TODO Auto-generated method stub
         return false;
     }
 
@@ -44,37 +43,28 @@ public class Wibble implements Map<String, ResourceBundle>, Serializable {
     }
 
     public ResourceBundle put(String key, ResourceBundle value) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public ResourceBundle remove(Object key) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public void putAll(Map<? extends String, ? extends ResourceBundle> m) {
-        // TODO Auto-generated method stub
-
     }
 
     public void clear() {
-        // TODO Auto-generated method stub
-
     }
 
     public Set<String> keySet() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Collection<ResourceBundle> values() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     public Set<java.util.Map.Entry<String, ResourceBundle>> entrySet() {
-        // TODO Auto-generated method stub
         return null;
     }
 
