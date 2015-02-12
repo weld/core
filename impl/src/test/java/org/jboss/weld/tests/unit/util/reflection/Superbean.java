1068a5ca757c8e7bb833f2d93884fb6fc6b20925
Martin Kouba <mkouba@redhat.com>
2015-02-12 15:16:35 +0100

    WELD-1853 Print member line on Definition/Deployment exception if possible

---
+++ impl/src/test/java/org/jboss/weld/tests/unit/util/reflection/Superbean.java
@@ -0,0 +1,32 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.unit.util.reflection;
+
+import java.util.List;
+
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+
+public class Superbean {
+
+    // Dummy comment
+    @Inject
+    void foo(@Small Vanilla vanilla, BeanManager beanManager, List<String> list) {
+        return;
+    }
+
+}
