0c67fa8f50032c860499e2f1e48bf126735a3c4f
Jozef Hartinger <jharting@redhat.com>
2015-02-17 11:29:43 +0100

    Minor changes to weld-pastecode that show off probe event monitoring

--- examples/jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/session/CodeFragmentPrinter.java
+++ examples/jsf/pastecode/src/main/java/org/jboss/weld/examples/pastecode/session/CodeFragmentPrinter.java
@@ -1,10 +1,33 @@
 package org.jboss.weld.examples.pastecode.session;
 
-import javax.ejb.Local;
+import java.util.logging.Logger;
 
-@Local
-public interface CodeFragmentPrinter {
+import javax.ejb.Stateless;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 
-    void startTimer();
+import org.jboss.weld.examples.pastecode.model.CodeFragment;
 
+@Stateless
+public class CodeFragmentPrinter {
+
+    @Inject
+    private CodeFragmentLogger logger;
+
+    @Inject
+    private Logger log;
+
+    public void print(@Observes TimerEvent event) {
+        // Print the code fragments retrieved in the last minute to the log
+        if (logger.getLog().size() > 0) {
+            log.info("These code fragments pasted in the last minute: ");
+            for (CodeFragment fragment : logger.getLog()) {
+                log.info(fragment.toString());
+            }
+            log.info("-----------------------------------------------------");
+            logger.clearLog();
+        } else {
+            log.info("No fragments pasted in the last minute");
+        }
+    }
 }
