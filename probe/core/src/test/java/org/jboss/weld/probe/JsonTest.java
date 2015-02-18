990cfe112e81603dd90e716c1c3c84b9b2d8806b
Martin Kouba <mkouba@redhat.com>
2015-02-18 11:45:12 +0100

    Probe JSON generator - simplify the use a little bit

--- probe/core/src/test/java/org/jboss/weld/probe/JsonTest.java
+++ probe/core/src/test/java/org/jboss/weld/probe/JsonTest.java
@@ -17,11 +17,13 @@
 package org.jboss.weld.probe;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
+import org.jboss.weld.probe.Json.JsonArrayBuilder;
+import org.jboss.weld.probe.Json.JsonObjectBuilder;
 import org.junit.Test;
 
 /**
- * TODO add more complex test
  *
  * @author Martin Kouba
  */
@@ -29,18 +31,35 @@ public class JsonTest {
 
     @Test
     public void testJsonArrayBuilder() {
-        assertEquals("[\"foo\",\"bar\",[\"baz\"]]", Json.newArrayBuilder().add("foo").add("bar").add(Json.newArrayBuilder().add("baz")).build());
+        assertEquals("[\"foo\",\"bar\",[\"baz\"]]", Json.arrayBuilder().add("foo").add("bar").add(Json.arrayBuilder().add("baz")).build());
     }
 
     @Test
     public void testJsonObjectBuilder() {
-        assertEquals("{\"foo\":\"bar\",\"baz\":[\"qux\"]}", Json.newObjectBuilder().add("foo", "bar").add("baz", Json.newArrayBuilder().add("qux")).build());
+        assertEquals("{\"foo\":\"bar\",\"baz\":[\"qux\"]}", Json.objectBuilder().add("foo", "bar").add("baz", Json.arrayBuilder().add("qux")).build());
     }
 
     @Test
     public void testIgnoreEmptyBuilders() {
-        assertEquals("[true]", Json.newArrayBuilder().setIgnoreEmptyBuilders(true).add(true).add(Json.newObjectBuilder().add("foo", Json.newObjectBuilder()))
-                .setIgnoreEmptyBuilders(true).build());
+        assertEquals("[true]", Json.arrayBuilder(true).add(true).add(Json.objectBuilder(true).add("foo", Json.objectBuilder())).build());
+
+        JsonObjectBuilder objectBuilder = Json.objectBuilder();
+        JsonArrayBuilder arrayBuilder = Json.arrayBuilder().add(objectBuilder);
+        objectBuilder.add("foo", "bar");
+        assertEquals("[{\"foo\":\"bar\"}]", arrayBuilder.build());
+    }
+
+    @Test
+    public void testABitMoreComplexStructure() {
+        JsonObjectBuilder builder = Json
+                .objectBuilder()
+                .add("items", Json.arrayBuilder().add(1).add(2))
+                .add("name", "Foo")
+                .add("parent",
+                        Json.objectBuilder(true).add("name", "Martin").add("age", 100).add("active", true)
+                                .add("children", Json.arrayBuilder(true).add(Json.objectBuilder())));
+        assertFalse(builder.isEmpty());
+        assertEquals("{\"items\":[1,2],\"name\":\"Foo\",\"parent\":{\"name\":\"Martin\",\"age\":100,\"active\":true}}", builder.build());
     }
 
 }
