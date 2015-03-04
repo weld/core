d439da3d2c3e332d33ed8f064bddf3a835787923
Ron Smeral <rsmeral@redhat.com>
2015-03-04 12:22:40 +0100

    Probe: integration test refactoring+fixes

---
+++ probe/integration-tests/src/test/java/org/jboss/weld/probe/tests/integration/ProbeInvocationsTest.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.probe.tests.integration;
+
+import static junit.framework.Assert.assertTrue;
+import static org.jboss.weld.probe.Strings.DATA;
+import static org.jboss.weld.probe.Strings.METHOD_NAME;
+import static org.jboss.weld.probe.tests.integration.JSONTestUtil.INVOCATIONS_PATH;
+import static org.jboss.weld.probe.tests.integration.JSONTestUtil.getPageAsJSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.weld.probe.tests.integration.deployment.InvokingServlet;
+import org.jboss.weld.probe.tests.integration.deployment.annotations.Collector;
+import org.jboss.weld.probe.tests.integration.deployment.beans.ModelBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Tomas Remes
+ */
+@RunWith(Arquillian.class)
+public class ProbeInvocationsTest extends ProbeIntegrationTest {
+
+    @ArquillianResource
+    private URL url;
+
+    private static final String TEST_ARCHIVE_NAME = "probe-invocations-test";
+
+    @Deployment(testable = false)
+    public static WebArchive deploy() {
+        return ShrinkWrap.create(WebArchive.class, TEST_ARCHIVE_NAME + ".war")
+                .addAsWebInfResource(ProbeInvocationsTest.class.getPackage(), "web.xml", "web.xml")
+                .addAsWebInfResource(ProbeInvocationsTest.class.getPackage(), "beans.xml", "beans.xml")
+                .addClass(InvokingServlet.class)
+                .addPackage(ModelBean.class.getPackage())
+                .addPackage(Collector.class.getPackage())
+                // exclude InvokingServlet from monitoring
+                .addAsResource("weld.properties", "weld.properties");
+    }
+
+    @Test
+    public void testInvocationsEndpoint() throws IOException {
+        WebClient client = invokeSimpleAction(url);
+        JsonObject invocations = getPageAsJSONObject(INVOCATIONS_PATH, url, client);
+        JsonArray invocationData = invocations.getJsonArray(DATA);
+        assertTrue("No invocations in invocation tree!", invocationData.size() > 0);
+        assertTrue(checkStringInArrayRecursively("simpleCall", METHOD_NAME, invocationData, false));
+    }
+
+}
