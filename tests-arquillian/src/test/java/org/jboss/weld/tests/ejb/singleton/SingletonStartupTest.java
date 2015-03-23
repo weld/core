0b6129c40a13f1298a7e4e7fdc760b099a764406
Martin Kouba <mkouba@redhat.com>
2015-03-23 13:57:07 +0100

    WFLY-4438 Only test that constructor is not called per method invocation

--- tests-arquillian/src/test/java/org/jboss/weld/tests/ejb/singleton/SingletonStartupTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/ejb/singleton/SingletonStartupTest.java
@@ -1,6 +1,23 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.tests.ejb.singleton;
 
-import junit.framework.Assert;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -19,8 +36,7 @@ public class SingletonStartupTest {
 
     @Deployment
     public static Archive<?> deploy() {
-        return ShrinkWrap.create(BeanArchive.class)
-                .addPackage(SingletonStartupTest.class.getPackage());
+        return ShrinkWrap.create(BeanArchive.class).addPackage(SingletonStartupTest.class.getPackage());
     }
 
     @Test
@@ -29,11 +45,13 @@ public class SingletonStartupTest {
     }
 
     @Test
-    @Ignore("WELD-1885")
+    @Ignore("WFLY-4438")
     public void testSingletonStartupCount(Foo foo) {
-        for (int i = 0; foo.getSomeValue() && i < 10; i++) {
+        int count = 10;
+        for (int i = 0; foo.getSomeValue() && i < count; i++) {
         }
-        Assert.assertEquals(1, Foo.getCountOfPostConstructCalled());
-        Assert.assertEquals(1, Foo.getCountOfConstructCalled());
+        assertEquals(1, Foo.getCountOfPostConstructCalled());
+        // Only test that constructor is not called per method invocation
+        assertTrue(Foo.getCountOfConstructorCalled() < count);
     }
 }
