24d6e1225a90978d373632b78c8c7b987899fbdd
Martin Kouba <mkouba@redhat.com>
2015-03-23 13:57:07 +0100

    WFLY-4438 Only test that constructor is not called per method invocation

--- tests-arquillian/src/test/java/org/jboss/weld/tests/ejb/singleton/Foo.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/ejb/singleton/Foo.java
@@ -1,3 +1,19 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.tests.ejb.singleton;
 
 import java.util.concurrent.atomic.AtomicInteger;
@@ -13,19 +29,19 @@ import javax.enterprise.context.ApplicationScoped;
 public class Foo {
 
     private static AtomicInteger countOfPostConstructCalled = new AtomicInteger();
-    private static AtomicInteger countOfConstructCalled = new AtomicInteger();
+    private static AtomicInteger countOfConstructorCalled = new AtomicInteger();
 
     public static boolean isPostConstructCalled() {
         return countOfPostConstructCalled.get() > 0;
     }
 
     public Foo() {
-        Foo.countOfConstructCalled.incrementAndGet();
+        Foo.countOfConstructorCalled.incrementAndGet();
     }
 
     public static void reset() {
         countOfPostConstructCalled.set(0);
-        countOfConstructCalled.set(0);
+        countOfConstructorCalled.set(0);
     }
 
     @PostConstruct
@@ -37,8 +53,8 @@ public class Foo {
         return countOfPostConstructCalled.get();
     }
 
-    public static int getCountOfConstructCalled() {
-        return countOfConstructCalled.get();
+    public static int getCountOfConstructorCalled() {
+        return countOfConstructorCalled.get();
     }
 
     public boolean getSomeValue() {
