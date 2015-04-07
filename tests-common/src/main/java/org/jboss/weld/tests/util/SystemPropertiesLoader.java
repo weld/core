389f137fbdec5e2a94e5e79a87ad935f8290c483
Tomas Remes <tremes@redhat.com>
2015-04-07 09:43:08 +0200

    Move PropertiesBuilder to tests-common.

--- tests-common/src/main/java/org/jboss/weld/tests/util/SystemPropertiesLoader.java
+++ tests-common/src/main/java/org/jboss/weld/tests/util/SystemPropertiesLoader.java
@@ -10,7 +10,7 @@
  * You may obtain a copy of the License at
  * http://www.apache.org/licenses/LICENSE-2.0
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,  
+ * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
@@ -20,12 +20,10 @@ package org.jboss.weld.tests.util;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Properties;
-
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
-
 import org.jboss.weld.config.ConfigurationKey;
 
 /**
