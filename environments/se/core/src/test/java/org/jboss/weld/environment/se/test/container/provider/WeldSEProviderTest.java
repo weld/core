70f9c815d44a0d2cf97b5de0d1f57a708a1679f3
Martin Kouba <mkouba@redhat.com>
2015-04-10 09:15:38 +0200

    WELD-1890 WeldContainer extends CDI, implement WeldSEProvider

---
+++ environments/se/core/src/test/java/org/jboss/weld/environment/se/test/container/provider/WeldSEProviderTest.java
@@ -0,0 +1,68 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.se.test.container.provider;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import javax.enterprise.inject.spi.CDI;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
+
+/**
+ *
+ * @author Martin Kouba
+ */
+public class WeldSEProviderTest {
+
+    @Test(expected = IllegalStateException.class)
+    public void testNoContainer() {
+        CDI.current();
+    }
+
+    @Test
+    public void testSingleContainer() {
+        try (WeldContainer weldContainer = new Weld().disableDiscovery().beanClasses(Foo.class).initialize()) {
+            assertCdi(CDI.current(), weldContainer.getId());
+            assertCdi(weldContainer.select(Foo.class).get().getCurrent(), weldContainer.getId());
+        }
+    }
+
+    @Test
+    public void testMultipleContainers() {
+        Weld weld = new Weld().disableDiscovery();
+        try {
+            WeldContainer weldContainer1 = weld.containerId("foo").beanClasses(Foo.class).initialize();
+            WeldContainer weldContainer2 = weld.containerId("bar").beanClasses(Bar.class).initialize();
+            // The caller cannot be used to determine the right container - the first one is returned
+            assertCdi(CDI.current(), weldContainer1.getId());
+            // Foo is caller
+            assertCdi(weldContainer1.select(Foo.class).get().getCurrent(), weldContainer1.getId());
+            // Bar is caller
+            assertCdi(weldContainer2.select(Bar.class).get().getCurrent(), weldContainer2.getId());
+        } finally {
+            weld.shutdown();
+        }
+    }
+
+    private void assertCdi(CDI<Object> cdi, String id) {
+        assertTrue(cdi instanceof WeldContainer);
+        assertEquals(id, ((WeldContainer) cdi).getId());
+    }
+}
