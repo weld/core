d9db456bb9120f08b749822ff1164452fcaff253
Tomas Remes <tremes@redhat.com>
2015-05-28 14:07:39 +0200

    WELD-1950 Testcase

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/injectionPoint/weld1950/Weld1950InjectionPointTest.java
@@ -0,0 +1,31 @@
+package org.jboss.weld.tests.injectionPoint.weld1950;
+
+import javax.inject.Inject;
+
+import junit.framework.Assert;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class Weld1950InjectionPointTest {
+
+    @Inject
+    ModelBean modelBean;
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(BeanArchive.class).addPackage(Weld1950InjectionPointTest.class.getPackage());
+    }
+
+    @Test
+    public void testInjectionPointGetBean(UserProducer producer) {
+        modelBean.ping();
+        Assert.assertEquals(TestDependentBean.class, producer.getIp().getBean().getBeanClass());
+    }
+}
