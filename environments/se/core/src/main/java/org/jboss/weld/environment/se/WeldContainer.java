226ef28a65559db8e49499d36e9176cbf1f7da88
Martin Kouba <mkouba@redhat.com>
2015-07-29 10:09:27 +0200

    WELD-2003 Improve Weld.initialize() javadoc so that it is clear that a shutdown hook is registered automatically

--- environments/se/core/src/main/java/org/jboss/weld/environment/se/WeldContainer.java
+++ environments/se/core/src/main/java/org/jboss/weld/environment/se/WeldContainer.java
@@ -209,6 +209,8 @@ public class WeldContainer extends AbstractCDI<Object> implements AutoCloseable,
 
     /**
      * Shutdown the container.
+     *
+     * @see Weld#initialize()
      */
     public synchronized void shutdown() {
         if (isRunning()) {
