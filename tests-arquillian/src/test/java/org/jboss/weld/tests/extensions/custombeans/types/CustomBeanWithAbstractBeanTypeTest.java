380b02b05a49c3aa73ab3306406b443c335c13a2
Jozef Hartinger <jharting@redhat.com>
2015-08-12 17:50:30 +0200

    WELD-2010 Testcase

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/extensions/custombeans/types/CustomBeanWithAbstractBeanTypeTest.java
@@ -0,0 +1,53 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.extensions.custombeans.types;
+
+import javax.enterprise.inject.spi.Extension;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Testcase for https://issues.jboss.org/browse/WELD-2010
+ *
+ * @author Jozef Hartinger
+ * @author Arjan Tijms
+ *
+ */
+@RunWith(Arquillian.class)
+public class CustomBeanWithAbstractBeanTypeTest {
+
+    @Inject
+    private MyMap map;
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return ShrinkWrap.create(BeanArchive.class).addPackage(CustomBeanWithAbstractBeanTypeTest.class.getPackage())
+                .addAsServiceProvider(Extension.class, MyMapExtension.class);
+    }
+
+    @Test
+    public void test() {
+        map.clear();
+    }
+}
