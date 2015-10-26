29ed1749691293849119da424153f46c6e04937c
Martin Kouba <mkouba@redhat.com>
2015-10-26 16:05:31 +0100

    WELD-2048 InjectionTargetValidator - fix possible concurrency issue

--- impl/src/main/java/org/jboss/weld/manager/InjectionTargetValidator.java
+++ impl/src/main/java/org/jboss/weld/manager/InjectionTargetValidator.java
@@ -1,13 +1,30 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.manager;
+
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import javax.enterprise.inject.spi.InjectionTarget;
 
 import org.jboss.weld.Container;
 import org.jboss.weld.ContainerState;
 import org.jboss.weld.bootstrap.Validator;
 import org.jboss.weld.bootstrap.api.Service;
-
-import javax.enterprise.inject.spi.InjectionTarget;
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class InjectionTargetValidator implements Service {
 
@@ -18,7 +35,7 @@ public class InjectionTargetValidator implements Service {
 
     public InjectionTargetValidator(BeanManagerImpl beanManager) {
         this.validator = new Validator();
-        this.injectionTargets = new ArrayList<InjectionTarget<?>>();
+        this.injectionTargets = new ConcurrentLinkedQueue<InjectionTarget<?>>();
         this.container = Container.instance();
         this.beanManager = beanManager;
     }
