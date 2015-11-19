c0e63780b97af1bf0228df36877fcf83abe505c6
Martin Kouba <mkouba@redhat.com>
2015-11-19 11:39:28 +0100

    WELD-2065 Test resource injection in SE

---
+++ environments/se/tests/src/test/java/org/jboss/weld/environment/se/test/resource/Golf.java
@@ -0,0 +1,39 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2015, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.environment.se.test.resource;
+
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+public class Golf {
+
+    @Inject
+    Delta delta;
+
+    @PersistenceContext(unitName="myDb")
+    EntityManager entityManager;
+
+    public Delta getDelta() {
+        return delta;
+    }
+
+    public EntityManager getEntityManager() {
+        return entityManager;
+    }
+
+}
