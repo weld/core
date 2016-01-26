53eb5c66f443c160d2123a3ea19958fa08c14cd8
Martin Kouba <mkouba@redhat.com>
2016-01-26 11:21:37 +0100

    WELD-2008 InterceptionContext - change key of interceptor instances map

--- impl/src/main/java/org/jboss/weld/bean/interceptor/CustomInterceptorMetadata.java
+++ impl/src/main/java/org/jboss/weld/bean/interceptor/CustomInterceptorMetadata.java
@@ -1,4 +1,22 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2014, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.bean.interceptor;
+
+import java.io.Serializable;
 
 import javax.enterprise.inject.spi.Interceptor;
 
@@ -14,15 +32,18 @@ public class CustomInterceptorMetadata<T> implements InterceptorClassMetadata<T>
 
     @SuppressWarnings("unchecked")
     public static <T> CustomInterceptorMetadata<T> of(Interceptor<T> interceptor) {
-        return new CustomInterceptorMetadata<T>(new CdiInterceptorFactory<T>(interceptor), (Class<T>) interceptor.getBeanClass());
+        return new CustomInterceptorMetadata<T>(new CdiInterceptorFactory<T>(interceptor), (Class<T>) interceptor.getBeanClass(),
+                interceptor.getName() != null && !interceptor.getName().isEmpty() ? interceptor.getName() : null);
     }
 
     private final CdiInterceptorFactory<T> factory;
     private final Class<T> javaClass;
+    private final String key;
 
-    private CustomInterceptorMetadata(CdiInterceptorFactory<T> factory, Class<T> javaClass) {
+    private CustomInterceptorMetadata(CdiInterceptorFactory<T> factory, Class<T> javaClass, String key) {
         this.factory = factory;
         this.javaClass = javaClass;
+        this.key = key;
     }
 
     @Override
@@ -50,4 +71,10 @@ public class CustomInterceptorMetadata<T> implements InterceptorClassMetadata<T>
     public Class<T> getJavaClass() {
         return javaClass;
     }
+
+    @Override
+    public Serializable getKey() {
+        return key != null ? key : InterceptorClassMetadata.super.getKey();
+    }
+
 }
