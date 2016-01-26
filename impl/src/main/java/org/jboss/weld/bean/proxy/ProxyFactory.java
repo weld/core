16873c6393466d43fe4a9155b71d1a4c9096ac94
Martin Kouba <mkouba@redhat.com>
2016-01-26 13:32:55 +0100

    WELD-2093 Support interception of default methods

--- impl/src/main/java/org/jboss/weld/bean/proxy/ProxyFactory.java
+++ impl/src/main/java/org/jboss/weld/bean/proxy/ProxyFactory.java
@@ -587,7 +587,12 @@ public class ProxyFactory<T> implements PrivilegedAction<T> {
                         try {
                             MethodInformation methodInfo = new RuntimeMethodInformation(method);
                             ClassMethod classMethod = proxyClassType.addMethod(method);
-                            createSpecialMethodBody(classMethod, methodInfo, staticConstructor);
+                            if (Reflections.isDefault(method)) {
+                                addConstructedGuardToMethodBody(classMethod);
+                                createForwardingMethodBody(classMethod, methodInfo, staticConstructor);
+                            } else {
+                                createSpecialMethodBody(classMethod, methodInfo, staticConstructor);
+                            }
                             BeanLogger.LOG.addingMethodToProxy(method);
                         } catch (DuplicateMemberException e) {
                         }
@@ -652,6 +657,10 @@ public class ProxyFactory<T> implements PrivilegedAction<T> {
         createInterceptorBody(proxyClassType, method, staticConstructor);
     }
 
+    protected void addConstructedGuardToMethodBody(final ClassMethod classMethod) {
+        addConstructedGuardToMethodBody(classMethod, classMethod.getClassFile().getSuperclass());
+    }
+
     /**
      * Adds the following code to a delegating method:
      * <p/>
@@ -662,7 +671,7 @@ public class ProxyFactory<T> implements PrivilegedAction<T> {
      * This means that the proxy will not start to delegate to the underlying
      * bean instance until after the constructor has finished.
      */
-    protected void addConstructedGuardToMethodBody(final ClassMethod classMethod) {
+    protected void addConstructedGuardToMethodBody(final ClassMethod classMethod, String className) {
         if (!proxyInstantiator.isUsingConstructor()) {
             return;
         }
@@ -677,7 +686,7 @@ public class ProxyFactory<T> implements PrivilegedAction<T> {
         // this is run when the proxy is being constructed
         cond.aload(0);
         cond.loadMethodParameters();
-        cond.invokespecial(classMethod.getClassFile().getSuperclass(), classMethod.getName(), classMethod.getDescriptor());
+        cond.invokespecial(className, classMethod.getName(), classMethod.getDescriptor());
         cond.returnInstruction();
         cond.branchEnd(jumpMarker);
     }
