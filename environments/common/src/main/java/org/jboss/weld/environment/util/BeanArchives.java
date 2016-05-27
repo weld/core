cfa1dbe99e036d120c20b79ab43040dda16c3e5f
Martin Kouba <mkouba@redhat.com>
2016-05-27 13:24:49 +0200

    WELD-2161 Fix BeanArchives.extractBeanArchiveId()

--- environments/common/src/main/java/org/jboss/weld/environment/util/BeanArchives.java
+++ environments/common/src/main/java/org/jboss/weld/environment/util/BeanArchives.java
@@ -98,7 +98,7 @@ public final class BeanArchives {
      * @param beanArchiveRef
      * @param base
      * @param separator
-     * @return
+     * @return the extracted bean archive id
      */
     public static String extractBeanArchiveId(String beanArchiveRef, String base, String separator) {
         beanArchiveRef = beanArchiveRef.replace('\\', '/');
@@ -107,6 +107,8 @@ public final class BeanArchives {
         id.append(BeanArchives.BEAN_ARCHIVE_ID_BASE_DELIMITER);
         if (beanArchiveRef.contains(separator)) {
             id.append(beanArchiveRef.substring(beanArchiveRef.indexOf(separator), beanArchiveRef.length()));
+        } else {
+            id.append(beanArchiveRef);
         }
         return id.toString();
     }
