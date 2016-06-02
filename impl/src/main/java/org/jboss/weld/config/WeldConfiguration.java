aea809e0d781229b04d7f1c959fdb43dee7f433d
Martin Kouba <mkouba@redhat.com>
2016-06-02 11:03:30 +0200

    WeldConfiguration - minor javadoc fix

--- impl/src/main/java/org/jboss/weld/config/WeldConfiguration.java
+++ impl/src/main/java/org/jboss/weld/config/WeldConfiguration.java
@@ -53,9 +53,9 @@ import org.jboss.weld.util.reflection.Reflections;
  * Each property may be set in three different sources (by priority in descending order):
  * </p>
  * <ol>
- * <li>Bootstrap configuration provided by an integrator</li>
- * <li>System property</li>
- * <li>Properties file <code>weld.properties</code></li>
+ * <li>In a properties file named `weld.properties`</li>
+ * <li>As a system property</li>
+ * <li>By a bootstrap configuration provided by an integrator</li>
  * </ol>
  *
  * <p>
