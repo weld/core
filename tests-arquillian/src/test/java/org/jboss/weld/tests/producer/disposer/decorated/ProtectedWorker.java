6e1c9161bc82bd0aad694caf092e3f52d724382e
Matej Novotny <manovotn@redhat.com>
2016-06-13 13:19:36 +0200

    WELD-2179 added tests for decorated producers and disposers.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/producer/disposer/decorated/ProtectedWorker.java
@@ -0,0 +1,50 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2016, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.producer.disposer.decorated;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.inject.Disposes;
+import javax.enterprise.inject.Produces;
+
+import org.jboss.weld.test.util.ActionSequence;
+
+/**
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@ApplicationScoped
+public class ProtectedWorker implements Worker{
+
+    private int hiddenField = 0;
+    
+    @Override
+    public void doStuff() {
+        ActionSequence.addAction(ProtectedWorker.class.getName());
+        hiddenField ++;
+    }
+    
+    @Produces
+    @Lazy
+    protected ProtectedWorker produceProtected(){
+        ActionSequence.addAction(ProtectedWorker.class.getName() + "-" + hiddenField);
+        return new ProtectedWorker();
+    }
+    
+    protected void dispose(@Disposes @Lazy ProtectedWorker worker) {
+        ActionSequence.addAction(ProtectedWorker.class.getName() + "-" + hiddenField);
+    }
+}
