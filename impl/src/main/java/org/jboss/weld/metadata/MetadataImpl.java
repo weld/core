03bef9c5d4f0f08e0c581f59099796a01d38a1f3
Tomas Remes <tremes@redhat.com>
2016-07-27 13:21:45 +0200

    WELD-2200 replace MetaDataImpl.

--- impl/src/main/java/org/jboss/weld/metadata/MetadataImpl.java
+++ impl/src/main/java/org/jboss/weld/metadata/MetadataImpl.java
@@ -1,12 +1,37 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2008, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.jboss.weld.metadata;
 
 import org.jboss.weld.bootstrap.spi.Metadata;
 
+/**
+ * @deprecated Use {@link org.jboss.weld.bootstrap.spi.helpers.MetadataImpl} insteads
+ * @param <T>
+ */
+@Deprecated
 public class MetadataImpl<T> implements Metadata<T> {
 
     private final String location;
     private final T value;
 
+    /**
+     * @param value
+     * @param location
+     */
     public MetadataImpl(T value, String location) {
         this.location = location;
         this.value = value;
@@ -24,5 +49,4 @@ public class MetadataImpl<T> implements Metadata<T> {
     public String toString() {
         return getLocation();
     }
-
 }
