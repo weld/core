d27a69c9f92cefee62df923dff70262fd280c6b5
Matej Novotny <manovotn@redhat.com>
2016-08-03 09:58:57 +0200

    Removal of util collection methods which were linked to Guava dependency.

--- impl/src/main/java/org/jboss/weld/injection/producer/ResourceInjector.java
+++ impl/src/main/java/org/jboss/weld/injection/producer/ResourceInjector.java
@@ -16,7 +16,7 @@
  */
 package org.jboss.weld.injection.producer;
 
-import static org.jboss.weld.util.collections.WeldCollections.immutableGuavaList;
+import static org.jboss.weld.util.collections.WeldCollections.immutableList;
 
 import java.util.List;
 import java.util.Set;
@@ -56,7 +56,7 @@ public class ResourceInjector<T> extends DefaultInjector<T> {
 
     protected ResourceInjector(EnhancedAnnotatedType<T> type, Bean<T> bean, BeanManagerImpl beanManager) {
         super(type, bean, beanManager);
-        this.resourceInjectionsHierarchy = immutableGuavaList(ResourceInjectionFactory.instance().getResourceInjections(bean, type,
+        this.resourceInjectionsHierarchy = immutableList(ResourceInjectionFactory.instance().getResourceInjections(bean, type,
                 beanManager));
     }
 
--- impl/src/main/java/org/jboss/weld/resolution/AbstractTypeSafeBeanResolver.java
+++ impl/src/main/java/org/jboss/weld/resolution/AbstractTypeSafeBeanResolver.java
@@ -16,7 +16,7 @@
  */
 package org.jboss.weld.resolution;
 
-import static org.jboss.weld.util.collections.WeldCollections.immutableGuavaSet;
+import static org.jboss.weld.util.collections.WeldCollections.immutableSet;
 
 import java.io.Serializable;
 import java.lang.reflect.GenericArrayType;
@@ -89,7 +89,7 @@ public abstract class AbstractTypeSafeBeanResolver<T extends Bean<?>, C extends 
                     allBeans.add(bean);
                 }
                 if (priorityBeans.isEmpty()) {
-                    return immutableGuavaSet(allBeans);
+                    return immutableSet(allBeans);
                 } else {
                     if (priorityBeans.size() == 1) {
                         return Collections.<Bean<?>>singleton(priorityBeans.iterator().next());
@@ -115,7 +115,7 @@ public abstract class AbstractTypeSafeBeanResolver<T extends Bean<?>, C extends 
                 Integer priority = beanManager.getEnabled().getAlternativePriority(bean.getBeanClass());
                 if (priority == null) {
                     // not all the beans left are alternatives with a priority - we are not able to resolve
-                    return immutableGuavaSet(alternatives);
+                    return immutableSet(alternatives);
                 }
                 if (priority > highestPriority) {
                     highestPriority = priority;
@@ -125,7 +125,7 @@ public abstract class AbstractTypeSafeBeanResolver<T extends Bean<?>, C extends 
                     selectedAlternativesWithHighestPriority.add(bean);
                 }
             }
-            return immutableGuavaSet(selectedAlternativesWithHighestPriority);
+            return immutableSet(selectedAlternativesWithHighestPriority);
         }
 
     }
