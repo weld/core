7ad6f32e93962c5240248d103807351142f5a354
Martin Kouba <mkouba@redhat.com>
2016-11-04 15:29:38 +0100

    WELD-2255 GroovyMethodFilter - filter methods only from groovy objects

--- impl/src/main/java/org/jboss/weld/bean/proxy/EnterpriseProxyFactory.java
+++ impl/src/main/java/org/jboss/weld/bean/proxy/EnterpriseProxyFactory.java
@@ -73,7 +73,7 @@ public class EnterpriseProxyFactory<T> extends ProxyFactory<T> {
     }
 
     @Override
-    protected boolean isMethodAccepted(Method method) {
-        return super.isMethodAccepted(method) && CommonProxiedMethodFilters.NON_PRIVATE.accept(method);
+    protected boolean isMethodAccepted(Method method, Class<?> proxySuperclass) {
+        return super.isMethodAccepted(method, proxySuperclass) && CommonProxiedMethodFilters.NON_PRIVATE.accept(method, proxySuperclass);
     }
 }
