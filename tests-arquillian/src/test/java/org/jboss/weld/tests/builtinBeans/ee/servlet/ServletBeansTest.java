be5f49fc40bddbce948c4af3add1d1835a818cf5
Martin Kouba <mkouba@redhat.com>
2017-03-03 16:48:40 +0100

    WELD-2346 Add test for eager session creation

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/ee/servlet/ServletBeansTest.java
@@ -0,0 +1,65 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.builtinBeans.ee.servlet;
+
+import java.net.URL;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.weld.test.util.Utils;
+import org.jboss.weld.tests.category.Integration;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+
+import com.gargoylesoftware.htmlunit.WebClient;
+
+@RunWith(Arquillian.class)
+@Category(Integration.class)
+public class ServletBeansTest {
+
+    @ArquillianResource
+    private URL url;
+    
+    @Deployment(testable = false)
+    public static WebArchive getDeployment() {
+        return ShrinkWrap.create(WebArchive.class, Utils.getDeploymentNameAsHash(ServletBeansTest.class, Utils.ARCHIVE_TYPE.WAR)).addClasses(Servlet.class, ServletBuiltinBeanInjectingBean.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
+    }
+
+    @Test
+    public void testHttpServletRequest() throws Exception {
+        WebClient client = new WebClient();
+        client.getPage(url + "/request?foo=bar");
+    }
+
+    @Test
+    public void testHttpSession() throws Exception {
+        WebClient client = new WebClient();
+        client.getPage(url + "/session");
+    }
+    
+    @Test
+    public void testServletContext() throws Exception {
+        WebClient client = new WebClient();
+        client.getPage(url + "/context");
+    }
+}
--- tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/ee/servlet/context/BarServlet.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/ee/servlet/context/BarServlet.java
@@ -14,19 +14,34 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.weld.tests.builtinBeans.ee.servletContext;
+package org.jboss.weld.tests.builtinBeans.ee.servlet.context;
 
-import javax.enterprise.context.Dependent;
+import java.io.IOException;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@Dependent
-public class SharedBean {
+@WebServlet("/")
+public class BarServlet extends HttpServlet {
 
     @Inject
     private ServletContext ctx;
 
-    public String getContextPath() {
-        return ctx.getContextPath();
+    @Inject
+    private SharedBean sharedBean;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain");
+        try {
+            resp.getWriter().write(ctx.getContextPath() + ";" + sharedBean.getContextPath());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
