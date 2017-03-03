ba3c3dc181d315f44a6b2315f233b536e7639e9d
Martin Kouba <mkouba@redhat.com>
2017-03-03 16:48:40 +0100

    WELD-2346 Add test for eager session creation

--- tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/ee/servlet/ServletBuiltinBeanInjectingBean.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/ee/servlet/ServletBuiltinBeanInjectingBean.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.jboss.weld.tests.builtinBeans.ee;
+package org.jboss.weld.tests.builtinBeans.ee.servlet;
 
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
