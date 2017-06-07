8bd88e80793ffa73f031778e75875947b0470e25
Matej Novotny <manovotn@redhat.com>
2017-06-07 11:34:13 +0200

    WELD-2393 Fix event firing ordering for ProcessInjectionTarget, ProcessProducer and ProcessBeanAttributes. Added a tests for this.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/event/lifecycle/ordering/LifecycleEventOrderingTest.java
@@ -0,0 +1,75 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.event.lifecycle.ordering;
+
+import java.util.List;
+
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessBeanAttributes;
+import javax.enterprise.inject.spi.ProcessInjectionPoint;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
+import javax.enterprise.inject.spi.ProcessManagedBean;
+import javax.enterprise.inject.spi.ProcessProducer;
+import javax.enterprise.inject.spi.ProcessProducerMethod;
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@RunWith(Arquillian.class)
+public class LifecycleEventOrderingTest {
+
+    @Deployment
+    public static Archive<?> getDeployment() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(LifecycleEventOrderingTest.class)).addPackage(LifecycleEventOrderingTest.class.getPackage())
+            .addAsServiceProvider(Extension.class, ProductManagement.class);
+    }
+
+    @Inject
+    ProductManagement extension;
+
+    @Test
+    public void testEventsWereFiredInCorrectOrderForProducer() {
+        List<Object> actualListOfEvents = extension.getListOfProducerEvents();
+        Assert.assertEquals(4, actualListOfEvents.size());
+        Assert.assertTrue(actualListOfEvents.get(0) instanceof ProcessInjectionPoint);
+        Assert.assertTrue(actualListOfEvents.get(1) instanceof ProcessProducer);
+        Assert.assertTrue(actualListOfEvents.get(2) instanceof ProcessBeanAttributes);
+        Assert.assertTrue(actualListOfEvents.get(3) instanceof ProcessProducerMethod);
+    }
+
+    @Test
+    public void testEventsWereFiredInCorrectOrderForOrdinaryBean() {
+        List<Object> actualListOfEvents = extension.getListOfBeanEvents();
+        Assert.assertEquals(4, actualListOfEvents.size());
+        Assert.assertTrue(actualListOfEvents.get(0) instanceof ProcessInjectionPoint);
+        Assert.assertTrue(actualListOfEvents.get(1) instanceof ProcessInjectionTarget);
+        Assert.assertTrue(actualListOfEvents.get(2) instanceof ProcessBeanAttributes);
+        Assert.assertTrue(actualListOfEvents.get(3) instanceof ProcessManagedBean);
+    }
+}
---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/event/lifecycle/ordering/ProductManagement.java
@@ -0,0 +1,85 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.event.lifecycle.ordering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.spi.Extension;
+import javax.enterprise.inject.spi.ProcessBeanAttributes;
+import javax.enterprise.inject.spi.ProcessInjectionPoint;
+import javax.enterprise.inject.spi.ProcessInjectionTarget;
+import javax.enterprise.inject.spi.ProcessManagedBean;
+import javax.enterprise.inject.spi.ProcessProducer;
+import javax.enterprise.inject.spi.ProcessProducerMethod;
+
+/**
+ * CDI defines this order (see Bean Discovery chapter) for producers: PIP -> PP -> PBA -> PPM (e.g. ProcessBean for producer methods)
+ * And following order for ordinary beans: PIP -> PIT -> PBA -> PB
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+public class ProductManagement implements Extension {
+
+    private List<Object> listOfProducerEvents = new ArrayList<>();
+    private List<Object> listOfBeanEvents = new ArrayList<>();
+
+    // producer method observers
+    
+    public void observePIP(@Observes ProcessInjectionPoint<PoorWorker, String> pip) {
+        listOfProducerEvents.add(pip);
+    }
+
+    public void observePP(@Observes ProcessProducer<PoorWorker, HighQualityAndLowCostProduct> pp) {
+        listOfProducerEvents.add(pp);
+    }
+
+    public void observePBA(@Observes ProcessBeanAttributes<HighQualityAndLowCostProduct> pba) {
+        listOfProducerEvents.add(pba);
+    }
+
+    public void observerPPM(@Observes ProcessProducerMethod<HighQualityAndLowCostProduct, PoorWorker> ppm) {
+        listOfProducerEvents.add(ppm);
+    }
+
+    // ordinary bean observers - PIP - PIT - PBA - PB
+
+    public void observePIPBean(@Observes ProcessInjectionPoint<PoorWorker,MassiveJugCoffee> pip) {
+        listOfBeanEvents.add(pip);
+    }
+    
+    public void observePIT(@Observes ProcessInjectionTarget<PoorWorker> pit) {
+        listOfBeanEvents.add(pit);
+    }
+
+    public void observePBABean(@Observes ProcessBeanAttributes<PoorWorker> pba) {
+        listOfBeanEvents.add(pba);
+    }
+
+    public void observerPB(@Observes ProcessManagedBean<PoorWorker> pb) {
+        listOfBeanEvents.add(pb);
+    }
+    
+    public List<Object> getListOfProducerEvents() {
+        return listOfProducerEvents;
+    }
+    
+    public List<Object> getListOfBeanEvents() {
+        return listOfBeanEvents;
+    }
+}
