4baca3c0e04fbdffe398fd2766ec95dc958dbaa2
Tomas Remes <tremes@redhat.com>
2017-07-14 14:37:09 +0200

    WELD-2407 fix selfinvocation of non-intercepted default method and expand SelfInvocationTest.

--- tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/self/SelfInvocationTest.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/self/SelfInvocationTest.java
@@ -1,3 +1,20 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.jboss.weld.tests.interceptors.self;
 
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -44,4 +61,19 @@ public class SelfInvocationTest {
         Assert.assertEquals(0, SecuredInterceptor.interceptedInvocations.size());
         Assert.assertEquals(0, BeanDecorator.decoratedInvocationCount);
     }
+
+    @Test
+    public void testSelfInvocationOfInterceptedMethod(SecondBean bean) {
+        // safety-check: make sure that intercepted method is intercepted when invoked standalone
+        SecuredInterceptor.reset();
+        bean.ping();
+        Assert.assertEquals(1, SecuredInterceptor.interceptedInvocations.size());
+        Assert.assertTrue(SecuredInterceptor.interceptedInvocations.contains("ping"));
+
+        // safety-check: make sure that intercepted method is not intercepted when invoked from unintercepted method
+        SecuredInterceptor.reset();
+        bean.doUnintercepted();
+        Assert.assertEquals(0, SecuredInterceptor.interceptedInvocations.size());
+    }
+
 }
