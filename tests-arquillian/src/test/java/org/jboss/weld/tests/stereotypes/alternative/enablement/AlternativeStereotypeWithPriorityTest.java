b6c00d403d201966f23813adc4c6de429d7b9ea4
Matej Novotny <matej.novotny2@gmail.com>
2017-12-19 15:26:13 +0100

    Add a test for stereotype alternative enabled by priority annotation.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/stereotypes/alternative/enablement/AlternativeStereotypeWithPriorityTest.java
@@ -0,0 +1,46 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.stereotypes.alternative.enablement;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Verify that a stereotype with @Alternative can be enabled by placing @Priority on the respective bean
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@RunWith(Arquillian.class)
+public class AlternativeStereotypeWithPriorityTest {
+
+    @Deployment
+    public static Archive<?> deployment() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(AlternativeStereotypeWithPriorityTest.class))
+            .addPackage(AlternativeStereotypeWithPriorityTest.class.getPackage());
+    }
+
+    @Test
+    public void testStereotypeGloballyEnabledByPriority(SomeInterface bean) {
+        Assert.assertEquals(AlternativeImpl.class.getSimpleName(), bean.ping());
+    }
+}
