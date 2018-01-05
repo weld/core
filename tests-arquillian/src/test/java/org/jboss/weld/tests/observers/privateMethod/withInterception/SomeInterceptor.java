da6bd4de73a855b243b256c49e817a6bf002d413
Matej Novotny <manovotn@redhat.com>
2018-01-05 11:26:31 +0100

    WELD-2443 add test.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/observers/privateMethod/withInterception/SomeInterceptor.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2017, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.observers.privateMethod.withInterception;
+
+import javax.annotation.Priority;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+/**
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@SomeBinding
+@Interceptor
+@Priority(1000)
+public class SomeInterceptor {
+
+    public static boolean INTERCEPTOR_TRIGGERED = false;
+
+    @AroundInvoke
+    public Object intercept(InvocationContext ic) throws Exception {
+        INTERCEPTOR_TRIGGERED = true;
+        return ic.proceed();
+    }
+}
