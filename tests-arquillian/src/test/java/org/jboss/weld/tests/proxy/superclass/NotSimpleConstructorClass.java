fa69aee89737e40d3b1feb91d3fffe8440b28774
Martin Kouba <mkouba@redhat.com>
2018-02-27 14:29:30 +0100

    WELD-2466: Relax unproxyable check in BM.getInjectableReference()

--- tests-arquillian/src/test/java/org/jboss/weld/tests/proxy/superclass/NotSimpleConstructorClass.java
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/proxy/superclass/NotSimpleConstructorClass.java
@@ -2,12 +2,19 @@ package org.jboss.weld.tests.proxy.superclass;
 
 public class NotSimpleConstructorClass {
 
+    private String value;
 
     public NotSimpleConstructorClass(String value) {
+        this.value = value;
     }
-    
+
     protected String giveMeNothing() {
         // dummy method
         return "nothing";
     }
+
+    public String getValue() {
+        return value;
+    }
+
 }
---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/proxy/superclass/unproxyable/GetInjectableReferenceUnproxyableCheckTest.java
@@ -0,0 +1,83 @@
+package org.jboss.weld.tests.proxy.superclass.unproxyable;
+
+import static org.junit.Assert.assertEquals;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Member;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.Set;
+
+import javax.enterprise.inject.spi.Annotated;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.jboss.weld.tests.proxy.superclass.NotSimpleConstructorClass;
+import org.jboss.weld.tests.proxy.superclass.SimpleBean;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ * @author Martin Kouba
+ */
+@RunWith(Arquillian.class)
+public class GetInjectableReferenceUnproxyableCheckTest {
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(GetInjectableReferenceUnproxyableCheckTest.class))
+                .addPackage(GetInjectableReferenceUnproxyableCheckTest.class.getPackage()).addClasses(SimpleBean.class, NotSimpleConstructorClass.class);
+    }
+
+    @Test
+    public void testClientProxy(BeanManager beanManager) {
+        SimpleBean simple = (SimpleBean) beanManager.getInjectableReference(new InjectionPoint() {
+
+            @Override
+            public boolean isTransient() {
+                return false;
+            }
+
+            @Override
+            public boolean isDelegate() {
+                return false;
+            }
+
+            @Override
+            public Type getType() {
+                return SimpleBean.class;
+            }
+
+            @Override
+            public Set<Annotation> getQualifiers() {
+                return Collections.emptySet();
+            }
+
+            @Override
+            public Member getMember() {
+                return null;
+            }
+
+            @Override
+            public Bean<?> getBean() {
+                return null;
+            }
+
+            @Override
+            public Annotated getAnnotated() {
+                return null;
+            }
+        }, beanManager.createCreationalContext(null));
+
+        assertEquals("nothing", simple.getValue());
+    }
+
+}
