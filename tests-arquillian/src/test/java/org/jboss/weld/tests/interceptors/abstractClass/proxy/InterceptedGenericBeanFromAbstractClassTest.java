aadd2edc6f76a97a2648a798e76c122f925c2682
Matej Novotny <manovotn@redhat.com>
2018-03-08 15:53:10 +0100

    WELD-2470 Fix InterceptedSubclassFactory, add reproducer.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/abstractClass/proxy/InterceptedGenericBeanFromAbstractClassTest.java
@@ -0,0 +1,53 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2018, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.interceptors.abstractClass.proxy;
+
+import javax.inject.Inject;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.BeanArchive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.weld.test.util.Utils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @see WELD-2470
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@RunWith(Arquillian.class)
+public class InterceptedGenericBeanFromAbstractClassTest {
+
+    @Deployment
+    public static Archive<?> deploy() {
+        return ShrinkWrap.create(BeanArchive.class, Utils.getDeploymentNameAsHash(InterceptedGenericBeanFromAbstractClassTest.class))
+            .addPackage(InterceptedGenericBeanFromAbstractClassTest.class.getPackage());
+    }
+
+    @Inject
+    CarServiceman serviceman;
+
+    @Test
+    public void testProxyCanBeCreated() {
+        Assert.assertNotNull(serviceman);
+        Assert.assertEquals(Car.class.getSimpleName(), serviceman.repair().getClass().getSimpleName());
+        Assert.assertTrue(VeryImportantPhoneCall.INTERCEPTOR_INVOKED);
+    }
+}
---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/interceptors/abstractClass/proxy/VeryImportantPhoneCall.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2018, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.interceptors.abstractClass.proxy;
+
+import javax.annotation.Priority;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+/**
+ *
+ * @author <a href="mailto:manovotn@redhat.com">Matej Novotny</a>
+ */
+@Interceptor
+@Priority(999)
+@InterruptWork
+public class VeryImportantPhoneCall {
+
+    public static boolean INTERCEPTOR_INVOKED = false;
+    
+    @AroundInvoke
+    public Object aroundInvoke(final InvocationContext invocation) throws Exception {
+        INTERCEPTOR_INVOKED = true;
+        return invocation.proceed();
+    }
+}
