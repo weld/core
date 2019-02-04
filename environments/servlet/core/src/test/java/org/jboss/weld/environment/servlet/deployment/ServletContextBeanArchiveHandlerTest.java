4e03bbd92797218045ce2bb2eae2dac2f2842660
Nikoleta Ziakova <nziakova@redhat.com>
2019-02-04 15:48:13 +0100

    WELD-2553 Updated specs versions to match Java EE 8

--- environments/servlet/core/src/test/java/org/jboss/weld/environment/servlet/deployment/ServletContextBeanArchiveHandlerTest.java
+++ environments/servlet/core/src/test/java/org/jboss/weld/environment/servlet/deployment/ServletContextBeanArchiveHandlerTest.java
@@ -254,6 +254,12 @@ public class ServletContextBeanArchiveHandlerTest {
         }
 
         @Override
+        public Dynamic addJspFile(String servletName, String jspFile) {
+
+            return null;
+        }
+
+        @Override
         public <T extends Servlet> T createServlet(Class<T> clazz) throws ServletException {
 
             return null;
@@ -371,6 +377,37 @@ public class ServletContextBeanArchiveHandlerTest {
         @Override
         public String getVirtualServerName() {
             return null;
+        }
+
+        @Override
+        public int getSessionTimeout() {
+
+            return 0;
+        }
+
+        @Override
+        public void setSessionTimeout(int sessionTimeout) {
+
+        }
+
+        @Override
+        public String getRequestCharacterEncoding() {
+            return null;
+        }
+
+        @Override
+        public void setRequestCharacterEncoding(String encoding) {
+
+        }
+
+        @Override
+        public String getResponseCharacterEncoding() {
+            return null;
+        }
+
+        @Override
+        public void setResponseCharacterEncoding(String encoding) {
+
         }
 
     }
