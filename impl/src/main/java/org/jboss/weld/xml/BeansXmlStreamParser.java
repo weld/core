48bbc510cd08e575e599170c34d6c50051ad5f80
rhsilva <rhsilva@localhost.localdomain>
2019-10-16 15:19:49 -0300

    Fixing issue about spec-descriptor-property-replacement and beans.xml

--- impl/src/main/java/org/jboss/weld/xml/BeansXmlStreamParser.java
+++ impl/src/main/java/org/jboss/weld/xml/BeansXmlStreamParser.java
@@ -388,7 +388,7 @@ public class BeansXmlStreamParser {
     }
 
     private String getTrimmedElementText(XMLEventReader reader) throws XMLStreamException {
-        return reader.getElementText().trim();
+        return interpolate(reader.getElementText().trim());
     }
 
     protected String interpolate(String value) {
