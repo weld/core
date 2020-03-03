a814dafd02ac601f1bd6244cba45807488aa604d
Martin Kouba <mkouba@redhat.com>
2020-03-03 10:50:31 +0100

    WELD-2615: Fix AttributeBeanStore.remove()
    - HttpSessionContext#destroy(Contextual<?>) should always destroy the
    underlying contextual instance

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/session/destroy/TestServlet.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2020, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.weld.tests.contexts.session.destroy;
+
+import java.io.IOException;
+
+import javax.enterprise.context.SessionScoped;
+import javax.enterprise.context.spi.AlterableContext;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+    @Inject
+    SessionScopedBean bean;
+
+    @Inject
+    BeanManager beanManager;
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String action = req.getParameter("action");
+        switch (action) {
+            case "init":
+                resp.getWriter().print(bean.ping());
+                break;
+            case "destroy":
+                Bean<?> bean = beanManager.getBeans(SessionScopedBean.class).iterator().next();
+                AlterableContext sessionContext = (AlterableContext) beanManager.getContext(SessionScoped.class);
+                sessionContext.destroy(bean);
+                resp.getWriter().print("ok");
+                break;
+            case "test":
+                resp.getWriter().print(SessionScopedBean.DESTROYED);
+                break;
+            default:
+                throw new IllegalStateException("Unknown action");
+        }
+    }
+
+}
