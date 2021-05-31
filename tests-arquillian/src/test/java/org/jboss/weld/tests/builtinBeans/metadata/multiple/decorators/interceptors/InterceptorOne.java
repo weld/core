c5a89a6a768c7201b35bc97e0f48ceedf5b2c557
Matej Novotny <manovotn@redhat.com>
2021-05-31 23:09:45 +0200

    WELD-2673 Add automated test, make sure that even when several decorators are present, they are always getting metadata of the bean they decorate.

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/builtinBeans/metadata/multiple/decorators/interceptors/InterceptorOne.java
@@ -0,0 +1,41 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2021, Red Hat, Inc., and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jboss.weld.tests.builtinBeans.metadata.multiple.decorators.interceptors;
+
+import javax.annotation.Priority;
+import javax.enterprise.inject.Intercepted;
+import javax.enterprise.inject.spi.Bean;
+import javax.inject.Inject;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.Interceptor;
+import javax.interceptor.InvocationContext;
+
+@Interceptor
+@Priority(1)
+@SomeBinding
+public class InterceptorOne {
+
+    @Inject
+    @Intercepted
+    Bean<?> metadata;
+
+    @AroundInvoke
+    public Object intercept(InvocationContext ctx) throws Exception {
+        return ctx.proceed() + metadata.getBeanClass().getSimpleName();
+    }
+}
