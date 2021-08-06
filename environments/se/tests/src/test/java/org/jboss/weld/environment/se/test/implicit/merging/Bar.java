59771bd30feccd7b5f526e891538ddd8b3b8d21d
Matej Novotny <manovotn@redhat.com>
2021-08-06 10:40:16 +0200

    Move BeansXmlMerging tests into environments/se/test folder to prevent classpath pollution

--- environments/se/tests/src/test/java/org/jboss/weld/environment/se/test/implicit/merging/Bar.java
+++ environments/se/tests/src/test/java/org/jboss/weld/environment/se/test/implicit/merging/Bar.java
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-package org.jboss.weld.environment.se.test.merging;
+package org.jboss.weld.environment.se.test.implicit.merging;
 
 public class Bar {
 }
