ff7677f3cb6fb334561d5e6b981a4ee71b37bd09
Matej Novotny <manovotn@redhat.com>
2023-09-15 08:37:13 +0200

    Format examples directory

--- examples/se/numberguess/src/main/java/org/jboss/weld/environment/se/example/numberguess/Main.java
+++ examples/se/numberguess/src/main/java/org/jboss/weld/environment/se/example/numberguess/Main.java
@@ -5,7 +5,8 @@ import org.jboss.weld.environment.se.Weld;
 public class Main {
 
     public static void main(String[] args) {
-        Weld weld = new Weld().beanClasses(Game.class, Generator.class, MessageGenerator.class, NumberGuessFrame.class).disableDiscovery();
+        Weld weld = new Weld().beanClasses(Game.class, Generator.class, MessageGenerator.class, NumberGuessFrame.class)
+                .disableDiscovery();
         // shutdown hook is registered automatically for WeldContainer
         weld.initialize();
     }
