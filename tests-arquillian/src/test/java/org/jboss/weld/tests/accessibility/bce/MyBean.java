1cd228698e4656e5cc8dc73fe9c04b4020bb7d62
Matej Novotny <manovotn@redhat.com>
2024-07-31 17:32:24 +0200

    WELD-2794 Add an automated test covering a related issue

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/accessibility/bce/MyBean.java
@@ -0,0 +1,7 @@
+package org.jboss.weld.tests.accessibility.bce;
+
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MyBean {
+}
---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/accessibility/bce/lib/MyBeanCreator.java
@@ -0,0 +1,17 @@
+package org.jboss.weld.tests.accessibility.bce.lib;
+
+import static org.junit.Assert.assertTrue;
+
+import jakarta.enterprise.inject.Instance;
+import jakarta.enterprise.inject.build.compatible.spi.Parameters;
+import jakarta.enterprise.inject.build.compatible.spi.SyntheticBeanCreator;
+import org.jboss.weld.tests.accessibility.bce.MyBean;
+
+public class MyBeanCreator implements SyntheticBeanCreator<SomeType> {
+    @Override
+    public SomeType create(Instance<Object> lookup, Parameters params) {
+        // assert that Instance provided here (BCE in non bean archive lib) can "see" MyBean
+        assertTrue(lookup.select(MyBean.class).isResolvable());
+        return new SomeType();
+    }
+}
