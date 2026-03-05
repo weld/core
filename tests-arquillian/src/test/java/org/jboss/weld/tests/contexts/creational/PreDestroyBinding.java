de8c4d2e3e5df8838f8d812039dbfdf928600d27
Matej Novotny <manovotn@ibm.com>
2026-03-05 15:00:55 +0100

    WELD-2834 Optimize in which cases Weld keeps reference to dependent instnace in CreationalContext

---
+++ tests-arquillian/src/test/java/org/jboss/weld/tests/contexts/creational/PreDestroyBinding.java
@@ -0,0 +1,11 @@
+package org.jboss.weld.tests.contexts.creational;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import jakarta.interceptor.InterceptorBinding;
+
+@Retention(RetentionPolicy.RUNTIME)
+@InterceptorBinding
+public @interface PreDestroyBinding {
+}
